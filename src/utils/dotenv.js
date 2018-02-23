08b63bee7a65f34af7cb51945746e3771133d100
Mariusz Roliński <mariusz@mondoreale.pl>
2018-02-23 12:22:15 +0100

    Check if a dot file exists*
    *) I let it explode on fs operation failure.

--- src/utils/dotenv.js
+++ src/utils/dotenv.js
@@ -4,9 +4,9 @@ const parse = (filename) => {
     const fs = require('fs')
     const dotenv = require('dotenv')
 
-    try {
+    if (fs.existsSync(filename)) {
         return dotenv.parse(fs.readFileSync(filename))
-    } catch (e) {
+    } else {
         return {}
     }
 }
