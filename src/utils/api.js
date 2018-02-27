eb0b8f5e0d9faca7baa8d8cb386700d0df9bcc31
Mariusz Roliński <mariusz@mondoreale.pl>
2018-02-27 12:56:34 +0100

    Keep url utils separately

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,26 +1,16 @@
 // @flow
 import request from './request'
 import type { ApiResult } from '../flowtype/common-types'
-
-const path = require('path')
-
-export const createApiUrl = (...urlParts: Array<string>): string => {
-    const rootUrl: string = process.env.API_URL || ''
-    const resource = path.join.apply(null, urlParts)
-
-    return `${rootUrl}/${resource}`
-}
+import { formatUrl } from './url'
 
 export const get = (endpoint: string, options?: Object): ApiResult => {
-    const fullUrl :string = createApiUrl(endpoint)
-
     const defaultOptions = {
         headers: {
             'Content-Type': 'application/json',
         },
     }
 
-    return request(fullUrl, {
+    return request(formatUrl(endpoint), {
         ...defaultOptions,
         ...options
     })
