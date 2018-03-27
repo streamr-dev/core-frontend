4921f316ea8855ea256fd05c8f32ff7c683291a2
Aapeli <aapeli.haanpuu@streamr.com>
2018-03-27 17:00:59 +0200

    createProduct service and tests

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -2,10 +2,12 @@
 
 import type { Currency, PriceUnit } from '../flowtype/common-types'
 
-export const currencies: Array<Currency> = [
-    'DATA',
-    'USD',
-]
+export const currencies: $ReadOnly<{
+    [Currency]: string
+}> = {
+    'DATA': 'DATA',
+    'USD': 'USD',
+}
 
 export const priceUnits: Array<PriceUnit> = [
     'second',
