f5b5abe642d9f789921cb414d36584cd267ba33a
Tuomas Koponen <tuomas.koponen@streamr.com>
2018-03-29 08:49:54 +0300

    Fix merge errors

--- src/components/ImageUpload/index.jsx
+++ src/components/ImageUpload/index.jsx
@@ -1,16 +1,48 @@
 // @flow
 
-import React, {Component} from 'react'
+import React, { Component } from 'react'
+import styles from './imageupload.pcss'
+import Dropzone from 'react-dropzone'
 
-type Props = {
-
+type DropzoneFile = File & {
+    preview?: string,
 }
 
-export default class Panel extends Component<Props> {
+type Props = {
+    setImageToUpload?: (File) => void,
+}
+
+type State = {
+    files: Array<DropzoneFile>,
+}
+
+export default class ImageUpload extends Component<Props, State> {
+    constructor() {
+        super()
+        this.state = {
+            files: [],
+        }
+    }
+
+    onDrop(files: Array<DropzoneFile>) {
+        if (files && files.length > 0 && this.props.setImageToUpload) {
+            const image = files[0]
+            this.props.setImageToUpload(image)
+        }
+
+        // Save image to the state also so that a preview can be shown
+        this.setState({
+            files
+        })
+    }
+
     render() {
         return (
             <div>
-                I present the image uploader to the user.
+                <Dropzone multiple={false} className={styles.dropzone} onDrop={(files) => this.onDrop(files)}>
+                    <p className={styles.helpText}>Drag & drop to upload a cover image or click to browse for one</p>
+                    {this.state.files.map((file, index) => <img className={styles.previewImage} key={index} src={file.preview} />)}
+                </Dropzone>
             </div>
         )
     }
