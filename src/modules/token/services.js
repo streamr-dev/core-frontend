027174c63eb8a95e5c55d96cef2bc7b51203641d
Aapeli <aapeli.haanpuu@streamr.com>
2018-04-04 15:21:18 +0200

    added service method for asking data/usd rate

--- src/modules/token/services.js
+++ src/modules/token/services.js
@@ -8,19 +8,29 @@ import getWeb3 from '../../web3/web3Provider'
 
 const tokenContractMethods = () => getContract(tokenConfig).methods
 const marketplaceContract = () => getContract(marketplaceConfig)
+const marketplaceContractMethods = () => marketplaceContract().methods
+
+export const getTokensFromWei = (wei: number) => (wei / 10e18)
 
 export const getMyAllowance = (): SmartContractCall<number> => {
     const web3 = getWeb3()
-    const tokenContract = getContract(tokenConfig)
     return web3.getDefaultAccount()
-        .then((myAddress) => call(tokenContract.methods.allowance(myAddress, marketplaceContract().options.address)))
+        .then((myAddress) => call(tokenContractMethods().allowance(myAddress, marketplaceContract().options.address)))
+        .then(parseFloat)
+        .then(getTokensFromWei)
 }
 
 export const getMyTokenBalance = (): SmartContractCall<number> => {
     const web3 = getWeb3()
     return web3.getDefaultAccount()
-        .then((myAddress) => call(getContract(tokenConfig).methods.balanceOf(myAddress)))
+        .then((myAddress) => call(tokenContractMethods().balanceOf(myAddress)))
+        .then(parseFloat)
+        .then(getTokensFromWei)
 }
+
+export const getDataPerUsd = (): SmartContractCall<number> => call(marketplaceContractMethods().dataPerUsd())
+    .then(parseFloat)
+    .then(getTokensFromWei)
 
 export const setAllowance = (amount: number): SmartContractTransaction => {
     if (amount < 0) {
