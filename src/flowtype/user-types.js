2c8905e27b5e7ee2b8de923b7cee65a6245143cf
Aapeli <aapeli.haanpuu@streamr.com>
2018-04-07 20:59:38 +0200

    added selector and forgotten type

--- src/flowtype/user-types.js
+++ src/flowtype/user-types.js
@@ -2,6 +2,12 @@
 
 export type UserId = string
 
+export type User = {
+    name: string,
+    username: string,
+    timezone: string,
+}
+
 export type LoginKey = {
     id: string,
     name?: string,
