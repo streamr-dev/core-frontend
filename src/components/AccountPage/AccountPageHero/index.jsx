7002a24e8a81bdd2c379184eac978ff3c795eb48
Aapeli <aapeli.haanpuu@streamr.com>
2018-04-08 09:56:23 +0200

    added account page hero with simple styling

--- src/components/AccountPage/AccountPageHero/index.jsx
+++ src/components/AccountPage/AccountPageHero/index.jsx
@@ -1,10 +1,12 @@
 // @flow
 
 import React from 'react'
-import { Link } from 'react-router-dom'
 import links from '../../../links'
 import type { User } from '../../../flowtype/user-types'
 import type { AccountPageTab } from '../../../containers/AccountPage'
+
+import styles from './accountPageHero.pcss'
+import Tab from './Tab'
 
 type Props = {
     user: ?User,
@@ -12,17 +14,19 @@ type Props = {
 }
 
 const AccountPageHero = ({ user, tab }: Props) => (
-    <div className="text-center">
-        <h1>{user && user.name}</h1>
-        <div>
-            <Link to={links.myProducts} className={tab === 'products' && 'selected'}>
-                My Products
-            </Link>
-            <Link to={links.myPurchases} className={tab === 'purchases' && 'selected'}>
-                My Purchases
-            </Link>
+    <section className={styles.accountPageHero}>
+        <h1 className={styles.title}>
+            {user && user.name}
+        </h1>
+        <div className={styles.tabBar}>
+            <Tab selected={tab} name="products" to={links.myProducts}>
+                Products
+            </Tab>
+            <Tab selected={tab} name="purchases" to={links.myPurchases}>
+                Purchases
+            </Tab>
         </div>
-    </div>
+    </section>
 )
 
 export default AccountPageHero
---
+++ src/components/AccountPage/MyPurchasesView/index.jsx
@@ -0,0 +1,13 @@
+// @flow
+
+import React from 'react'
+
+import styles from './myPurchasesView.pcss'
+
+const MyPurchasesView = () => (
+    <div className={styles.myPurchasesView}>
+        My Purchases Page
+    </div>
+)
+
+export default MyPurchasesView
