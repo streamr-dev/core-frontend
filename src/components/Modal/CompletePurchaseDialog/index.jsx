9582bf197111a20c1eb08ebd32aa84bccbc92daf
Juha Haavisto <juha.haavisto@sujuwa.fi>
2018-04-09 10:20:54 +0300

    Change if-elses into a switch in CompletePurchaseDialog

--- src/components/Modal/CompletePurchaseDialog/index.jsx
+++ src/components/Modal/CompletePurchaseDialog/index.jsx
@@ -11,36 +11,40 @@ export type Props = {
 }
 
 const CompletePurchaseDialog = ({ purchaseState }: Props) => {
-    if (purchaseState === transactionStates.HASH_RECEIVED) {
-        return (
-            <Dialog title="Writing to the blockchain">
-                <div>
-                    <p>Writing...</p>
-                    <p>You can wait for it to complete or close this window</p>
-                </div>
-            </Dialog>
-        )
-    } else if (purchaseState === transactionStates.MINED) {
-        return (
-            <Dialog title="Transaction complete">
-                <div>
-                    <p>Done!</p>
-                    <p>Please sign in or Sign up to view your purchase</p>
-                </div>
-            </Dialog>
-        )
-    } else if (purchaseState === transactionStates.FAILED) {
-        return (
-            <Dialog title="Error">
-                <div>
-                    <p>Oops...</p>
-                    <p>Something went wrong :(</p>
-                </div>
-            </Dialog>
-        )
-    }
+    switch (purchaseState) {
+        case transactionStates.HASH_RECEIVED:
+            return (
+                <Dialog title="Writing to the blockchain">
+                    <div>
+                        <p>Writing...</p>
+                        <p>You can wait for it to complete or close this window</p>
+                    </div>
+                </Dialog>
+            )
 
-    return null
+        case transactionStates.MINED:
+            return (
+                <Dialog title="Transaction complete">
+                    <div>
+                        <p>Done!</p>
+                        <p>Please sign in or Sign up to view your purchase</p>
+                    </div>
+                </Dialog>
+            )
+
+        case transactionStates.FAILED:
+            return (
+                <Dialog title="Error">
+                    <div>
+                        <p>Oops...</p>
+                        <p>Something went wrong :(</p>
+                    </div>
+                </Dialog>
+            )
+
+        default:
+            return null
+    }
 }
 
 export default CompletePurchaseDialog
