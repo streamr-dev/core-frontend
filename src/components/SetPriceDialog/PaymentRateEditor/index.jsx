ec49224d38985b49018b806e4b457609503120bf
Mariusz Roliński <mariusz@mondoreale.pl>
2018-04-11 12:13:04 +0200

    Split Rate editor into smaller components

--- src/components/SetPriceDialog/PaymentRateEditor/index.jsx
+++ src/components/SetPriceDialog/PaymentRateEditor/index.jsx
@@ -2,12 +2,11 @@
 
 import React from 'react'
 import { Row, Col } from '@streamr/streamr-layout'
-import { timeUnits, DATA_PER_USD, currencies } from '../../../utils/constants'
 import styles from './paymentRateEditor.pcss'
 import classNames from 'classnames'
 import type { Currency, TimeUnit } from '../../../flowtype/common-types'
-import TimeUnitButton from '../TimeUnitButton'
-import { convert, sanitize } from '../../../utils/price'
+import TimeUnitSelector from '../TimeUnitSelector'
+import AmountEditor from '../AmountEditor'
 
 export type PaymentRateChange = {
     amount?: ?number,
@@ -28,27 +27,10 @@ type State = {
 }
 
 class PaymentRateEditor extends React.Component<Props, State> {
-    state = {
-        amount: '',
-        currency: currencies.DATA,
-    }
-
-    componentWillMount() {
-        const amount = (this.props.amount || 0).toString()
-        const { currency } = this.props
-
-        this.setState({
+    onAmountChange = (amount: number) => {
+        this.props.onChange({
             amount,
-            currency,
         })
-    }
-
-    onUsdAmountChange = (e: SyntheticInputEvent<EventTarget>) => {
-        this.setAmount(e.target.value, currencies.USD)
-    }
-
-    onDataAmountChange = (e: SyntheticInputEvent<EventTarget>) => {
-        this.setAmount(e.target.value, currencies.DATA)
     }
 
     onTimeUnitChange = (timeUnit: TimeUnit) => {
@@ -57,81 +39,25 @@ class PaymentRateEditor extends React.Component<Props, State> {
         })
     }
 
-    setAmount = (amount: string, currency: Currency) => {
-        this.setState({
-            amount,
-            currency,
-        })
-
-        this.props.onChange({
-            amount: convert(sanitize(parseFloat(amount)), DATA_PER_USD, currency, this.props.currency),
-        })
-    }
-
-    getLocalAmount = (currency: Currency) => {
-        const { amount } = this.state
-
-        if (currency === this.state.currency) {
-            return amount
-        }
-
-        return convert(sanitize(parseFloat(amount)), DATA_PER_USD, this.state.currency, currency)
-    }
-
     render() {
-        const { className, timeUnit } = this.props
+        const { className, timeUnit, amount, currency } = this.props
 
         return (
             <div className={classNames(styles.editor, className)}>
                 <Row>
                     <Col xs={5}>
-                        <Row>
-                            <Col xs={5}>
-                                <Row>
-                                    <Col xs={8}>
-                                        <input
-                                            type="text"
-                                            value={this.getLocalAmount(currencies.DATA)}
-                                            onChange={this.onDataAmountChange}
-                                        />
-                                    </Col>
-                                    <Col xs={4}>
-                                        {currencies.DATA}
-                                    </Col>
-                                </Row>
-                            </Col>
-                            <Col xs={2}>
-                                =
-                            </Col>
-                            <Col xs={5}>
-                                <Row>
-                                    <Col xs={8}>
-                                        <input
-                                            type="text"
-                                            value={this.getLocalAmount(currencies.USD)}
-                                            onChange={this.onUsdAmountChange}
-                                        />
-                                    </Col>
-                                    <Col xs={4}>
-                                        {currencies.USD}
-                                    </Col>
-                                </Row>
-                            </Col>
-                        </Row>
+                        <AmountEditor
+                            amount={amount}
+                            currency={currency}
+                            onChange={this.onAmountChange}
+                        />
                     </Col>
                     <Col xs={2}>Per</Col>
                     <Col xs={5}>
-                        <div className={styles.timeUnits}>
-                            {Object.keys(timeUnits).map((unit) => (
-                                <TimeUnitButton
-                                    key={unit}
-                                    active={unit === timeUnit}
-                                    value={unit}
-                                    onClick={this.onTimeUnitChange}
-                                    className={styles.timeUnit}
-                                />
-                            ))}
-                        </div>
+                        <TimeUnitSelector
+                            selected={timeUnit}
+                            onChange={this.onTimeUnitChange}
+                        />
                     </Col>
                 </Row>
             </div>
