157bfdad51f71cf8bc1e98fab187fc3b9a82f39a
Tim Oxley <secoif@gmail.com>
2018-04-20 16:49:21 +0800

    Inject real dataPerUsd into ChooseAccessPeriodDialog.

---
+++ src/containers/ChooseAccessPeriodDialog/index.js
@@ -0,0 +1,13 @@
+// @flow
+
+import { connect } from 'react-redux'
+
+import { selectDataPerUsd } from '../../modules/global/selectors'
+import ChooseAccessPeriodDialog from '../../components/Modal/ChooseAccessPeriodDialog'
+
+const mapStateToProps = (state) => ({
+    dataPerUsd: selectDataPerUsd(state),
+})
+
+export default connect(mapStateToProps)(ChooseAccessPeriodDialog)
+
