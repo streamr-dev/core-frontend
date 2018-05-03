562cbd485123e5ff27f5c31c96e7ca174f60c595
Tuomas Koponen <tuomas.koponen@streamr.com>
2018-05-03 16:31:10 +0300

    Fix parameter name to match API

--- src/modules/categories/services.js
+++ src/modules/categories/services.js
@@ -6,5 +6,5 @@ import type { ApiResult } from '../../flowtype/common-types'
 import type { Category } from '../../flowtype/category-types'
 
 export const getCategories = (includeEmpty: boolean): ApiResult<Array<Category>> => get(formatUrl('categories', {
-    empty: includeEmpty,
+    includeEmpty,
 }))
