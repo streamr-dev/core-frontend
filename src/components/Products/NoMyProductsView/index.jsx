a29eb724d74559842e89f0321735a695d97f1a9f
Tommi <mato12@hotmail.fi>
2018-05-03 22:39:28 +0300

    [MAR-138] Moves special button styles in global (#165)
    * Changes styling to in global scope
    
    * rechanged the order of styles
    
    * Moves global stylings in app.pcss
    
    * Remove font-family

--- src/components/Products/NoMyProductsView/index.jsx
+++ src/components/Products/NoMyProductsView/index.jsx
@@ -1,6 +1,7 @@
 // @flow
 
 import React from 'react'
+import classNames from 'classnames'
 
 import NoProductsBaseView from '../NoProductsBaseView'
 import styles from './noMyProductsView.pcss'
@@ -10,7 +11,7 @@ const NoProductsView = () => (
         <p>You haven’t created any products yet.</p>
         <p>Click + to make one, or check out <br /> links below for some help.</p>
         <div className={styles.spacing}>
-            <a href="https://www.streamr.com/canvas/editor" className={styles.button}>
+            <a href="https://www.streamr.com/canvas/editor" className={classNames(styles.button, 'btn-special')}>
                 Go to editor
             </a>
         </div>
