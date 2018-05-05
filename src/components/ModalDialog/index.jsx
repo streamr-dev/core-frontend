aedc952fd06dcfa18a4efa5b77647cc46124935e
Aapeli Haanpuu <aapzu@iki.fi>
2018-05-05 13:17:55 +0200

    Modal overflow hidden (#190)
    * Add scroll disable/enable helpers
    
    * Disable scroll when opening modal

--- src/components/ModalDialog/index.jsx
+++ src/components/ModalDialog/index.jsx
@@ -6,6 +6,7 @@ import ReactModal2 from 'react-modal2'
 import classNames from 'classnames'
 
 import './accessibility'
+import { disableScroll, enableScroll } from '../../utils/scroll'
 import styles from './modaldialog.pcss'
 
 export type Props = {
@@ -16,24 +17,37 @@ export type Props = {
     backdropClassName?: string,
 }
 
-const ModalDialog = ({
-    onClose,
-    children,
-    className,
-    lightBackdrop,
-    backdropClassName,
-}: Props) => (
-    <ReactModal2
-        onClose={onClose}
-        backdropClassName={classNames(backdropClassName, (lightBackdrop ? styles.lightBackdrop : styles.backdrop))}
-        modalClassName={classNames(className, styles.dialog)}
-    >
-        {children}
-    </ReactModal2>
-)
+class ModalDialog extends React.Component<Props> {
+    static defaultProps = {
+        lightBackdrop: false,
+    }
 
-ModalDialog.defaultProps = {
-    lightBackdrop: false,
+    componentDidMount() {
+        disableScroll()
+    }
+
+    componentWillUnmount() {
+        enableScroll()
+    }
+
+    render() {
+        const {
+            onClose,
+            children,
+            className,
+            lightBackdrop,
+            backdropClassName,
+        } = this.props
+        return (
+            <ReactModal2
+                onClose={onClose}
+                backdropClassName={classNames(backdropClassName, (lightBackdrop ? styles.lightBackdrop : styles.backdrop))}
+                modalClassName={classNames(className, styles.dialog)}
+            >
+                {children}
+            </ReactModal2>
+        )
+    }
 }
 
 export default ModalDialog
