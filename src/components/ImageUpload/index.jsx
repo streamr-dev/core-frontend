f978d6ded2b8810d11b6235b196b5e79638da0ce
Matthew <fonty1@users.noreply.github.com>
2018-05-10 18:37:01 +0300

    [MAR 316] Product image upload styling (#240)
    [MAR 316] Product image upload styling (#240)

--- src/components/ImageUpload/index.jsx
+++ src/components/ImageUpload/index.jsx
@@ -3,6 +3,7 @@
 import React, { Component } from 'react'
 import Dropzone from 'react-dropzone'
 
+import UploadIcon from './ImageUploadIcon'
 import styles from './imageUpload.pcss'
 
 type DropzoneFile = File & {
@@ -16,11 +17,17 @@ type Props = {
 
 type State = {
     file: ?DropzoneFile,
+    hover: ?boolean,
+    imageUploading: ?boolean,
+    imageUploaded: ?boolean,
 }
 
 export default class ImageUpload extends Component<Props, State> {
     state = {
         file: null,
+        hover: false,
+        imageUploading: false,
+        imageUploaded: false,
     }
 
     onDrop = (files: Array<DropzoneFile>) => {
@@ -30,6 +37,8 @@ export default class ImageUpload extends Component<Props, State> {
             // Save image to the state also so that a preview can be shown
             this.setState({
                 file: image,
+                imageUploading: true,
+                imageUploaded: false,
             })
 
             if (this.props.setImageToUpload) {
@@ -38,26 +47,72 @@ export default class ImageUpload extends Component<Props, State> {
         }
     }
 
+    onDropAccepted = () => {
+        this.setState({
+            imageUploading: false,
+            imageUploaded: true,
+        })
+    }
+
+    onDropRejected = () => {
+        this.setState({
+            imageUploading: false,
+            imageUploaded: false,
+        })
+    }
+
     getPreviewImage = () => this.state.file && this.state.file.preview
+
+    setDropzoneHover = (hoverState: boolean) => {
+        this.setState({
+            hover: hoverState,
+        })
+    }
+
+    determineStyles = (hasImage: boolean) => {
+        const { imageUploaded, hover } = this.state
+        if ((hasImage && hover) || (imageUploaded && hover) || (!hasImage && !imageUploaded)) {
+            return styles.dropzoneAdvice
+        }
+
+        return styles.hide
+    }
 
     render() {
         const { originalImage } = this.props
+        const { imageUploading, imageUploaded, hover } = this.state
         const srcImage = this.getPreviewImage() || originalImage
         return (
-            <div>
+            <div
+                onMouseEnter={() => this.setDropzoneHover(true)}
+                onMouseLeave={() => this.setDropzoneHover(false)}
+            >
                 <Dropzone
                     multiple={false}
                     className={styles.dropzone}
                     onDrop={this.onDrop}
+                    onDropAccepted={this.onDropAccepted}
+                    onDropRejected={this.onDropRejected}
                     accept="image/jpeg, image/png"
                     maxSize={10e6}
                 >
-                    <p className={styles.helpText}>Drag & drop to upload a cover image or click to browse for one</p>
+                    <div
+                        className={imageUploading
+                            ? styles.dropzoneAdviceImageLoading
+                            : this.determineStyles(!!srcImage)
+                        }
+                    >
+                        <UploadIcon color={hover ? '#303030' : '#A6A6A6'} />
+                        <p>
+                            Drag & drop to {(imageUploaded || !!srcImage) ? 'replace your' : 'upload a'} cover image <br />
+                            or click to browse for one
+                        </p>
+                    </div>
                     {srcImage && (
                         <img
                             className={styles.previewImage}
                             src={srcImage}
-                            alt="To be uploaded"
+                            alt="Product Preview"
                         />
                     )}
                 </Dropzone>
