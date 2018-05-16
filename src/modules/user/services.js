0ecc46db8a22afdf4f9abc1bf393c089239fe2e8
rapuhotelli <rapuhotelli@users.noreply.github.com>
2018-05-16 13:12:16 +0300

    MAR-347 Make user requests unique (#279)

--- src/modules/user/services.js
+++ src/modules/user/services.js
@@ -6,11 +6,15 @@ import type { ApiResult } from '../../flowtype/common-types'
 import type { User, IntegrationKey, ApiKey, UserProductPermissionList } from '../../flowtype/user-types'
 import type { ProductId } from '../../flowtype/product-types'
 
-export const getMyKeys = (): ApiResult<Array<ApiKey>> => get(formatApiUrl('users', 'me', 'keys'))
+export const getMyKeys = (): ApiResult<Array<ApiKey>> => get(formatApiUrl('users', 'me', 'keys', {
+    noCache: Date.now(),
+}))
 
 export const getIntegrationKeys = (): ApiResult<Array<IntegrationKey>> => get(formatApiUrl('integration_keys'))
 
-export const getUserData = (): ApiResult<User> => get(formatApiUrl('users', 'me'))
+export const getUserData = (): ApiResult<User> => get(formatApiUrl('users', 'me', {
+    noCache: Date.now(),
+}))
 
 export const getUserProductPermissions = (id: ProductId): ApiResult<Array<UserProductPermissionList>> => (
     get(formatApiUrl('products', id, 'permissions', 'me'))
