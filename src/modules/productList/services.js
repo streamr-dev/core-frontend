cb9a4fbb3763b6b3fc2f240420f2861c639d5ef0
fonty1 <matthew.fontana@streamr.com>
2018-06-04 14:52:32 +0300

    remove states from getproducts

--- src/modules/productList/services.js
+++ src/modules/productList/services.js
@@ -6,14 +6,12 @@ import { formatApiUrl } from '../../utils/url'
 import type { ApiResult } from '../../flowtype/common-types'
 import type { Filter, ProductListPageWrapper } from '../../flowtype/product-types'
 import { mapProductFromApi } from '../../utils/product'
-import { productStates } from '../../utils/constants'
 
 export const getProducts = (filter: Filter, pageSize: number, offset: number): ApiResult<ProductListPageWrapper> => (
     get(formatApiUrl('products', {
         ...filter,
         publicAccess: true,
         grantedAccess: false,
-        states: productStates.DEPLOYED,
         max: pageSize + 1, // query 1 extra element to determine if we should show "load more" button
         offset,
     }))
