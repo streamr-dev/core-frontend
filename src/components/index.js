7a6cd4e730c6c3025e94661d9c79b810a52db724
Mariusz Roliński <mariusz@mondoreale.pl>
2018-06-08 18:44:19 +0200

    Add footer, button, dropdown, etc.

---
+++ src/components/index.js
@@ -0,0 +1,5 @@
+export { default as Page } from './Page'
+export { default as Button } from './Button'
+export { default as Dropdown } from './Dropdown'
+export { default as Footer } from './Footer'
+export { default as FooterColumn } from './Footer/FooterColumn'
