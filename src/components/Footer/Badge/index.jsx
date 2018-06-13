56141222cfb82e849c9c614a6306e1c7abef089f
Mariusz Roliński <mariusz@mondoreale.pl>
2018-06-13 13:32:07 +0200

    Fix badge links

--- src/components/Footer/Badge/index.jsx
+++ src/components/Footer/Badge/index.jsx
@@ -4,12 +4,8 @@ import React from 'react'
 import classNames from 'classnames'
 import { Translate } from 'react-redux-i18n'
 import Icon from '../../Icon'
-// import links from '../../../../../links'
+import urls from '../../../urls'
 import styles from './badge.pcss'
-
-const links = {
-    community: {},
-}
 
 type Props = {
     id: string,
@@ -18,7 +14,7 @@ type Props = {
 
 const Badge = ({ id, disabled }: Props) => (
     <div className={classNames(styles.badge, disabled && styles.disabled)}>
-        <a href={links.community[id]} target="_blank" rel="noopener noreferrer">
+        <a href={urls[id]} target="_blank" rel="noopener noreferrer">
             <Icon name={id} className={styles.icon} />
             <div className={styles.labelWrapper}>
                 <div className={styles.label}>
--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 // @flow
 
 import * as utils from './utils'
+import * as urls from './urls'
 
 export { default as breakpoints } from './breakpoints'
 export * from './components'
 export * from 'react-redux-i18n'
 
-export { utils }
+export {
+    utils,
+    urls,
+}
