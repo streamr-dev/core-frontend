de8f9c81f8432c40c214f57f823a8fdc1140eb93
Tim Oxley <secoif@gmail.com>
2018-06-19 22:16:56 +0800

    Read STREAMR_ env vars into frontend.

--- config/env.js
+++ config/env.js
@@ -51,9 +51,9 @@ process.env.NODE_PATH = (process.env.NODE_PATH || '')
     .map((folder) => path.resolve(appDirectory, folder))
     .join(path.delimiter)
 
-// Grab NODE_ENV and REACT_APP_* environment variables and prepare them to be
+// Grab NODE_ENV and STREAMR_* environment variables and prepare them to be
 // injected into the application via DefinePlugin in Webpack configuration.
-const REACT_APP = /^REACT_APP_/i
+const REACT_APP = /^STREAMR_/i
 
 function getClientEnvironment(publicUrl) {
     const raw = Object.keys(process.env)
