f7d020134b5f320e0cbfa986dfee71834b40a1e9
Tim Oxley <secoif@gmail.com>
2018-07-02 11:23:03 +0200

    Remove cruft.

--- src/history.js
+++
@@ -1,5 +0,0 @@
-// @flow
-
-import createHistory from 'history/createBrowserHistory'
-
-export default createHistory()
--- src/layouts/Logo.jsx
+++
@@ -1,6 +0,0 @@
-import React from 'react'
-import src from '../../public/images/streamr-logo.png'
-
-export default function Logo(props) {
-    return <img src={src} alt="Streamr Logo" {...props} />
-}
