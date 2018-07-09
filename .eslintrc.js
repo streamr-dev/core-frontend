9869dba5f8e6cdcbe247ee50eab905bebcaed058
Tim Oxley <secoif@gmail.com>
2018-07-09 14:59:28 +0800

    Relax some eslint rules.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,9 @@
 module.exports = exports = {
     extends: 'streamr',
     rules: {
+        "consistent-return": "off",
+        "no-param-reassign": "off",
+        "no-shadow": "off",
         "react/no-multi-comp": "off",
         "react/prop-types": "off",
         "react/no-unused-prop-types": "off",
