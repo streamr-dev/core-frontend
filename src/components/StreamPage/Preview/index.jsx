2d88e7b65547e1986ae696045b3dab0a8f0ab9d1
Tim Oxley <secoif@gmail.com>
2018-07-16 13:37:41 +0800

    [Stream] Clean up.

--- src/components/StreamPage/Preview/index.jsx
+++ src/components/StreamPage/Preview/index.jsx
@@ -1,7 +1,5 @@
 // @flow
 
-declare var keyId: string
-
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { Panel, Table, Modal, Button } from 'react-bootstrap'
