035761e0814309cdc7987342a32360402bdbeff9
fonty1 <matthew.fontana@streamr.com>
2018-08-09 11:33:58 +0300

    MAR-465 - Custom stack breakpoints for main product view - Upped max products that fit on a page (5 col grid max instead of 4) - Mobile text and tile padding tweaks - Actionbar mobile tweaking

--- src/components/Products/settings/index.jsx
+++ src/components/Products/settings/index.jsx
@@ -48,8 +48,8 @@ const setup = {
         cols: {
             xs: 12,
             sm: 6,
-            md: 4,
-            lg: 'custom',
+            md: 3,
+            lg: 'custom-products',
         },
     },
     relatedProducts: {
