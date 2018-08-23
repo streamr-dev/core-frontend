8fd2804d446894ebf19ddca3b470cbd36dda6348
mikhaelsantos <mikhaelsantos@gmail.com>
2018-08-23 11:40:10 +0300

    Initial travis commit

--- marketplace/travis_scripts/build-app.sh
+++ marketplace/travis_scripts/build-app.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
+cd marketplace
 npm run build
 
--- marketplace/travis_scripts/linter.sh
+++ marketplace/travis_scripts/linter.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
+cd marketplace
 ./node_modules/.bin/eslint src
 ./node_modules/.bin/stylelint **/*.css **/*.[ps]css
