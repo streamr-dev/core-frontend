52652eee06f766ba62ef6959c4337508a36b3414
Tim Oxley <secoif@gmail.com>
2018-08-28 16:01:00 +0800

    [ci] Bootstrap install in CI.

--- marketplace/travis_scripts/linter.sh
+++ marketplace/travis_scripts/linter.sh
@@ -1,10 +1,6 @@
 #!/usr/bin/env bash
 cd marketplace
-echo "NPM install"
-npm install
-
 echo "Run eslint"
-$TRAVIS_BUILD_DIR/marketplace/node_modules/.bin/eslint src
-
-echo "Run stylint"
-$TRAVIS_BUILD_DIR/marketplace//node_modules/.bin/stylelint **/*.css **/*.[ps]css
+./node_modules/.bin/eslint src
+echo "Run stylinglint"
+./node_modules/.bin/stylelint **/*.css **/*.[ps]css
