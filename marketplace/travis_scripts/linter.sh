c18f6bd22d60caae6ffb1f941283458abc633a8f
mikhaelsantos <mikhaelsantos@gmail.com>
2018-08-28 10:47:05 +0300

    chore: Add encryption: * Add SENTRY_TOKEN * Add NPM_TOKEN * Add AWS secret for S3 Upload
    -  Message

--- marketplace/travis_scripts/linter.sh
+++ marketplace/travis_scripts/linter.sh
@@ -1,4 +1,10 @@
 #!/usr/bin/env bash
 cd marketplace
-./node_modules/.bin/eslint src
-./node_modules/.bin/stylelint **/*.css **/*.[ps]css
+echo "NPM install"
+npm install
+
+echo "Run eslint"
+$TRAVIS_BUILD_DIR/marketplace/node_modules/.bin/eslint src
+
+echo "Run stylint"
+$TRAVIS_BUILD_DIR/marketplace//node_modules/.bin/stylelint **/*.css **/*.[ps]css
--- marketplace/travis_scripts/unit-tests.sh
+++ marketplace/travis_scripts/unit-tests.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 cd marketplace
+npm install
 npm test
 
