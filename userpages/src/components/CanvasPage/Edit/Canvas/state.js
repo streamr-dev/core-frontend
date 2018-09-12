b8470d52defe8c6f6fd1ad73ae348c0154adebf8
Tim Oxley <secoif@gmail.com>
2018-09-12 18:15:44 +0800

    [userpages Canvas/Edit] Factor out canvas module/port update+query operations. Create module/port convenience indexes.

--- userpages/src/components/CanvasPage/Edit/Canvas/state.js
+++ userpages/src/components/CanvasPage/Edit/Canvas/state.js
@@ -1,15 +1,220 @@
-import React, { Component } from 'react'
+import update from 'lodash/fp/update'
+import get from 'lodash/get'
 
 export const DragTypes = {
     Module: 'Module',
     Port: 'Port',
 }
 
-export class CanvasState extends React.Component {
+/**
+ * Module hash -> path to module in canvas
+ */
+function indexModules(canvas) {
+    return canvas.modules.reduce((o, m, index) => Object.assign(o, {
+        [m.hash]: ['modules', index],
+    }), {})
+}
 
-    render () {
-    
+/**
+ * Port Id -> path to port in canvas
+ */
+
+function indexPorts(canvas, moduleIndex) {
+    return Object.values(moduleIndex).reduce((o, modulePath) => {
+        const module = get(canvas, modulePath)
+        // create port paths by appending
+        // module path + inputs/outputs/params + port's index
+        module.params.forEach((port, index) => {
+            o[port.id] = modulePath.concat('params', index)
+        })
+        module.inputs.forEach((port, index) => {
+            o[port.id] = modulePath.concat('inputs', index)
+        })
+        module.outputs.forEach((port, index) => {
+            o[port.id] = modulePath.concat('outputs', index)
+        })
+        return o
+    }, {})
+}
+
+/**
+ * Module and Port Indexes
+ */
+
+function createIndex(canvas) {
+    if (!canvas || typeof canvas !== 'object') {
+        throw new Error('bad canvas')
+    }
+    const modules = indexModules(canvas)
+    const ports = indexPorts(canvas, modules)
+    return {
+        canvas,
+        modules,
+        ports,
     }
 }
 
+/**
+ * Memoized Index
+ */
 
+const cache = new WeakMap()
+
+export function getIndex(canvas) {
+    const cached = cache.get(canvas)
+    if (cached) { return cached }
+    const canvasIndex = createIndex(canvas)
+    cache.set(canvas, canvasIndex)
+    return canvasIndex
+}
+
+/**
+ * Determine if port is input/output/param
+ */
+
+function getPortType(canvas, portId) {
+    const canvasIndex = getIndex(canvas)
+    const path = canvasIndex.ports[portId]
+    if (!path) { return }
+    return path[path.length - 2].slice(0, -1) // inputs -> input, outputs -> output
+}
+
+/**
+ * Path to port's parent module
+ */
+
+function getPortModulePath(canvas, portId) {
+    const canvasIndex = getIndex(canvas)
+    const path = canvasIndex.ports[portId]
+    if (!path) { return }
+    return path.slice(0, -2)
+}
+
+/**
+ * True if port is an output port
+ */
+
+function getIsOutput(canvas, portId) {
+    const type = getPortType(canvas, portId)
+    return type === 'output'
+}
+
+export function getModuleForPort(canvas, portId) {
+    return get(canvas, getPortModulePath(canvas, portId))
+}
+
+export function getPort(canvas, portId) {
+    const { ports } = getIndex(canvas)
+    return get(canvas, ports[portId])
+}
+
+export function getConnectedPortIds(canvas, portId) {
+    const port = getPort(canvas, portId)
+    if (!getIsOutput(canvas, portId)) {
+        return [port.sourceId]
+    }
+
+    const { ports } = getIndex(canvas)
+    return Object.keys(ports).filter((id) => {
+        const { sourceId } = getPort(canvas, id)
+        return sourceId === portId
+    })
+}
+
+export function isPortConnected(canvas, portId) {
+    const conn = getConnectedPortIds(canvas, portId)
+    return !!conn.length
+}
+
+export function updatePort(canvas, portId, fn) {
+    const { ports } = getIndex(canvas)
+    return update(ports[portId], fn, canvas)
+}
+
+export function updateModulePosition(canvas, moduleId, diff) {
+    const { modules } = getIndex(canvas)
+    const modulePath = modules[moduleId]
+    return update(modulePath.concat('layout', 'position'), (position) => ({
+        ...position,
+        top: `${Number.parseInt(position.top, 10) + diff.y}px`,
+        left: `${Number.parseInt(position.left, 10) + diff.x}px`,
+    }), canvas)
+}
+
+function getOutputInputPorts(canvas, portIdA, portIdB) {
+    const ports = [getPort(canvas, portIdA), getPort(canvas, portIdB)]
+    if (getIsOutput(canvas, portIdB)) {
+        ports.reverse() // ensure output port is first
+    }
+
+    return ports
+}
+
+export function canConnectPorts(canvas, portIdA, portIdB) {
+    if (getIsOutput(canvas, portIdA) === getIsOutput(canvas, portIdB)) {
+        // if both inputs or both outputs, cannot connect
+        return false
+    }
+
+    const [output, input] = getOutputInputPorts(canvas, portIdA, portIdB)
+
+    if (!input.canConnect || !output.canConnect) { return false }
+
+    // verify compatible types
+    const inputTypes = new Set(input.acceptedTypes)
+    // Object type can connect to anything
+    if (output.type === 'Object' || inputTypes.has('Object')) { return true }
+    return inputTypes.has(output.type)
+}
+
+export function disconnectPorts(canvas, portIdA, portIdB) {
+    const [output, input] = getOutputInputPorts(canvas, portIdA, portIdB)
+    if (input.sourceId !== output.id) {
+        return canvas // not connected
+    }
+
+    // disconnect input
+    let nextCanvas = updatePort(canvas, input.id, (port) => ({
+        ...port,
+        sourceId: null,
+        connected: false,
+    }))
+
+    // disconnect output
+    nextCanvas = updatePort(nextCanvas, output.id, (port) => ({
+        ...port,
+        connected: isPortConnected(nextCanvas, output.id),
+    }))
+
+    return nextCanvas
+}
+
+export function connectPorts(canvas, portIdA, portIdB) {
+    if (!canConnectPorts(canvas, portIdA, portIdB)) {
+        throw new Error(`cannot connect ${portIdA}, ${portIdB}`)
+    }
+
+    const [output, input] = getOutputInputPorts(canvas, portIdA, portIdB)
+
+    let nextCanvas = canvas
+
+    if (input.sourceId) {
+        // disconnect existing input connection
+        nextCanvas = disconnectPorts(nextCanvas, input.sourceId, input.id)
+    }
+
+    // connect input
+    nextCanvas = updatePort(nextCanvas, input.id, (port) => ({
+        ...port,
+        sourceId: output.id,
+        connected: true,
+    }))
+
+    // connect output
+    nextCanvas = updatePort(nextCanvas, output.id, (port) => ({
+        ...port,
+        connected: isPortConnected(nextCanvas, output.id),
+    }))
+
+    return nextCanvas
+}
--- userpages/src/components/CanvasPage/Edit/index.jsx
+++ userpages/src/components/CanvasPage/Edit/index.jsx
@@ -1,10 +1,10 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import cloneDeep from 'lodash/cloneDeep'
-import update from 'lodash/fp/update'
 
 import { getCanvas } from '../../../modules/canvas/actions'
 import Canvas from './Canvas/index'
+import * as CanvasState from './Canvas/state'
 import CanvasToolbar from './Toolbar'
 
 import styles from './index.pcss'
@@ -32,124 +32,43 @@ export default connect((state, props) => ({
         this.props.getCanvas(this.props.match.params.id)
     }
 
-    static findModuleForPort(portId, canvas) {
-        return canvas.modules.find((m) => (
-            m.params.find(({ id }) => id === portId)
-            || m.outputs.find(({ id }) => id === portId)
-            || m.inputs.find(({ id }) => id === portId)
-        ))
-    }
-
-    static findPort(portId, canvas) {
-        const m = this.findModuleForPort(portId, canvas)
-        return m && (
-            m.params.find(({ id }) => id === portId)
-            || m.outputs.find(({ id }) => id === portId)
-            || m.inputs.find(({ id }) => id === portId)
-        )
-    }
-
-    static isOutputPortConnected(portId, canvas) {
-        return canvas.modules.find((m) => (
-            m.params.find(({ sourceId }) => sourceId === portId)
-            || m.inputs.find(({ sourceId }) => sourceId === portId)
-        ))
-    }
-
-    static updatePort(portId, { module, location }, fn, canvas) {
-        const moduleIndex = canvas.modules.findIndex((m) => m.hash === module.hash)
-        const portIndex = module[location].findIndex((p) => p.id === portId)
-        return update(['modules', moduleIndex, location, portIndex], fn, canvas)
-    }
-
     onDropModule = (props, monitor) => {
-        const { module } = monitor.getItem()
-        const { hash } = module
+        const { moduleId } = monitor.getItem()
         const diff = monitor.getDifferenceFromInitialOffset()
-        this.setState(({ canvas }) => {
-            const index = canvas.modules.findIndex((m) => m.hash === hash)
-            const newCanvas = update(['modules', index, 'layout', 'position'], (position) => {
-                if (!position) { return null }
-                return {
-                    ...position,
-                    top: `${Number.parseInt(position.top, 10) + diff.y}px`,
-                    left: `${Number.parseInt(position.left, 10) + diff.x}px`,
-                }
-            }, canvas)
-            return { canvas: newCanvas }
-        })
+
+        this.setState(({ canvas }) => ({
+            canvas: CanvasState.updateModulePosition(canvas, moduleId, diff),
+        }))
     }
 
-    onCanDropPort = (to, monitor) => {
+    onDragModule = (props) => ({ moduleId: props.module.hash })
+
+    dndModule = {
+        onDrag: this.onDragModule,
+        onDrop: this.onDropModule,
+        onCanDrop: () => true,
+        onCanDrag: () => true,
+    }
+
+    onCanDropPort = (props, monitor) => {
         const from = monitor.getItem()
-        if (from.direction === to.direction) { return false }
-
-        const ports = [from.port, to.port]
-        if (from.direction === 'output') {
-            ports.reverse()
-        }
-
-        const [input, output] = ports
-        if (!input.canConnect) { return false }
-        const inputTypes = new Set(input.acceptedTypes)
-        if (output.type === 'Object' || inputTypes.has('Object')) { return true }
-        return inputTypes.has(output.type)
+        return CanvasState.canConnectPorts(this.state.canvas, from.portId, props.port.id)
     }
 
-    onDragModule = (props) => props
-
-    onDragPort = (props) => props
+    onDragPort = (props) => ({ portId: props.port.id })
 
     onDropPort = (props, monitor) => {
-        const item = monitor.getItem()
-        let from
-        let to
-        if (item.direction === 'input') {
-            from = props
-            to = item
-        } else {
-            from = item
-            to = props
-        }
+        const from = monitor.getItem()
 
-        this.setState(({ canvas }) => {
-            let newCanvas = CanvasEdit.updatePort(from.port.id, from, (port) => ({
-                ...port,
-                connected: true,
-            }), canvas)
-
-            newCanvas = CanvasEdit.updatePort(to.port.id, to, (port) => ({
-                ...port,
-                sourceId: from.port.id,
-                connected: true,
-            }), newCanvas)
-
-            if (to.port.connected) {
-                // disconnect previous inputs
-                newCanvas = CanvasEdit.updatePort(to.port.sourceId, {
-                    module: CanvasEdit.findModuleForPort(to.port.sourceId, newCanvas),
-                    location: 'outputs',
-                }, (port) => ({
-                    ...port,
-                    connected: CanvasEdit.isOutputPortConnected(to.port.sourceId, newCanvas),
-                }), newCanvas)
-            }
-
-            return { canvas: newCanvas }
-        })
+        this.setState(({ canvas }) => ({
+            canvas: CanvasState.connectPorts(canvas, from.portId, props.port.id),
+        }))
     }
 
     dndPort = {
         onDrag: this.onDragPort,
         onDrop: this.onDropPort,
         onCanDrop: this.onCanDropPort,
-        onCanDrag: () => true,
-    }
-
-    dndModule = {
-        onDrag: this.onDragModule,
-        onDrop: this.onDropModule,
-        onCanDrop: () => true,
         onCanDrag: () => true,
     }
 
