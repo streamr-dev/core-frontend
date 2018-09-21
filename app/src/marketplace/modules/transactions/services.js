b9121bbcc1c5796746c07f6e5c17cd35a256d262
Juha Haavisto <juha.haavisto@sujuwa.fi>
2018-09-21 16:09:11 +0300

    Start tests for transaction modele

---
+++ app/src/marketplace/modules/transactions/services.js
@@ -0,0 +1,32 @@
+// @flow
+
+import { isSessionStorageAvailable } from '../../utils/storage'
+
+export const getTransactionsFromSessionStorage = (): Object => (
+    isSessionStorageAvailable() ? JSON.parse(sessionStorage.getItem('pendingTransactions') || '{}') : {}
+)
+
+const setTransactionsToSessionStorage = (txList: Object): void => {
+    if (isSessionStorageAvailable()) {
+        sessionStorage.setItem('pendingTransactions', JSON.stringify(txList))
+    }
+}
+
+export const addTransactionToSessionStorage = (id: Hash, type: TransactionType): void => {
+    const txList = {
+        ...getTransactionsFromSessionStorage(),
+        [id]: type,
+    }
+
+    setTransactionsToSessionStorage(txList)
+}
+
+export const removeTransactionFromSessionStorage = (id: Hash): void => {
+    const txList = {
+        ...getTransactionsFromSessionStorage(),
+        [id]: undefined,
+    }
+
+    setTransactionsToSessionStorage(txList)
+}
+
