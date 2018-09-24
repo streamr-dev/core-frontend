f9aa62a019f1f6a72408ce6a531976c31a96cd71
Juha Haavisto <juha.haavisto@sujuwa.fi>
2018-09-24 15:04:19 +0300

    Add tests for reducer & selectors

---
+++ app/test/unit/modules/transactions/selectors.test.js
@@ -0,0 +1,43 @@
+import assert from 'assert-diff'
+import { normalize } from 'normalizr'
+
+import * as all from '$mp/modules/transactions/selectors'
+import { transactionsSchema } from '$mp/modules/entities/schema'
+
+const transactions = [
+    {
+        id: '12345',
+        type: 'purchase',
+        state: 'pending',
+    },
+    {
+        id: 'abcdef',
+        type: 'allowance',
+        state: 'completed',
+    },
+]
+
+const normalized = normalize(transactions, transactionsSchema)
+
+const state = {
+    transactions: {
+        pending: ['12345'],
+        completed: ['abcdef'],
+    },
+    entities: normalized.entities,
+}
+
+describe('transactions - selectors', () => {
+    it('selects pending transaction ids', () => {
+        assert.deepStrictEqual(all.selectPendingTransactionIds(state), state.transactions.pending)
+    })
+
+    it('selects completed transaction ids', () => {
+        assert.deepStrictEqual(all.selectCompletedTransactionIds(state), state.transactions.completed)
+    })
+
+    it('selects the transaction entity', () => {
+        const selector = all.makeSelectTransaction('12345')
+        assert.deepStrictEqual(selector(state), transactions[0])
+    })
+})
