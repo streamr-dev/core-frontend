52fd9f1f6c6449890bbeb22dc49459fb8a1f37ac
Aapeli <aapeli.haanpuu@streamr.com>
2018-10-01 09:50:55 +0300

    Change require('react-redux-i18n').t to require('react-redux-i18n').I18n.t

--- app/src/marketplace/components/Products/NoMyProductsView/index.jsx
+++ app/src/marketplace/components/Products/NoMyProductsView/index.jsx
@@ -1,7 +1,7 @@
 // @flow
 
 import React from 'react'
-import { Translate, t } from 'react-redux-i18n'
+import { Translate, I18n } from 'react-redux-i18n'
 
 import { newCanvas } from '../../../../links'
 import EmptyState from '../../EmptyState'
@@ -14,7 +14,7 @@ const NoProductsView = () => (
             <img
                 src={emptyStateIcon}
                 srcSet={`${emptyStateIcon2x} 2x`}
-                alt={t('error.notFound')}
+                alt={I18n.t('error.notFound')}
             />
         )}
         link={(
