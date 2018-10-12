01a3cb1fea4d288b4eeb868a7646ead64373563e
Juha Haavisto <juha.haavisto@sujuwa.fi>
2018-10-12 16:15:56 +0200

    Update table styles

--- app/src/userpages/components/StreamPage/List/index.jsx
+++ app/src/userpages/components/StreamPage/List/index.jsx
@@ -5,8 +5,8 @@ import { Button } from 'reactstrap'
 import links from '../../../../links'
 import { getStreams } from '$userpages/modules/userPageStreams/actions'
 import * as StreamDelete from '../Show/InfoView/StreamDeleteButton'
-import Table from '../../Table'
 import { selectStreams } from '$userpages/modules/userPageStreams/selectors'
+import Table from '$shared/components/Table'
 
 const StreamDeleteButton = connect(null, StreamDelete.mapDispatchToProps)(StreamDelete.StreamDeleteButton)
 
@@ -25,35 +25,30 @@ export default connect((state) => ({
                 <Button id="streamlist-create-stream">
                     <Link to={links.userpages.streamCreate}>Create Stream</Link>
                 </Button>
-                <span>
-                    Streams
-                </span>
                 <Table>
                     <thead>
                         <tr>
                             <th>Name</th>
                             <th>Description</th>
-                            <th>Type</th>
                             <th>Updated</th>
+                            <th>Last Data</th>
+                            <th>Status</th>
                             <th />
                         </tr>
                     </thead>
                     <tbody>
                         {!Object.values(this.props.streams).length && (
-                            <Table.EmptyRow>
+                            <tr>
                                 <td colSpan="5">No Streams</td>
-                            </Table.EmptyRow>
+                            </tr>
                         )}
                         {Object.values(this.props.streams).map((stream) => (
                             <tr key={stream.id}>
-                                <td>
-                                    <Link to={`${links.userpages.streamShow}/${stream.id}`}>
-                                        {stream.name}
-                                    </Link>
-                                </td>
-                                <td>{stream.description}</td>
-                                <td>{stream.feed.name}</td>
+                                <th>{stream.name}</th>
+                                <td title={stream.description}>{stream.description}</td>
                                 <td>{new Date(stream.lastUpdated).toLocaleString()}</td>
+                                <td>-</td>
+                                <td>-</td>
                                 <td>
                                     <Button>Share</Button>
                                     <StreamDeleteButton stream={stream}>Delete</StreamDeleteButton>
