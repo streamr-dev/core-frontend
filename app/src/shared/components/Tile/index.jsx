5c7017f7bd0c238a3900ea5fc91b17777b883994
Tuomas Koponen <tuomas.koponen@streamr.com>
2018-10-16 13:59:19 +0200

    Add canvas listing page

---
+++ app/src/shared/components/Tile/index.jsx
@@ -0,0 +1,46 @@
+// @flow
+
+import React, { type Element } from 'react'
+import { Link } from 'react-router-dom'
+
+import { withHover } from '$mp/components/WithHover'
+import DropdownActions from '$shared/components/DropdownActions'
+import Meatball from '$shared/components/Meatball'
+
+import styles from './tile.pcss'
+
+type Props = {
+    children: Array<Element<any>>,
+    link: string,
+    imageUrl: string,
+    isHovered: boolean,
+    dropdownActions?: Array<typeof DropdownActions.Item>,
+}
+
+const Tile = ({
+    link,
+    imageUrl,
+    children,
+    isHovered,
+    dropdownActions,
+}: Props) => (
+    <Link className={styles.tile} to={link}>
+        <div className={styles.image}>
+            <img src={imageUrl} alt="TODO" />
+            {isHovered && dropdownActions &&
+                <DropdownActions
+                    className={styles.menu}
+                    title={<Meatball alt="Select" white />}
+                    noCaret
+                >
+                    {dropdownActions}
+                </DropdownActions>
+            }
+        </div>
+        <div className={styles.content}>
+            {children}
+        </div>
+    </Link>
+)
+
+export default withHover(Tile)
--- app/src/userpages/components/CanvasPage/List/index.jsx
+++ app/src/userpages/components/CanvasPage/List/index.jsx
@@ -1,63 +1,117 @@
-import React, { Component } from 'react'
+// @flow
+
+import React, { Component, Fragment } from 'react'
 import { connect } from 'react-redux'
-import { Link } from 'react-router-dom'
-import { Button } from 'reactstrap'
+import { Container, Row, Col } from 'reactstrap'
+import { capital } from 'case'
+import { push } from 'react-router-redux'
+import { CopyToClipboard } from 'react-copy-to-clipboard'
+import { Translate } from 'react-redux-i18n'
+
 import links from '../../../../links'
-import { getCanvases } from '../../../modules/canvas/actions'
-import Table from '../../Table'
+import { getCanvases, deleteCanvas } from '../../../modules/canvas/actions'
+import Tile from '$shared/components/Tile'
+import DropdownActions from '$shared/components/DropdownActions'
+import { formatExternalUrl } from '$shared/utils/url'
 import Layout from '../../Layout'
 
-export default connect((state) => ({
-    canvases: state.canvas.list || [],
-}), {
-    getCanvases,
-})(class CanvasList extends Component {
+import styles from './list.pcss'
+
+export type StateProps = {
+    canvases: any,
+}
+
+export type DispatchProps = {
+    getCanvases: () => void,
+    deleteCanvas: (id: string) => void,
+    navigate: (to: string) => void,
+}
+
+type Props = StateProps & DispatchProps
+
+class CanvasList extends Component<Props> {
     componentDidMount() {
         this.props.getCanvases()
     }
 
+    getActions = (canvas) => {
+        const { navigate, deleteCanvas } = this.props
+
+        return (
+            <Fragment>
+                <DropdownActions.Item onClick={() => navigate(`${links.userpages.canvasEditor}/${canvas.id}`)}>
+                    <Translate value="canvasList.edit" />
+                </DropdownActions.Item>
+                <DropdownActions.Item
+                    onClick={() => console.error('Not implemented')}
+                >
+                    <Translate value="canvasList.share" />
+                </DropdownActions.Item>
+                <DropdownActions.Item>
+                    <CopyToClipboard
+                        text={formatExternalUrl(
+                            process.env.PLATFORM_ORIGIN_URL,
+                            process.env.PLATFORM_BASE_PATH,
+                            `${links.userpages.canvasEditor}/${canvas.id}`,
+                        )}
+                    >
+                        <Translate value="canvasList.copyUrl" />
+                    </CopyToClipboard>
+                </DropdownActions.Item>
+                <DropdownActions.Item onClick={() => deleteCanvas(canvas.id)}>
+                    <Translate value="canvasList.delete" />
+                </DropdownActions.Item>
+            </Fragment>
+        )
+    }
+
     render() {
+        const { canvases } = this.props
+
+        const cols = {
+            xs: 12,
+            sm: 6,
+            md: 6,
+            lg: 3,
+        }
+
+        // Disable max-len until we have proper canvas images available
+        /* eslint-disable max-len */
         return (
             <Layout>
-                <div className="container">
-                    <Button>
-                        <Link to={links.userpages.newCanvas}>Create Canvas</Link>
-                    </Button>
-                    <h1>Canvases</h1>
-                    <Table>
-                        <thead>
-                            <tr>
-                                <th>Name</th>
-                                <th>State</th>
-                                <th>Updated</th>
-                                <th />
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {!this.props.canvases.length && (
-                                <tr>
-                                    <td colSpan="4">No Canvases</td>
-                                </tr>
-                            )}
-                            {this.props.canvases.map((canvas) => (
-                                <tr key={canvas.id}>
-                                    <td>
-                                        <Link to={`${links.userpages.canvasEditor}/${canvas.id}`}>
-                                            {canvas.name}
-                                        </Link>
-                                    </td>
-                                    <td>{canvas.state}</td>
-                                    <td>{new Date(canvas.updated).toLocaleString()}</td>
-                                    <td>
-                                        <Button>Share</Button>
-                                        <Button>Delete</Button>
-                                    </td>
-                                </tr>
-                            ))}
-                        </tbody>
-                    </Table>
-                </div>
+                <Container>
+                    {!canvases.length && (
+                        <span>TODO: Empty state component here when available</span>
+                    )}
+                    <Row>
+                        {canvases.map((canvas) => (
+                            <Col {...cols} key={canvas.id}>
+                                <Tile
+                                    link={`${links.userpages.canvasEditor}/${canvas.id}`}
+                                    imageUrl="https://s3-eu-west-1.amazonaws.com/streamr-dev-public/product-images/test-hero-images/pexels-photo-158904.jpeg"
+                                    dropdownActions={this.getActions(canvas)}
+                                >
+                                    <div className={styles.title}>{canvas.name}</div>
+                                    <div className={styles.date}>{new Date(canvas.updated).toLocaleString()}</div>
+                                    <div className={styles.status}>{capital(canvas.state)}</div>
+                                </Tile>
+                            </Col>
+                        ))}
+                    </Row>
+                </Container>
             </Layout>
         )
     }
+}
+
+export const mapStateToProps = (state: any): StateProps => ({
+    canvases: state.canvas.list || [],
 })
+
+export const mapDispatchToProps = (dispatch: Function): DispatchProps => ({
+    getCanvases: () => dispatch(getCanvases()),
+    deleteCanvas: (id) => dispatch(deleteCanvas(id)),
+    navigate: (to) => dispatch(push(to)),
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(CanvasList)
