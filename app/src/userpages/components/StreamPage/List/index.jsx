661f4fc1883bafe36e22db9bf56b8131d0e8e016
Juha Haavisto <juha.haavisto@sujuwa.fi>
2018-10-17 11:06:11 +0200

    Add dropdown actions

--- app/src/userpages/components/StreamPage/List/index.jsx
+++ app/src/userpages/components/StreamPage/List/index.jsx
@@ -4,11 +4,13 @@ import { Link } from 'react-router-dom'
 import { Button } from 'reactstrap'
 import links from '../../../../links'
 import { getStreams } from '$userpages/modules/userPageStreams/actions'
-import * as StreamDelete from '../Show/InfoView/StreamDeleteButton'
+// import * as StreamDelete from '../Show/InfoView/StreamDeleteButton'
 import { selectStreams } from '$userpages/modules/userPageStreams/selectors'
 import Table from '$shared/components/Table'
+import DropdownActions from '$shared/components/DropdownActions'
+import Meatball from '$shared/components/Meatball'
 
-const StreamDeleteButton = connect(null, StreamDelete.mapDispatchToProps)(StreamDelete.StreamDeleteButton)
+// const StreamDeleteButton = connect(null, StreamDelete.mapDispatchToProps)(StreamDelete.StreamDeleteButton)
 
 export default connect((state) => ({
     streams: selectStreams(state),
@@ -50,8 +52,17 @@ export default connect((state) => ({
                                 <td>-</td>
                                 <td>-</td>
                                 <td>
-                                    <Button>Share</Button>
-                                    <StreamDeleteButton stream={stream}>Delete</StreamDeleteButton>
+                                    <DropdownActions
+                                        title={<Meatball alt="Select" />}
+                                        noCaret
+                                    >
+                                        <DropdownActions.Item>Open</DropdownActions.Item>
+                                        <DropdownActions.Item>Edit</DropdownActions.Item>
+                                        <DropdownActions.Item>Copy ID</DropdownActions.Item>
+                                        <DropdownActions.Item>Copy Snippet</DropdownActions.Item>
+                                        <DropdownActions.Item>Share</DropdownActions.Item>
+                                        <DropdownActions.Item>Delete</DropdownActions.Item>
+                                    </DropdownActions>
                                 </td>
                             </tr>
                         ))}
