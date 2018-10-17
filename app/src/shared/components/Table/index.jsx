f423621d5f1654c3ad6cf0e5cacd2f6eb09d91cb
Juha Haavisto <juha.haavisto@sujuwa.fi>
2018-10-17 13:14:57 +0200

    Add separate components for table, row and cell

--- app/src/shared/components/Table/index.jsx
+++ app/src/shared/components/Table/index.jsx
@@ -1,6 +1,6 @@
 // @flow
 
-import React from 'react'
+import React, { type Node } from 'react'
 
 import PropTypes from 'prop-types'
 import { Table as ReactstrapTable } from 'reactstrap'
@@ -12,10 +12,46 @@ type Props = {
     className?: String,
 }
 
-const Table = ({ children, className, ...props }: Props) => (
-    <ReactstrapTable responsive className={cx(className, styles.table)} {...props}>
-        {children}
-    </ReactstrapTable>
-)
+const makeComponent = (Element: string, styleOverrides?: Array<string> = []) => {
+    type ComponentProps = {
+        className?: string,
+        children?: Node,
+    }
 
-export default Table
+    return ({ children, className, ...props }: ComponentProps) => {
+        const styleFlags = (styleOverrides || []).reduce((result, flag) => (
+            styles[flag] ? {
+                ...result,
+                [styles[flag]]: !!props[flag],
+            } : result
+        ), {})
+
+        return (
+            <Element className={cx(className, styleFlags)} {...props}>{children}</Element>
+        )
+    }
+}
+
+export const Head = makeComponent('thead')
+export const Body = makeComponent('tbody')
+export const Tr = makeComponent('tr')
+export const Td = makeComponent('td', ['noWrap'])
+export const Th = makeComponent('th', ['noWrap'])
+
+export default class Table extends React.Component<Props> {
+    static Head = Head
+    static Body = Body
+    static Tr = Tr
+    static Th = Th
+    static Td = Td
+
+    render() {
+        const { children, className, ...props } = this.props
+
+        return (
+            <ReactstrapTable responsive className={cx(className, styles.table)} {...props}>
+                {children}
+            </ReactstrapTable>
+        )
+    }
+}
