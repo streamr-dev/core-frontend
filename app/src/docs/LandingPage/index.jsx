380450a26f7b92c13990352ebaa08417fba8ebe4
fonty1 <matthew.fontana@streamr.com>
2018-10-22 08:21:03 +0300

    using Layout instead of Page which was removed from the shared components

--- app/src/docs/LandingPage/index.jsx
+++ app/src/docs/LandingPage/index.jsx
@@ -5,15 +5,20 @@ import { Container, Row, Col } from 'reactstrap'
 
 // $FlowFixMe
 import ExampleDoc from './exampleDoc.mdx'
+import Layout from '$mp/components/Layout'
+
+import styles from './landingPage.pcss'
 
 const LandingPage = () => (
-    <Container>
-        <Row>
-            <Col>
-                <ExampleDoc />
-            </Col>
-        </Row>
-    </Container>
+    <Layout className={styles.layout} footer>
+        <Container>
+            <Row>
+                <Col>
+                    <ExampleDoc />
+                </Col>
+            </Row>
+        </Container>
+    </Layout>
 )
 
 export default LandingPage
