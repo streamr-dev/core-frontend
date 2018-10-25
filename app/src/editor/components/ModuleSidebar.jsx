ca4df334396ab757a28a3cb30894f36e054fb0ea
Tim Oxley <secoif@gmail.com>
2018-10-25 14:52:01 +0800

    [editor] Constrain sidebar height to editor area. Enable scrolling. Fix width.

--- app/src/editor/components/ModuleSidebar.jsx
+++ app/src/editor/components/ModuleSidebar.jsx
@@ -44,44 +44,54 @@ export default class ModuleSidebar extends React.Component {
         const optionsKeys = Object.keys(module.options || {})
         return (
             <div className={cx(styles.sidebar)} hidden={!this.props.isOpen}>
-                <div className={cx(styles.header)}>
-                    <h3 className={cx(styles.name)}>{module.displayName || module.name}</h3>
-                    <button type="button" onClick={() => this.props.open(false)}>X</button>
-                </div>
-                <div className={cx(styles.content)}>
-                    {!optionsKeys.length ? null : (
-                        <div className={cx(styles.options)}>
-                            <h4>Options</h4>
-                            <div className={cx(styles.optionsFields)}>
-                                {optionsKeys.map((name) => {
-                                    const option = module.options[name]
-                                    const id = `${module.id}.options.${name}`
-                                    return (
-                                        <React.Fragment key={id}>
-                                            <label htmlFor={id}>{startCase(name)}</label>
-                                            {option.possibleValues ? (
-                                                <select id={id} value={option.value} onChange={this.onChangeValue(name)}>
-                                                    {option.possibleValues.map(({ text, value }) => (
-                                                        <option key={value} value={value}>{text}</option>
-                                                    ))}
-                                                </select>
-                                            ) : (
-                                                (option.type === 'boolean' && (
-                                                    <input id={id} checked={option.value} type="checkbox" onChange={this.onChangeChecked(name)} />
-                                                )) || (
-                                                    <TextInput value={option.value} onChange={this.onChange(name)}>
-                                                        {({ innerRef, ...props }) => (
-                                                            <input id={id} type="text" {...props} ref={innerRef} />
-                                                        )}
-                                                    </TextInput>
-                                                )
-                                            )}
-                                        </React.Fragment>
-                                    )
-                                })}
+                <div className={cx(styles.sidebarInner)}>
+                    <div className={cx(styles.header)}>
+                        <h3 className={cx(styles.name)}>{module.displayName || module.name}</h3>
+                        <button type="button" onClick={() => this.props.open(false)}>X</button>
+                    </div>
+                    <div className={cx(styles.content)}>
+                        {!optionsKeys.length ? null : (
+                            <div className={cx(styles.options)}>
+                                <h4>Options</h4>
+                                <div className={cx(styles.optionsFields)}>
+                                    {optionsKeys.map((name) => {
+                                        const option = module.options[name]
+                                        const id = `${module.id}.options.${name}`
+                                        return (
+                                            <React.Fragment key={id}>
+                                                <label htmlFor={id}>{startCase(name)}</label>
+                                                {option.possibleValues ? (
+                                                    /* Select */
+                                                    <select id={id} value={option.value} onChange={this.onChangeValue(name)}>
+                                                        {option.possibleValues.map(({ text, value }) => (
+                                                            <option key={value} value={value}>{text}</option>
+                                                        ))}
+                                                    </select>
+                                                ) : (
+                                                    /* Toggle */
+                                                    (option.type === 'boolean' && (
+                                                        <input
+                                                            id={id}
+                                                            checked={option.value}
+                                                            type="checkbox"
+                                                            onChange={this.onChangeChecked(name)}
+                                                        />
+                                                    )) || (
+                                                        /* Text */
+                                                        <TextInput value={option.value} onChange={this.onChange(name)}>
+                                                            {({ innerRef, ...props }) => (
+                                                                <input id={id} type="text" {...props} ref={innerRef} />
+                                                            )}
+                                                        </TextInput>
+                                                    )
+                                                )}
+                                            </React.Fragment>
+                                        )
+                                    })}
+                                </div>
                             </div>
-                        </div>
-                    )}
+                        )}
+                    </div>
                 </div>
             </div>
         )
