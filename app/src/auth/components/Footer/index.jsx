59a587b484588fe52018b8153a9bf0d6ead46067
Mariusz Roliński <mariusz@mondoreale.pl>
2018-10-25 10:33:43 +0200

    Add spaces around the coffee cup and the heart

--- app/src/auth/components/Footer/index.jsx
+++ app/src/auth/components/Footer/index.jsx
@@ -14,9 +14,9 @@ const Footer = ({ className }: Props) => (
     <div className={cx(className, styles.root)}>
         <div className={styles.inner}>
             Made with
-            <span role="img" aria-label="love">❤️</span>
+            <span role="img" aria-label="love"> ❤️ </span>
             &
-            <span role="img" aria-label="coffee">☕️</span>
+            <span role="img" aria-label="coffee"> ☕️ </span>
             by Streamr Network AG in 2018
         </div>
     </div>
