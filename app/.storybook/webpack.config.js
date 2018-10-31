b0ba3dfd73dfe2e997079d774c62c6a126dafd1b
Aapeli <aapeli.haanpuu@streamr.com>
2018-10-31 13:29:20 +0200

    Add MiniCssExtractPlugin

--- app/.storybook/webpack.config.js
+++ app/.storybook/webpack.config.js
@@ -1,18 +1,26 @@
 // For more information refer the docs: https://storybook.js.org/configurations/custom-webpack-config
 
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const isProduction = require('../scripts/isProduction')
+
 process.env.NO_DOTENV = true
 const origConfig = require('../webpack.config')
 
 module.exports = {
-    plugins: [
-        ...origConfig.plugins,
-    ],
     module: {
         rules: [
             ...origConfig.module.rules,
         ],
     },
-    plugins: [],
+    plugins: [
+        // We use MiniCssExtractPlugin.loader in webpack.config.js. The loader requires the plugin to work so added it manually
+        new MiniCssExtractPlugin({
+            // Options similar to the same options in webpackOptions.output
+            // both options are optional
+            filename: isProduction() ? '[name].css' : '[name].[hash].css',
+            chunkFilename: isProduction() ? '[id].css' : '[id].[hash].css',
+        }),
+    ],
     resolve: {
         ...origConfig.resolve,
     }
