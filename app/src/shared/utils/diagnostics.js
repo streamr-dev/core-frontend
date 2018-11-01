514e3fd52dad2133af9cbe3d96759f856de33a3e
Tim Oxley <secoif@gmail.com>
2018-11-01 12:53:06 +0800

    Attach diagnostic function as global.streamr.info(). Don't log by default.

--- app/src/shared/utils/diagnostics.js
+++ app/src/shared/utils/diagnostics.js
@@ -1,10 +1,14 @@
-/* Print diagnostic info to console */
+/* Attach global diagnostic object */
 
 const navigator = global.navigator || {}
-// eslint-disable-next-line no-console
-console.info('Streamr Platform', {
-    userAgent: navigator.userAgent,
-    environment: process.env.NODE_ENV,
-    version: process.env.GIT_VERSION,
-    branch: process.env.GIT_BRANCH,
+
+global.streamr = Object.assign(global.streamr || {}, {
+    info() {
+        return {
+            userAgent: navigator.userAgent,
+            environment: process.env.NODE_ENV,
+            version: process.env.GIT_VERSION,
+            branch: process.env.GIT_BRANCH,
+        }
+    },
 })
