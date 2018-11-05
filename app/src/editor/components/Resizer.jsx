76bff61149bfd3fd4747e8bee56dee849a49e9aa
Tim Oxley <secoif@gmail.com>
2018-11-05 16:27:14 +0800

    [editor Resizer] Cancel resize if Resizer unmounted (e.g. module deleted). Add comments.

--- app/src/editor/components/Resizer.jsx
+++ app/src/editor/components/Resizer.jsx
@@ -18,6 +18,8 @@ export function isModuleResizable(module) {
 }
 
 export class Resizer extends React.Component {
+    attached = false
+
     initialState = {
         diffX: 0,
         diffY: 0,
@@ -29,66 +31,142 @@ export class Resizer extends React.Component {
 
     state = this.initialState
 
+    /**
+     * Start Dragging
+     */
+
     onMouseDown = (event) => {
         if (!this.props.target.current) { return }
         event.stopPropagation()
+        this.attach()
+        this.startDrag(event)
+    }
+
+    /**
+     * Dragging keyboard controls
+     * (esc to cancel)
+     */
+
+    onKeyDown = (event) => {
+        if (event.key === 'Escape') {
+            event.stopPropagation()
+            this.cancel()
+        }
+    }
+
+    /**
+     * End & Reset
+     */
+
+    onDragCancel = (event) => {
+        event.stopPropagation()
+        this.cancel()
+    }
+
+    /**
+     * End & Commit
+     */
+
+    onMouseUp = (event) => {
+        event.stopPropagation()
+        this.detach()
+        this.endDrag()
+        this.commit()
+        this.reset()
+    }
+
+    onDragMove = (event) => {
+        this.updateSize(event)
+    }
+
+    /**
+     * Add global listeners & styles
+     */
+
+    attach() {
+        this.attached = true
         // attach to window to ensure 'release outside' is detected
         window.addEventListener('mouseup', this.onMouseUp)
         window.addEventListener('mousemove', this.onDragMove)
         document.body.addEventListener('keydown', this.onKeyDown)
         document.body.style.cursor = 'nwse-resize' // force resize cursor for duration of drag
         document.body.style.userSelect = 'none' // no text selection while dragging
-
-        this.props.onResizing(true)
-
-        // capture current actual width
-        const rect = this.props.target.current.getBoundingClientRect()
-        this.setState({
-            initX: event.clientX,
-            initY: event.clientY,
-            initWidth: rect.width,
-            initHeight: rect.height,
-        })
     }
 
-    onKeyDown = (event) => {
-        if (event.key === 'Escape') {
-            this.onDragCancel(event)
-        }
-    }
+    /**
+     * Remove global listeners & styles
+     */
 
-    onDragCancel = (event) => {
-        event.stopPropagation()
-        this.props.onResizing(false)
-
-        // reset -> update noop
-        this.setState(this.initialState)
-        this.onMouseUp(event)
-    }
-
-    onMouseUp = (event) => {
-        event.stopPropagation()
+    detach() {
+        if (!this.attached) { return }
+        this.attached = false
         window.removeEventListener('mouseup', this.onMouseUp)
         window.removeEventListener('mousemove', this.onDragMove)
         document.body.removeEventListener('keydown', this.onKeyDown)
+        // restore global styles
         document.body.style.cursor = ''
         document.body.style.userSelect = ''
+    }
 
-        this.props.onResizing(false)
+    /**
+     * Commit calculated size changes to module.
+     */
 
-        // only commit change after cancel
+    commit() {
         if (!this.state.diffX && !this.state.diffY) { return } // noop if no change
         this.props.api.updateModuleSize(this.props.module.hash, {
             width: this.state.initWidth - this.state.diffX,
             height: this.state.initHeight - this.state.diffY,
         })
-
-        this.setState(this.initialState) // reset
     }
 
-    onDragMove = (event) => {
-        this.updateSize(event)
+    /**
+     * Cancel any drag in progress & reset
+     */
+
+    cancel() {
+        this.detach()
+        this.endDrag()
+        this.reset()
     }
+
+    /**
+     * Reset state
+     */
+
+    reset() {
+        this.setState(this.initialState)
+    }
+
+    /**
+     * Capture initial dimensions and click location
+     * Tell parent we're resizing
+     */
+
+    startDrag({ clientX, clientY }) {
+        this.props.onResizing(true)
+        // capture current actual width
+        const rect = this.props.target.current.getBoundingClientRect()
+        this.setState({
+            initX: clientX,
+            initY: clientY,
+            initWidth: rect.width,
+            initHeight: rect.height,
+        })
+    }
+
+    /**
+     * Tell parent we're not resizing
+     * (Mainly for symmetry with startDrag)
+     */
+
+    endDrag() {
+        this.props.onResizing(false)
+    }
+
+    /**
+     * Update temp size state.
+     */
 
     updateSize = ({ clientX, clientY }, done) => {
         this.setState(({ initX, initY }) => ({
@@ -103,6 +181,10 @@ export class Resizer extends React.Component {
                 done()
             }
         })
+    }
+
+    componentWillUnmount() {
+        this.cancel()
     }
 
     render() {
