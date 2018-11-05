20dda13d0a5f39821eb3ae456c699d947609b163
Mariusz Roliński <mariusz@mondoreale.pl>
2018-11-05 06:13:29 +0100

    Extract icon SVGs

--- app/src/shared/components/Calendar/index.jsx
+++ app/src/shared/components/Calendar/index.jsx
@@ -5,28 +5,13 @@ import React from 'react'
 import ReactCalendar, { CalendarProps } from 'react-calendar/dist/entry.nostyle'
 import moment from 'moment'
 
+import Arrow from '../CalendarArrowIcon'
 import styles from './calendar.pcss'
 
 type Props = CalendarProps & {
     initialValue?: Date,
     onChange?: (Date) => void,
 }
-
-const ArrowRight = () => (
-    <svg xmlns="http://www.w3.org/2000/svg" width="19" height="14" className={styles.arrow}>
-        <g strokeWidth="1.5" stroke="#323232" fill="none" fillRule="evenodd" opacity=".5" strokeLinecap="round" strokeLinejoin="round">
-            <path d="M12 1.343L17.657 7 12 12.657M17 7H1" />
-        </g>
-    </svg>
-)
-
-const ArrowLeft = () => (
-    <svg xmlns="http://www.w3.org/2000/svg" width="19" height="14" className={styles.arrow}>
-        <g strokeWidth="1.5" stroke="#323232" fill="none" fillRule="evenodd" opacity=".5" strokeLinecap="round" strokeLinejoin="round">
-            <path d="M7 1.343L1.343 7 7 12.657M2 7h16" />
-        </g>
-    </svg>
-)
 
 const formatShortWeekday = (date: Date) => moment(date).format('dd')
 
@@ -38,8 +23,8 @@ const Calendar = (props: Props) => (
         minDetail="decade"
         formatShortWeekday={formatShortWeekday}
         formatMonth={formatMonth}
-        prevLabel={<ArrowLeft />}
-        nextLabel={<ArrowRight />}
+        prevLabel={<Arrow left />}
+        nextLabel={<Arrow />}
         {...props}
         next2Label={null}
         prev2Label={null}
