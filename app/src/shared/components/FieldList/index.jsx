72eaaf4f3adc3d6b58486e8c75e3fe8371299f94
Mariusz Roliński <mariusz@mondoreale.pl>
2018-11-20 13:43:00 +0100

    Fix hover state when dragging

--- app/src/shared/components/FieldList/index.jsx
+++ app/src/shared/components/FieldList/index.jsx
@@ -1,25 +1,71 @@
 // @flow
 
 import * as React from 'react'
+import cx from 'classnames'
 
 import SortableList from '../SortableList'
 import FieldItem from './FieldItem'
-import styles from './fieldList.pcss'
 
 type Props = {
     className?: string,
     children: React.Node,
+    onSortStart?: (Object) => void,
+    onSortEnd?: (Object) => void,
 }
 
-const FieldList = ({ children, className, ...props }: Props) => (
-    <SortableList
-        className={styles.root}
-        helperClass={FieldItem.styles.helper}
-        lockAxis="y"
-        {...props}
-    >
-        {children}
-    </SortableList>
-)
+type State = {
+    locked: boolean,
+}
+
+class FieldList extends React.Component<Props, State> {
+    state = {
+        locked: false,
+    }
+
+    onSortStart = (...args: any) => {
+        const { onSortStart } = this.props
+
+        this.setState({
+            locked: true,
+        })
+
+        if (onSortStart) {
+            onSortStart(...args)
+        }
+    }
+
+    onSortEnd = (...args: any) => {
+        const { onSortEnd } = this.props
+
+        this.setState({
+            locked: false,
+        })
+
+        if (onSortEnd) {
+            onSortEnd(...args)
+        }
+    }
+
+    render() {
+        const { children, className, ...props } = this.props
+        const { locked } = this.state
+
+        return (
+            <SortableList
+                className={cx({
+                    [FieldItem.styles.locked]: locked,
+                })}
+                helperClass={FieldItem.styles.helper}
+                lockAxis="y"
+                {...props}
+                onSortStart={this.onSortStart}
+                onSortEnd={this.onSortEnd}
+                useDragHandle
+            >
+                {children}
+            </SortableList>
+        )
+    }
+}
 
 export default FieldList
