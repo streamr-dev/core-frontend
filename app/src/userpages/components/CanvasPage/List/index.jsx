upperwal/streamr-platform

Add search and filtering feature to the Canvas listing

app/src/userpages/components/CanvasPage/List/index.jsx
@@ -10,10 +10,13 @@ import copy from 'copy-to-clipboard'
 import { Translate, I18n } from 'react-redux-i18n'
 import { Helmet } from 'react-helmet'
 
+import type { Filter } from '../../../flowtype/common-types'
+import type { Canvas } from '../../../flowtype/canvas-types'
+
 import Layout from '../../Layout'
 import links from '../../../../links'
-import { getCanvases, deleteCanvas } from '../../../modules/canvas/actions'
-import { selectCanvases } from '../../../modules/canvas/selectors'
+import { getCanvases, getCanvasesDebounced, deleteCanvas, updateFilter } from '../../../modules/canvas/actions'
+import { selectCanvases, selectFilter } from '../../../modules/canvas/selectors'
 import { defaultColumns } from '../../../utils/constants'
 import Tile from '$shared/components/Tile'
 import DropdownActions from '$shared/components/DropdownActions'
@@ -22,13 +25,19 @@ import EmptyState from '$shared/components/EmptyState'
 import emptyStateIcon from '$shared/assets/images/empty_state_icon.png'
 import emptyStateIcon2x from '$shared/assets/images/empty_state_icon@2x.png'
 
+import FilterSelector from '$mp/components/ActionBar/FilterSelector'
+import FilterDropdownItem from '$mp/components/ActionBar/FilterDropdownItem'
+
 export type StateProps = {
-    canvases: any,
+    canvases: Array<Canvas>,
+    filter: ?Filter,
 }
 
 export type DispatchProps = {
     getCanvases: () => void,
+    getCanvasesDebounced: () => void,
     deleteCanvas: (id: string) => void,
+    updateFilter: (filter: Filter) => void,
     navigate: (to: string) => void,
     copyToClipboard: (text: string) => void,
 }
@@ -41,6 +50,42 @@ const CreateCanvasButton = () => (
             <Translate value="userpages.canvases.createCanvas" />
         </Link>
     </Button>
+)
+
+const searchComponent = (currentValue, onChange: (e: SyntheticInputEvent<EventTarget>) => void) => (
+    <input value={currentValue || ''} onChange={onChange} />
+)
+
+const sortOptions = [
+    {
+        id: 'recent',
+        apiName: 'lastUpdated',
+        displayName: 'Recent',
+    },
+    {
+        id: 'state',
+        apiName: 'state',
+        displayName: 'State',
+    },
+]
+
+const sortDropdownComponent = (currentSelection, onChange: Function) => (
+    <FilterSelector
+        title="Sort by"
+        selected={currentSelection}
+        onClear={() => onChange(null)}
+    >
+        {!!sortOptions && sortOptions.map((s) => (
+            <FilterDropdownItem
+                key={s.id}
+                value={s.id}
+                selected={s.id === currentSelection}
+                onSelect={onChange}
+            >
+                {s.displayName}
+            </FilterDropdownItem>
+        ))}
+    </FilterSelector>
 )
 
 class CanvasList extends Component<Props> {
@@ -77,12 +122,42 @@ class CanvasList extends Component<Props> {
         )
     }
 
+    onSearchChange = (e: SyntheticInputEvent<EventTarget>) => {
+        const { filter, updateFilter, getCanvasesDebounced } = this.props
+        const newFilter = {
+            ...filter,
+            search: e.target.value,
+        }
+        updateFilter(newFilter)
+        getCanvasesDebounced()
+    }
+
+    onSortChange = (sortOption) => {
+        const { filter, updateFilter, getCanvases } = this.props
+        const results = sortOptions.filter((s) => s.id === sortOption)
+        const apiField = (results && results.length > 0 && results[0].apiName) || null
+
+        const newFilter = {
+            ...filter,
+            sortBy: apiField,
+        }
+        updateFilter(newFilter)
+        getCanvases()
+    }
+
+    mapSortBy = (apiSortBy: ?string) => {
+        const filtered = sortOptions.filter((s) => s.apiName === apiSortBy)
+        return (filtered && filtered.length > 0 && filtered[0].displayName) || null
+    }
+
     render() {
-        const { canvases } = this.props
+        const { canvases, filter } = this.props
 
         return (
             <Layout
                 headerAdditionalComponent={<CreateCanvasButton />}
+                headerSearchComponent={searchComponent(filter && filter.search, this.onSearchChange)}
+                headerFilterComponent={sortDropdownComponent(this.mapSortBy(filter && filter.sortBy), this.onSortChange)}
             >
                 <Container>
                     <Helmet>
@@ -124,11 +199,14 @@ class CanvasList extends Component<Props> {
 
 export const mapStateToProps = (state: any): StateProps => ({
     canvases: selectCanvases(state),
+    filter: selectFilter(state),
 })
 
 export const mapDispatchToProps = (dispatch: Function): DispatchProps => ({
     getCanvases: () => dispatch(getCanvases()),
+    getCanvasesDebounced: () => dispatch(getCanvasesDebounced()),
     deleteCanvas: (id) => dispatch(deleteCanvas(id)),
+    updateFilter: (filter) => dispatch(updateFilter(filter)),
     navigate: (to) => dispatch(push(to)),
     copyToClipboard: (text) => copy(text),
 })

streamr-dev/core-frontend

app/src/userpages/components/CanvasPage/List/index.jsx
@@ -10,10 +10,13 @@ import copy from 'copy-to-clipboard'
 import { Translate, I18n } from 'react-redux-i18n'
 import { Helmet } from 'react-helmet'
 
+import type { Filter } from '../../../flowtype/common-types'
+import type { Canvas } from '../../../flowtype/canvas-types'
+
 import Layout from '../../Layout'
 import links from '../../../../links'
-import { getCanvases, deleteCanvas } from '../../../modules/canvas/actions'
-import { selectCanvases } from '../../../modules/canvas/selectors'
+import { getCanvases, getCanvasesDebounced, deleteCanvas, updateFilter } from '../../../modules/canvas/actions'
+import { selectCanvases, selectFilter } from '../../../modules/canvas/selectors'
 import { defaultColumns } from '../../../utils/constants'
 import Tile from '$shared/components/Tile'
 import DropdownActions from '$shared/components/DropdownActions'
@@ -22,13 +25,19 @@ import EmptyState from '$shared/components/EmptyState'
 import emptyStateIcon from '$shared/assets/images/empty_state_icon.png'
 import emptyStateIcon2x from '$shared/assets/images/empty_state_icon@2x.png'
 
+import FilterSelector from '$mp/components/ActionBar/FilterSelector'
+import FilterDropdownItem from '$mp/components/ActionBar/FilterDropdownItem'
+
 export type StateProps = {
-    canvases: any,
+    canvases: Array<Canvas>,
+    filter: ?Filter,
 }
 
 export type DispatchProps = {
     getCanvases: () => void,
+    getCanvasesDebounced: () => void,
     deleteCanvas: (id: string) => void,
+    updateFilter: (filter: Filter) => void,
     navigate: (to: string) => void,
     copyToClipboard: (text: string) => void,
 }
@@ -41,6 +50,42 @@ const CreateCanvasButton = () => (
             <Translate value="userpages.canvases.createCanvas" />
         </Link>
     </Button>
+)
+
+const searchComponent = (currentValue, onChange: (e: SyntheticInputEvent<EventTarget>) => void) => (
+    <input value={currentValue || ''} onChange={onChange} />
+)
+
+const sortOptions = [
+    {
+        id: 'recent',
+        apiName: 'lastUpdated',
+        displayName: 'Recent',
+    },
+    {
+        id: 'state',
+        apiName: 'state',
+        displayName: 'State',
+    },
+]
+
+const sortDropdownComponent = (currentSelection, onChange: Function) => (
+    <FilterSelector
+        title="Sort by"
+        selected={currentSelection}
+        onClear={() => onChange(null)}
+    >
+        {!!sortOptions && sortOptions.map((s) => (
+            <FilterDropdownItem
+                key={s.id}
+                value={s.id}
+                selected={s.id === currentSelection}
+                onSelect={onChange}
+            >
+                {s.displayName}
+            </FilterDropdownItem>
+        ))}
+    </FilterSelector>
 )
 
 class CanvasList extends Component<Props> {
@@ -77,12 +122,46 @@ class CanvasList extends Component<Props> {
         )
     }
 
+    onSearchChange = (e: SyntheticInputEvent<EventTarget>) => {
+        const { filter, updateFilter, getCanvasesDebounced } = this.props
+        const newFilter = {
+            ...filter,
+            search: e.target.value,
+        }
+        updateFilter(newFilter)
+        getCanvasesDebounced()
+    }
+
+    onSortChange = (sortOption) => {
+        const { filter, updateFilter, getCanvases } = this.props
+        const apiField = this.mapSortByFromIdToApi(sortOption)
+
+        const newFilter = {
+            ...filter,
+            sortBy: apiField,
+        }
+        updateFilter(newFilter)
+        getCanvases()
+    }
+
+    mapSortByFromIdToApi = (id: ?string) => {
+        const filtered = sortOptions.filter((s) => s.id === id)
+        return (filtered && filtered.length > 0 && filtered[0].apiName) || null
+    }
+
+    mapSortByFromApiToDisplayName = (apiSortBy: ?string) => {
+        const filtered = sortOptions.filter((s) => s.apiName === apiSortBy)
+        return (filtered && filtered.length > 0 && filtered[0].displayName) || null
+    }
+
     render() {
-        const { canvases } = this.props
+        const { canvases, filter } = this.props
 
         return (
             <Layout
                 headerAdditionalComponent={<CreateCanvasButton />}
+                headerSearchComponent={searchComponent(filter && filter.search, this.onSearchChange)}
+                headerFilterComponent={sortDropdownComponent(this.mapSortByFromApiToDisplayName(filter && filter.sortBy), this.onSortChange)}
             >
                 <Container>
                     <Helmet>
@@ -124,11 +203,14 @@ class CanvasList extends Component<Props> {
 
 export const mapStateToProps = (state: any): StateProps => ({
     canvases: selectCanvases(state),
+    filter: selectFilter(state),
 })
 
 export const mapDispatchToProps = (dispatch: Function): DispatchProps => ({
     getCanvases: () => dispatch(getCanvases()),
+    getCanvasesDebounced: () => dispatch(getCanvasesDebounced()),
     deleteCanvas: (id) => dispatch(deleteCanvas(id)),
+    updateFilter: (filter) => dispatch(updateFilter(filter)),
     navigate: (to) => dispatch(push(to)),
     copyToClipboard: (text) => copy(text),
 })