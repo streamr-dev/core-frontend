8cb7304fb1a8da1092587987be872ce0b57b5d2a
Tim Oxley <secoif@gmail.com>
2018-11-20 14:19:38 +0800

    [editor Port] Factor out PortParam control.

--- app/src/editor/components/Port.jsx
+++ app/src/editor/components/Port.jsx
@@ -133,62 +133,57 @@ class Port extends React.PureComponent {
         }
 
         if (isParam) {
-            const portSize = this.props.size + 2 // add some padding
             /* add input for params */
             portContent.push((
                 <div key={`${port.id}.value`} className={cx(styles.portValueContainer)} role="gridcell">
-                    {port.possibleValues ? (
-                        /* Select */
-                        <select
-                            className={styles.portValue}
-                            value={this.state.value}
-                            onChange={this.onChange}
-                            disabled={!!port.connected || !!isRunning}
-                            onMouseOver={() => this.props.setIsDraggable(false)}
-                            onMouseOut={() => this.props.setIsDraggable(true)}
-                            onBlur={this.onBlur}
-                            onFocus={this.onFocus}
-                            style={{
-                                // setting minWidth allows size transition
-                                minWidth: `${portSize}ch`,
-                            }}
-                        >
-                            {port.possibleValues.map(({ name, value }) => (
-                                <option key={value} value={value}>{name}</option>
-                            ))}
-                        </select>
-                    ) : (
-                        <input
-                            className={styles.portValue}
-                            value={this.state.value}
-                            disabled={!!port.connected || !!isRunning}
-                            onChange={this.onChange}
-                            size={portSize}
-                            style={{
-                                // setting minWidth allows size transition
-                                minWidth: `${portSize}ch`,
-                            }}
-                            onBlur={this.onBlur}
-                            onFocus={this.onFocus}
-                            onMouseOver={() => this.props.setIsDraggable(false)}
-                            onMouseOut={() => this.props.setIsDraggable(true)}
-                        />
-                    )}
+                    <PortParam
+                        className={styles.portValue}
+                        port={port}
+                        size={this.props.size}
+                        value={this.state.value}
+                        onChange={this.onChange}
+                        disabled={!!port.connected || !!isRunning}
+                        onMouseOver={() => this.props.setIsDraggable(false)}
+                        onMouseOut={() => this.props.setIsDraggable(true)}
+                        onBlur={this.onBlur}
+                        onFocus={this.onFocus}
+                    />
                 </div>
             ))
         } else if (isInput) {
             /* placeholder div for consistent icon vertical alignment */
             portContent.push((
                 <div key={`${port.id}.value`} className={cx(styles.portValueContainer)} role="gridcell">
-                    <div
-                        className={styles.portValue}
-                    />
+                    <div className={styles.portValue} />
                 </div>
             ))
         }
 
         return portContent
     }
+}
+
+function PortParam({ port, size, ...props }) {
+    const portSize = size + 2 // add some padding
+
+    const style = {
+        minWidth: `${portSize}ch`, // setting minWidth allows size transition
+    }
+
+    /* Select */
+    if (port.possibleValues) {
+        return (
+            <select {...props} style={style}>
+                {port.possibleValues.map(({ name, value }) => (
+                    <option key={value} value={value}>{name}</option>
+                ))}
+            </select>
+        )
+    }
+
+    return (
+        <input {...props} style={style} />
+    )
 }
 
 const PortDrag = DragSource(DragTypes.Port)
