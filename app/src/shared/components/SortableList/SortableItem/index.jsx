0498f29b0de1beae162c10d62bede7d7b97a767c
Mariusz Roliński <mariusz@mondoreale.pl>
2018-11-22 10:55:51 +0100

    SortableItem: forward props

--- app/src/shared/components/SortableList/SortableItem/index.jsx
+++ app/src/shared/components/SortableList/SortableItem/index.jsx
@@ -1,16 +1,18 @@
 // @flow
 
 import * as React from 'react'
+import cx from 'classnames'
 import { SortableElement } from 'react-sortable-hoc'
 
 import styles from './sortableItem.pcss'
 
 type Props = {
     children: React.Node,
+    className?: string,
 }
 
-const SortableItem = ({ children }: Props) => (
-    <div className={styles.root}>
+const SortableItem = ({ children, className, ...props }: Props) => (
+    <div className={cx(styles.root, className)} {...props}>
         {children}
     </div>
 )
