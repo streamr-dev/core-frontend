c3f2ee3e26023683ae6d204f0044259ec31c876d
Mariusz Roliński <mariusz@mondoreale.pl>
2018-12-09 18:26:05 +0100

    Make Modal trigger an exception if rendered outside of ModalRoot

--- app/src/shared/components/Modal/index.jsx
+++ app/src/shared/components/Modal/index.jsx
@@ -4,6 +4,7 @@ import React, { type Node } from 'react'
 import { createPortal } from 'react-dom'
 
 import Context from '$shared/contexts/Modal'
+import NoModalRootError from '$shared/errors/NoModalRootError'
 
 type Props = {
     children: Node,
@@ -13,7 +14,11 @@ class Modal extends React.Component<Props> {
     static contextType = Context
 
     render() {
-        return createPortal(this.props.children, this.context.root)
+        const { root } = this.context
+        if (!root) {
+            throw new NoModalRootError()
+        }
+        return createPortal(this.props.children, root)
     }
 }
 
