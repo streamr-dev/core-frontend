549fb242c55500c884d84e882519fb9469e01dd2
Mariusz Roliński <mariusz@mondoreale.pl>
2018-12-09 19:48:13 +0100

    Add AutoScroll
    AutoScroll scrolls to the top of the page when the
    route changes. It utilizes the (also) new RouteWatcher
    component.
    
    The plan here is to replace ModalManager completely
    in the near future.

--- app/src/shared/components/BodyClass/index.jsx
+++ app/src/shared/components/BodyClass/index.jsx
@@ -10,6 +10,13 @@ export const NO_SCROLL = 'overflow-hidden'
 export const PAGE_SECONDARY = 'page-secondary'
 
 class BodyClass extends React.Component<Props> {
+    static includes(className: string): ?boolean {
+        if (!document.body) {
+            return null
+        }
+        return document.body.classList.contains(className)
+    }
+
     componentDidMount() {
         if (document.body) {
             document.body.classList.add(...this.props.className.split(/\s+/))
