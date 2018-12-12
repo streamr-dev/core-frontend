882a1c67370b37581d20402278c1a66a79b31a85
Tuomas Koponen <tuomas.koponen@streamr.com>
2018-12-12 15:40:34 +0200

    Modify BodyClass to support server side rendering

--- app/src/shared/components/BodyClass/index.jsx
+++ app/src/shared/components/BodyClass/index.jsx
@@ -1,6 +1,7 @@
 // @flow
 
 import React from 'react'
+import { Helmet } from 'react-helmet'
 
 type Props = {
     className: string,
@@ -10,19 +11,33 @@ export const NO_SCROLL = 'overflow-hidden'
 export const PAGE_SECONDARY = 'page-secondary'
 
 class BodyClass extends React.Component<Props> {
-    componentDidMount() {
-        if (document.body) {
-            document.body.classList.add(...this.props.className.split(/\s+/))
+    static classList: Array<string> = []
+
+    componentWillMount() {
+        const { className } = this.props
+        const { classList } = BodyClass
+
+        if (classList.indexOf(className) === -1) {
+            classList.push(className)
         }
     }
 
     componentWillUnmount() {
-        if (document.body) {
-            document.body.classList.remove(...this.props.className.split(/\s+/))
+        const { className } = this.props
+        const { classList } = BodyClass
+
+        if (classList.indexOf(className) !== -1) {
+            classList.splice(classList.indexOf(className), 1)
         }
     }
 
-    render = () => null
+    render = () => (
+        <Helmet
+            bodyAttributes={{
+                class: BodyClass.classList.join(' '),
+            }}
+        />
+    )
 }
 
 export default BodyClass
