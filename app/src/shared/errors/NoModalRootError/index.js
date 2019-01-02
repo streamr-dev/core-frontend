7b4f2d99bf8793776bf1427dea32a2654f76cd31
Mariusz Roliński <mariusz@mondoreale.pl>
2019-01-02 11:45:46 +0100

    Update NoModalRootError message

--- app/src/shared/errors/NoModalRootError/index.js
+++ app/src/shared/errors/NoModalRootError/index.js
@@ -4,7 +4,7 @@ export default class NoModalRootError extends Error {
     __proto__: any
 
     constructor() {
-        super('Root element for modals is missing. Forgot to mount ModalRoot?')
+        super('Root element for modals is missing.')
         this.__proto__ = NoModalRootError.prototype // eslint-disable-line no-proto
     }
 }
