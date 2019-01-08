6e071ef92d872b587a9da21dc0fbb88c69fea153
Mariusz Roliński <mariusz@mondoreale.pl>
2019-01-08 10:43:06 +0100

    Ensure unique Notification IDs

--- app/test/unit/utils/Notification.test.js
+++ app/test/unit/utils/Notification.test.js
@@ -2,32 +2,44 @@ import sinon from 'sinon'
 import Notification from '$shared/utils/Notification'
 
 describe(Notification, () => {
-    describe('push/subscribe/unsubscribe', () => {
-        it('emits PUSH event with a Notification instance as an argument', () => {
-            const handler = sinon.spy()
-            Notification.subscribe(handler)
-            Notification.push({
-                title: 'Title',
-            })
-            Notification.unsubscribe(handler)
-            Notification.push({
-                title: 'Title 2',
-            })
-            sinon.assert.calledOnce(handler)
-            sinon.assert.calledWith(handler, sinon.match.instanceOf(Notification))
-            sinon.assert.calledWith(handler, sinon.match.has('title', 'Title'))
+    describe('id', () => {
+        let clock
+
+        beforeEach(() => {
+            clock = sinon.useFakeTimers(new Date())
+        })
+
+        afterEach(() => {
+            clock.restore()
+        })
+
+        it('generates unique IDs', () => {
+            expect(new Notification({}).id).not.toEqual(new Notification({}).id)
         })
     })
 
-    describe('autoDismissAfter', () => {
-        it('is 0 if isTx is true', () => {
-            expect(new Notification({}).autoDismissAfter).toBeGreaterThan(0)
-            expect(new Notification({
-                autoDismiss: true,
-            }).autoDismissAfter).toBeGreaterThan(0)
-            expect(new Notification({
-                autoDismiss: false,
-            }).autoDismissAfter).toEqual(0)
+    it('subscribes to PUSH events; emits PUSH event; unsubscribes from PUSH event', () => {
+        const handler = sinon.spy()
+        Notification.subscribe(handler)
+        Notification.push({
+            title: 'Title',
         })
+        Notification.unsubscribe(handler)
+        Notification.push({
+            title: 'Title 2',
+        })
+        sinon.assert.calledOnce(handler)
+        sinon.assert.calledWith(handler, sinon.match.instanceOf(Notification))
+        sinon.assert.calledWith(handler, sinon.match.has('title', 'Title'))
+    })
+
+    it('determines correct autoDismissAfter based on the autoDismiss flag parameter', () => {
+        expect(new Notification({}).autoDismissAfter).toBeGreaterThan(0)
+        expect(new Notification({
+            autoDismiss: true,
+        }).autoDismissAfter).toBeGreaterThan(0)
+        expect(new Notification({
+            autoDismiss: false,
+        }).autoDismissAfter).toEqual(0)
     })
 })
