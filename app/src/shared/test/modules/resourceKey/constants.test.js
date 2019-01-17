bf096007660125996dabe8c2ec37472ce53a9c60
Juha Haavisto <juha.haavisto@sujuwa.fi>
2019-01-17 09:30:33 +0200

    Fix shared modules constant namespace

--- app/src/shared/test/modules/resourceKey/constants.test.js
+++ app/src/shared/test/modules/resourceKey/constants.test.js
@@ -3,7 +3,7 @@ import * as constants from '$shared/modules/resourceKey/constants'
 describe('resourceKey - constants', () => {
     it('is namespaced correctly', () => {
         Object.keys(constants).forEach((key) => {
-            expect(constants[key]).toEqual(expect.stringMatching(/^marketplace\/resourceKey\//))
+            expect(constants[key]).toEqual(expect.stringMatching(/^shared\/resourceKey\//))
         })
     })
 })
