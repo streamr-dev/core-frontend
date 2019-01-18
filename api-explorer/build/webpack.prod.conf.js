ffa1a63b48c022926e029804daee8408ced376a0
Matthew <matthew.fontana@streamr.com>
2019-01-18 11:43:36 +0200

    remove comment

--- api-explorer/build/webpack.prod.conf.js
+++ api-explorer/build/webpack.prod.conf.js
@@ -54,10 +54,6 @@ const webpackConfig = merge(baseWebpackConfig, {
         safe: true
       }
     }),
-
-    // generate dist index.html with correct asset hash for caching.
-    // you can customize output by editing /index.html
-    // see https://github.com/ampedandwired/html-webpack-plugin
     new HtmlWebpackPlugin({
       filename: config.build.index,
       template: 'index.html',
