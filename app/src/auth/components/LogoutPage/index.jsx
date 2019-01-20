b30560983c9baced07e32e5b0a33677091b6ad69
Mariusz Roliński <mariusz@mondoreale.pl>
2019-01-20 20:00:02 +0100

    Hit the backend on logout

--- app/src/auth/components/LogoutPage/index.jsx
+++ app/src/auth/components/LogoutPage/index.jsx
@@ -1,8 +1,12 @@
 // @flow
 
 import React from 'react'
+import axios from 'axios'
 
+import ErrorPageView from '$mp/components/ErrorPageView'
 import { type Props as SessionProps } from '$auth/contexts/Session'
+import { type ErrorInUi } from '$shared/flowtype/common-types'
+import $routes from '$routes'
 
 export type DispatchProps = {
     logout: () => void,
@@ -11,18 +15,36 @@ export type DispatchProps = {
 type Props = DispatchProps & SessionProps & {
 }
 
-class LogoutPage extends React.Component<Props> {
+type State = {
+    error: ?ErrorInUi,
+}
+
+class LogoutPage extends React.Component<Props, State> {
+    state = {
+        error: null,
+    }
+
     componentDidMount() {
         const { logout, setSessionToken } = this.props
 
-        if (setSessionToken) {
-            logout()
-            setSessionToken(null)
-        }
+        axios
+            .get($routes.externalLogout())
+            .then(() => {
+                if (setSessionToken) {
+                    logout()
+                    setSessionToken(null)
+                }
+            }, (error) => {
+                this.setState({
+                    error,
+                })
+            })
     }
 
     render() {
-        return null
+        return !!this.state.error && (
+            <ErrorPageView />
+        )
     }
 }
 
