8343bb15937a0476fcfde8bba0fb37c5feed552b
Juha Haavisto <juha.haavisto@sujuwa.fi>
2019-01-31 11:10:41 +0200

    Let Modal component do the mounting

--- app/src/shared/utils/confirm.jsx
+++ app/src/shared/utils/confirm.jsx
@@ -5,37 +5,21 @@ import { render, unmountComponentAtNode } from 'react-dom'
 
 import ConfirmDialog, { type Properties } from '$shared/components/ConfirmDialog'
 
-const removeElementReconfirm = () => {
-    const target = document.getElementById('shared-confirm-alert')
-
-    if (target) {
-        unmountComponentAtNode(target)
-
-        if (target.parentNode) {
-            target.parentNode.removeChild(target)
-        }
-    }
-}
-
 const confirmDialog = (props: Properties): Promise<boolean> => new Promise((resolve: Function) => {
+    // Modal component takes care of mounting the dialog, just render dummy component
     const divTarget = document.createElement('div')
-    divTarget.id = 'shared-confirm-alert'
 
-    if (document.body) {
-        document.body.appendChild(divTarget)
-
-        render(<ConfirmDialog
-            onAccept={() => {
-                removeElementReconfirm()
-                resolve(true)
-            }}
-            onReject={() => {
-                removeElementReconfirm()
-                resolve(false)
-            }}
-            {...props}
-        />, divTarget)
-    }
+    render(<ConfirmDialog
+        onAccept={() => {
+            unmountComponentAtNode(divTarget)
+            resolve(true)
+        }}
+        onReject={() => {
+            unmountComponentAtNode(divTarget)
+            resolve(false)
+        }}
+        {...props}
+    />, divTarget)
 })
 
 export default confirmDialog
