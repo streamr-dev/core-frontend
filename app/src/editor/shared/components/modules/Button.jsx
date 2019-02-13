a32ae7eff1cabdeda38642e93eb7012409f0e75f
Tim Oxley <secoif@gmail.com>
2019-02-13 13:08:13 +0800

    [editor modules/Button] Read button value from param. Reset state on stop.

--- app/src/editor/shared/components/modules/Button.jsx
+++ app/src/editor/shared/components/modules/Button.jsx
@@ -6,11 +6,16 @@ import ModuleSubscription from '../ModuleSubscription'
 import ButtonStyles from '$shared/components/Button/button.pcss'
 import styles from './Button.pcss'
 
+function getModuleButtonName(module) {
+    const param = module.params.find((p) => p.name === 'buttonName')
+    return param.value || param.defaultValue
+}
+
 export default class ButtonModule extends React.Component {
     subscription = React.createRef()
 
     state = {
-        value: 'Button',
+        value: undefined,
     }
 
     onLoad = async ({ state }) => {
@@ -35,6 +40,24 @@ export default class ButtonModule extends React.Component {
         })
     }
 
+    getValue = () => {
+        let { value } = this.state
+        if (value == null) {
+            // use module value unless state set
+            value = getModuleButtonName(this.props.module)
+        }
+
+        return value
+    }
+
+    onActiveChange = (isActive) => {
+        if (!isActive) {
+            this.setState({
+                value: undefined,
+            })
+        }
+    }
+
     render() {
         return (
             <div className={cx(this.props.className, styles.Button)}>
@@ -43,10 +66,11 @@ export default class ButtonModule extends React.Component {
                     onLoad={this.onLoad}
                     loadOptions={{ type: 'getState' }}
                     onMessage={this.onMessage}
+                    onActiveChange={this.onActiveChange}
                     ref={this.subscription}
                 />
                 <button className={cx(styles.button, ButtonStyles.btn, ButtonStyles.btnPrimary)} onClick={this.onClick}>
-                    {this.state.value}
+                    {this.getValue()}
                 </button>
             </div>
         )
