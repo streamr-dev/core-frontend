3602f8e97071f47a5984f790ceb25ad537f3fdc1
Matthew <mr.matto@gmail.com>
2019-02-21 17:06:24 +0200

    getting started content
    minor tweaks of documentation + boilerplate engine content

--- app/src/docs/components/StreamrEnginePage/index.jsx
+++ app/src/docs/components/StreamrEnginePage/index.jsx
@@ -5,13 +5,8 @@ import React from 'react'
 import DocsLayout from '../DocsLayout'
 import StreamrEnginePageContent from './Content.mdx'
 
-const subNav = {
-    'streamr-tech-stack': 'Streamr Tech Stack',
-    'realtime-engine': 'Realtime Engine',
-}
-
 const StreamrEnginePage = () => (
-    <DocsLayout subNav={subNav}>
+    <DocsLayout >
         <StreamrEnginePageContent />
     </DocsLayout>
 )
