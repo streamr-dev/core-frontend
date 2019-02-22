3f388a3551ade77c199de9b540357478c23bba3d
Matthew <fonty1@users.noreply.github.com>
2019-02-22 16:21:26 +0200

    including iframe and inline code formatting, API page content. (#251)

--- app/src/docs/components/ApiPage/index.jsx
+++ app/src/docs/components/ApiPage/index.jsx
@@ -6,8 +6,11 @@ import DocsLayout from '../DocsLayout'
 import ApiPageContent from './Content.mdx'
 
 const subNav = {
-    clientLibraries: 'Client Libraries',
+    'introduction-to-streamr-apis': 'Introduction to Streamr APIs',
     authentication: 'Authentication',
+    'data-input': 'Data Input',
+    'data-output': 'Data Output',
+    'api-explorer': 'API Explorer',
 }
 
 const ApiPage = () => (
