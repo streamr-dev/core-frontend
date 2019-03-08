13bf721770b4dd88a083beec15cd483bc604ee1a
Tim Oxley <secoif@gmail.com>
2019-03-08 17:51:48 +0800

    [CI] Split building of docker env into own file. Run editor API tests on npm test.

---
+++ app/travis_scripts/build-docker-env.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+(
+    cd $TRAVIS_BUILD_DIR
+    sudo /etc/init.d/mysql stop
+    sudo sysctl fs.inotify.max_user_watches=524288; sudo sysctl -p
+    sudo ifconfig docker0 10.200.10.1/24
+    git clone https://github.com/streamr-dev/streamr-docker-dev.git
+    streamr-docker-dev/streamr-docker-dev/bin.sh start 5
+    while true; do http_code=$(curl -s -o /dev/null -I -w "%{http_code}" http://localhost:8081/streamr-core/login/auth); if [ $http_code = 200 ]; then echo "EE up and running"; break; else echo "EE not receiving connections"; sleep 5s; fi; done
+    streamr-docker-dev/streamr-docker-dev/bin.sh restart data-api
+    while true; do http_code=$(curl -s -o /dev/null -I -w "%{http_code}" http://localhost:8890/); if [ $http_code = 404 ]; then echo "Data-api up and running"; break; else echo "Data API not receiving connections"; sleep 5s; fi; done
+    sleep 60
+)
+
+npm run test-local || (
+    docker logs streamr_dev_data-api --tail 500
+    docker logs streamr_dev_engine-and-editor --tail 500
+)
+#npm test
