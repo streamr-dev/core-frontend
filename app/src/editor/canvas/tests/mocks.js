521b02bfd175223dcdefb9e2369bc88452444861
Juha Haavisto <juha.haavisto@sujuwa.fi>
2019-03-12 11:32:00 +0200

    Update port connections when updating canvas

--- app/src/editor/canvas/tests/mocks.js
+++ app/src/editor/canvas/tests/mocks.js
@@ -1,18 +1,33 @@
 import uuid from 'uuid'
 
-function remapIds(items) {
-    return items.map((item) => ({
-        ...item,
-        id: uuid(),
-    }))
+function remapIds(items, index = {}) {
+    return items.map((item) => {
+        if (!index[item.id]) {
+            index[item.id] = uuid()
+        }
+
+        return {
+            ...item,
+            id: index[item.id],
+        }
+    })
+}
+
+function remapModule(data, index = {}) {
+    const newData = { ...data }
+    if (newData.inputs) { newData.inputs = remapIds(newData.inputs, index) }
+    if (newData.outputs) { newData.outputs = remapIds(newData.outputs, index) }
+    if (newData.params) { newData.params = remapIds(newData.params, index) }
+    return newData
 }
 
 function createMockModuleFactory(data) {
     return () => {
-        const newData = { ...data }
-        if (newData.inputs) { newData.inputs = remapIds(newData.inputs) }
-        if (newData.outputs) { newData.outputs = remapIds(newData.outputs) }
-        if (newData.params) { newData.params = remapIds(newData.params) }
+        const index = {}
+        const newData = remapModule(data, index)
+        if (newData.modules) {
+            newData.modules = newData.modules.map((m) => remapModule(m, index))
+        }
         return newData
     }
 }
@@ -179,4 +194,578 @@ export const Clock = createMockModuleFactory({
     name: 'Clock',
     canClearState: true,
     canRefresh: false,
+})
+
+export const Canvas = createMockModuleFactory({
+    params: [
+        {
+            id: 'ep_D9GOWidPQC6czKHFb8zhEw',
+            name: 'canvas',
+            longName: 'Canvas.canvas',
+            type: 'String',
+            connected: false,
+            canConnect: false,
+            export: false,
+            drivingInput: false,
+            canToggleDrivingInput: true,
+            acceptedTypes: [
+                'String',
+            ],
+            requiresConnection: false,
+            defaultValue: null,
+            value: null,
+            updateOnChange: true,
+            isTextArea: false,
+            possibleValues: [
+                {
+                    value: null,
+                    name: 'Select >>',
+                },
+                {
+                    value: 'x7vAq9cjRvavWJj8LVg0CwTUIoOhuSSPirPQcOa_RZOg',
+                    name: 'first',
+                },
+                {
+                    value: 'ajuyRmXFSumhychAIeJ47wGn3ueg32TYaOPG047Aletw',
+                    name: 'second',
+                },
+            ],
+        },
+    ],
+    inputs: [],
+    outputs: [],
+    id: 81,
+    jsModule: 'CanvasModule',
+    type: 'module',
+    name: 'Canvas',
+    canClearState: true,
+    canRefresh: true,
+    uiChannel: {
+        webcomponent: null,
+        name: 'Notifications',
+        id: 'FfZOECs6REuJtsvNgWp4uw',
+    },
+    options: {
+        uiResendAll: {
+            value: false,
+            type: 'boolean',
+        },
+        uiResendLast: {
+            value: 0,
+            type: 'int',
+        },
+    },
+    modules: [],
+})
+
+export const CanvasWithSelected = createMockModuleFactory({
+    params: [
+        {
+            id: 'ep_D9GOWidPQC6czKHFb8zhEw',
+            name: 'canvas',
+            longName: 'clock with export.canvas',
+            type: 'String',
+            connected: false,
+            canConnect: false,
+            export: false,
+            value: 'x7vAq9cjRvavWJj8LVg0CwTUIoOhuSSPirPQcOa_RZOg',
+            drivingInput: false,
+            canToggleDrivingInput: true,
+            acceptedTypes: [
+                'String',
+            ],
+            requiresConnection: false,
+            defaultValue: 'x7vAq9cjRvavWJj8LVg0CwTUIoOhuSSPirPQcOa_RZOg',
+            updateOnChange: true,
+            isTextArea: false,
+            possibleValues: [
+                {
+                    value: 'x7vAq9cjRvavWJj8LVg0CwTUIoOhuSSPirPQcOa_RZOg',
+                    name: 'clock with export',
+                },
+                {
+                    value: 'ajuyRmXFSumhychAIeJ47wGn3ueg32TYaOPG047Aletw',
+                    name: 'button & textfield',
+                },
+                {
+                    value: 'CvG8YRKUQs6hG5seYcSXWQeP09Vr96T9SastNczRkbOA',
+                    name: 'clock with table',
+                },
+                {
+                    value: 'KLrwEPthTQ6wJnedXD30nw2onkvnk1QEGgZ9Y0QWYW5w',
+                    name: 'new button',
+                },
+                {
+                    value: 'uyxthPNPQ_WZTmYiaeAmbggiU5ZlXBRV-ivf4PPd0J0A',
+                    name: 'button',
+                },
+            ],
+        },
+    ],
+    inputs: [],
+    outputs: [
+        {
+            id: 'xJ3z0tjDQhyGjk2Y0suXSA3ktRHgTMQpitaDjA6OGuZw',
+            name: 'timestamp',
+            longName: 'Clock.timestamp',
+            type: 'Double',
+            connected: true,
+            canConnect: true,
+            export: false,
+            noRepeat: false,
+            canBeNoRepeat: true,
+        },
+    ],
+    id: 81,
+    jsModule: 'CanvasModule',
+    type: 'module',
+    name: 'clock with export',
+    canClearState: true,
+    canRefresh: true,
+    hash: -1857250076,
+    layout: {
+        width: '250px',
+        position: {
+            top: '0px',
+            left: '0px',
+        },
+        height: '150px',
+    },
+    uiChannel: {
+        webcomponent: null,
+        name: 'Notifications',
+        id: 'FfZOECs6REuJtsvNgWp4uw',
+    },
+    options: {
+        uiResendAll: {
+            value: false,
+            type: 'boolean',
+        },
+        uiResendLast: {
+            value: 0,
+            type: 'int',
+        },
+    },
+    modules: [
+        {
+            params: [
+                {
+                    id: 'ep_SPoRlvchT7e1Z7Nm7PGsGg',
+                    name: 'timezone',
+                    longName: 'Clock.timezone',
+                    type: 'String',
+                    connected: false,
+                    canConnect: true,
+                    export: false,
+                    value: 'UTC',
+                    drivingInput: false,
+                    canToggleDrivingInput: true,
+                    acceptedTypes: [
+                        'String',
+                    ],
+                    requiresConnection: false,
+                    defaultValue: 'UTC',
+                },
+                {
+                    id: 'ep_YvOONmHlRS6tvFfqWn-oqg',
+                    name: 'format',
+                    longName: 'Clock.format',
+                    type: 'String',
+                    connected: false,
+                    canConnect: true,
+                    export: false,
+                    value: 'yyyy-MM-dd HH:mm:ss z',
+                    drivingInput: false,
+                    canToggleDrivingInput: false,
+                    acceptedTypes: [
+                        'String',
+                    ],
+                    requiresConnection: false,
+                    defaultValue: 'yyyy-MM-dd HH:mm:ss z',
+                    isTextArea: false,
+                },
+                {
+                    id: 'ep_EZWe5hLBRiGTxOAX9X2P7A',
+                    name: 'rate',
+                    longName: 'Clock.rate',
+                    type: 'Double',
+                    connected: false,
+                    canConnect: true,
+                    export: false,
+                    value: 5,
+                    drivingInput: false,
+                    canToggleDrivingInput: false,
+                    acceptedTypes: [
+                        'Double',
+                    ],
+                    requiresConnection: false,
+                    defaultValue: 1,
+                },
+                {
+                    id: 'ep_Q6O5iSctTVuMTXP10TCVMg',
+                    name: 'unit',
+                    longName: 'Clock.unit',
+                    type: 'String',
+                    connected: false,
+                    canConnect: true,
+                    export: false,
+                    value: 'SECOND',
+                    drivingInput: false,
+                    canToggleDrivingInput: false,
+                    acceptedTypes: [
+                        'String',
+                    ],
+                    requiresConnection: false,
+                    possibleValues: [
+                        {
+                            name: 'second',
+                            value: 'SECOND',
+                        },
+                        {
+                            name: 'minute',
+                            value: 'MINUTE',
+                        },
+                        {
+                            name: 'hour',
+                            value: 'HOUR',
+                        },
+                        {
+                            name: 'day',
+                            value: 'DAY',
+                        },
+                    ],
+                    defaultValue: 'SECOND',
+                },
+            ],
+            inputs: [],
+            outputs: [
+                {
+                    id: 'ep_HOUkv4p4RXalZzWzQD8Syw',
+                    name: 'date',
+                    longName: 'Clock.date',
+                    type: 'String',
+                    connected: false,
+                    canConnect: true,
+                    export: false,
+                    noRepeat: false,
+                    canBeNoRepeat: true,
+                },
+                {
+                    id: 'xJ3z0tjDQhyGjk2Y0suXSA3ktRHgTMQpitaDjA6OGuZw',
+                    name: 'timestamp',
+                    longName: 'Clock.timestamp',
+                    type: 'Double',
+                    connected: true,
+                    canConnect: true,
+                    export: false,
+                    noRepeat: false,
+                    canBeNoRepeat: true,
+                },
+            ],
+            id: 209,
+            jsModule: 'GenericModule',
+            type: 'module',
+            name: 'Clock',
+            canClearState: true,
+            canRefresh: false,
+            hash: -261017640,
+            layout: {
+                width: '250px',
+                position: {
+                    top: '35px',
+                    left: '14px',
+                },
+                height: '150px',
+            },
+        },
+        {
+            params: [],
+            inputs: [
+                {
+                    id: 'ep_gqv8xvrlTQST5IU_0Vy3KQ',
+                    name: 'endpoint-1550222185495',
+                    longName: 'Table.in1',
+                    type: 'Object',
+                    connected: true,
+                    canConnect: true,
+                    export: false,
+                    displayName: 'in1',
+                    jsClass: 'VariadicInput',
+                    variadic: {
+                        requiresConnection: true,
+                        isLast: false,
+                        index: 1,
+                    },
+                    drivingInput: true,
+                    canToggleDrivingInput: false,
+                    acceptedTypes: [
+                        'Object',
+                    ],
+                    requiresConnection: false,
+                    sourceId: 'xJ3z0tjDQhyGjk2Y0suXSA3ktRHgTMQpitaDjA6OGuZw',
+                },
+                {
+                    id: 'd48b6616-5e91-45ab-b0d0-f3cc09377419',
+                    name: 'endpoint-d48b6616-5e91-45ab-b0d0-f3cc09377419',
+                    longName: 'Table.in2',
+                    type: 'Object',
+                    connected: false,
+                    canConnect: true,
+                    export: false,
+                    displayName: 'in2',
+                    jsClass: 'VariadicInput',
+                    variadic: {
+                        isLast: true,
+                        index: 2,
+                    },
+                    drivingInput: true,
+                    canToggleDrivingInput: false,
+                    acceptedTypes: [
+                        'Object',
+                    ],
+                    requiresConnection: false,
+                },
+            ],
+            outputs: [],
+            id: 527,
+            jsModule: 'TableModule',
+            type: 'module event-table-module',
+            name: 'Table',
+            canClearState: true,
+            canRefresh: false,
+            hash: -261008375,
+            layout: {
+                width: '250px',
+                position: {
+                    top: '45px',
+                    left: '701px',
+                },
+                height: '150px',
+            },
+            uiChannel: {
+                webcomponent: 'streamr-table',
+                name: 'Table',
+                id: 'oj1NZ9bMRiqE6JeE8F1hMw',
+            },
+            options: {
+                uiResendAll: {
+                    value: false,
+                    type: 'boolean',
+                },
+                uiResendLast: {
+                    value: 20,
+                    type: 'int',
+                },
+                maxRows: {
+                    value: 20,
+                    type: 'int',
+                },
+                showOnlyNewValues: {
+                    value: true,
+                    type: 'boolean',
+                },
+            },
+            tableConfig: {
+                headers: [
+                    'timestamp',
+                    'timestamp',
+                ],
+                title: 'Table',
+            },
+        },
+    ],
+})
+
+export const CanvasWithSelected2 = createMockModuleFactory({
+    params: [
+        {
+            id: 'ep_D9GOWidPQC6czKHFb8zhEw',
+            name: 'canvas',
+            longName: 'button.canvas',
+            type: 'String',
+            connected: false,
+            canConnect: false,
+            export: false,
+            value: 'uyxthPNPQ_WZTmYiaeAmbggiU5ZlXBRV-ivf4PPd0J0A',
+            drivingInput: false,
+            canToggleDrivingInput: true,
+            acceptedTypes: [
+                'String',
+            ],
+            requiresConnection: false,
+            defaultValue: 'uyxthPNPQ_WZTmYiaeAmbggiU5ZlXBRV-ivf4PPd0J0A',
+            updateOnChange: true,
+            isTextArea: false,
+            possibleValues: [
+                {
+                    value: 'x7vAq9cjRvavWJj8LVg0CwTUIoOhuSSPirPQcOa_RZOg',
+                    name: 'clock with export',
+                },
+                {
+                    value: 'ajuyRmXFSumhychAIeJ47wGn3ueg32TYaOPG047Aletw',
+                    name: 'button & textfield',
+                },
+                {
+                    value: 'CvG8YRKUQs6hG5seYcSXWQeP09Vr96T9SastNczRkbOA',
+                    name: 'clock with table',
+                },
+                {
+                    value: 'KLrwEPthTQ6wJnedXD30nw2onkvnk1QEGgZ9Y0QWYW5w',
+                    name: 'new button',
+                },
+                {
+                    value: 'uyxthPNPQ_WZTmYiaeAmbggiU5ZlXBRV-ivf4PPd0J0A',
+                    name: 'button',
+                },
+            ],
+        },
+        {
+            id: '1BOhcAMaTMeSQ4NQyGpAwwxpRQDl2jRgWZ_EQa_gzQZQ',
+            name: 'buttonValue',
+            longName: 'Button.buttonValue',
+            type: 'Double',
+            connected: false,
+            canConnect: true,
+            export: false,
+            value: 0,
+            drivingInput: false,
+            canToggleDrivingInput: true,
+            acceptedTypes: [
+                'Double',
+            ],
+            requiresConnection: false,
+            defaultValue: 0,
+        },
+    ],
+    inputs: [],
+    outputs: [
+        {
+            id: 'H90du9fXTIqz_8YaT6zcXgZgIA3wTpSv-SqaTpUNm7aQ',
+            name: 'out',
+            longName: 'Button.secondOut',
+            type: 'Double',
+            connected: false,
+            canConnect: true,
+            export: false,
+            displayName: 'secondOut',
+            noRepeat: false,
+            canBeNoRepeat: false,
+        },
+    ],
+    id: 81,
+    jsModule: 'CanvasModule',
+    type: 'module',
+    name: 'button',
+    canClearState: true,
+    canRefresh: true,
+    hash: -1857250076,
+    displayName: 'asdasd',
+    layout: {
+        width: '250px',
+        position: {
+            top: '0px',
+            left: '0px',
+        },
+        height: '150px',
+    },
+    uiChannel: {
+        webcomponent: null,
+        name: 'Notifications',
+        id: 'FfZOECs6REuJtsvNgWp4uw',
+    },
+    options: {
+        uiResendAll: {
+            value: false,
+            type: 'boolean',
+        },
+        uiResendLast: {
+            value: 0,
+            type: 'int',
+        },
+    },
+    modules: [
+        {
+            params: [
+                {
+                    id: 'ep_PDch2bw-SOyFB45Qz1BELQ',
+                    name: 'buttonName',
+                    longName: 'Button.buttonName',
+                    type: 'String',
+                    connected: false,
+                    canConnect: true,
+                    export: false,
+                    value: 'button',
+                    drivingInput: true,
+                    canToggleDrivingInput: true,
+                    acceptedTypes: [
+                        'String',
+                    ],
+                    requiresConnection: false,
+                    defaultValue: 'button',
+                    isTextArea: false,
+                },
+                {
+                    id: '1BOhcAMaTMeSQ4NQyGpAwwxpRQDl2jRgWZ_EQa_gzQZQ',
+                    name: 'buttonValue',
+                    longName: 'Button.buttonValue',
+                    type: 'Double',
+                    connected: false,
+                    canConnect: true,
+                    export: false,
+                    value: 0,
+                    drivingInput: false,
+                    canToggleDrivingInput: true,
+                    acceptedTypes: [
+                        'Double',
+                    ],
+                    requiresConnection: false,
+                    defaultValue: 0,
+                },
+            ],
+            inputs: [],
+            outputs: [
+                {
+                    id: 'H90du9fXTIqz_8YaT6zcXgZgIA3wTpSv-SqaTpUNm7aQ',
+                    name: 'out',
+                    longName: 'Button.secondOut',
+                    type: 'Double',
+                    connected: false,
+                    canConnect: true,
+                    export: false,
+                    displayName: 'secondOut',
+                    noRepeat: false,
+                    canBeNoRepeat: false,
+                },
+            ],
+            id: 218,
+            jsModule: 'InputModule',
+            type: 'module',
+            name: 'Button',
+            canClearState: false,
+            canRefresh: false,
+            hash: 0,
+            layout: {
+                position: {
+                    top: '162px',
+                    left: '273px',
+                },
+            },
+            uiChannel: {
+                webcomponent: 'streamr-button',
+                name: 'Button',
+                id: 'rwnRheYGSfCZBuzaOLlwXQ',
+            },
+            options: {
+                uiResendAll: {
+                    value: false,
+                    type: 'boolean',
+                },
+                uiResendLast: {
+                    value: 1,
+                    type: 'int',
+                },
+            },
+            widget: 'StreamrButton',
+        },
+    ],
 })
--- app/src/editor/canvas/tests/state.test.js
+++ app/src/editor/canvas/tests/state.test.js
@@ -1,4 +1,5 @@
 import * as State from '../state'
+import * as Mocks from './mocks'
 
 describe('Canvas State', () => {
     describe('emptyCanvas', () => {
@@ -6,6 +7,89 @@ describe('Canvas State', () => {
             expect(State.emptyCanvas()).toMatchSnapshot({
                 modules: [],
             })
+        })
+    })
+
+    describe('updateCanvas', () => {
+        it('has a port to select canvas', () => {
+            let canvas = State.emptyCanvas()
+            canvas = State.addModule(canvas, Mocks.Canvas())
+
+            const subCanvas = canvas.modules.find((m) => m.name === 'Canvas')
+            expect(subCanvas).toBeTruthy()
+
+            const canvasPort = State.findModulePort(canvas, subCanvas.hash, (p) => p.name === 'canvas')
+            expect(canvasPort).toBeTruthy()
+        })
+
+        it('reloads inputs and outputs from selected canvas', () => {
+            let canvas = State.emptyCanvas()
+            canvas = State.addModule(canvas, Mocks.Canvas())
+            let subCanvas = canvas.modules.find((m) => m.jsModule === 'CanvasModule')
+
+            const newModule = Mocks.CanvasWithSelected()
+            canvas = State.updateCanvas(State.updateModule(canvas, subCanvas.hash, () => newModule))
+            subCanvas = canvas.modules.find((m) => m.jsModule === 'CanvasModule')
+
+            expect(subCanvas.params.length === newModule.params.length).toBeTruthy()
+            expect(subCanvas.inputs.length === newModule.inputs.length).toBeTruthy()
+            expect(subCanvas.outputs.length === newModule.outputs.length).toBeTruthy()
+
+            subCanvas.outputs.forEach((output) => {
+                const exportedOutput = subCanvas.modules.find((m) => m.outputs.find((o) => o.id === output.id))
+                expect(exportedOutput).toBeTruthy()
+            })
+            subCanvas.inputs.forEach((input) => {
+                const exportedInput = subCanvas.modules.find((m) => m.inputs.find((i) => i.id === input.id))
+                expect(exportedInput).toBeTruthy()
+            })
+
+            const newModule2 = Mocks.CanvasWithSelected2()
+            canvas = State.updateCanvas(State.updateModule(canvas, subCanvas.hash, () => newModule2))
+            subCanvas = canvas.modules.find((m) => m.jsModule === 'CanvasModule')
+
+            expect(subCanvas.params.length === newModule2.params.length).toBeTruthy()
+            expect(subCanvas.inputs.length === newModule2.inputs.length).toBeTruthy()
+            expect(subCanvas.outputs.length === newModule2.outputs.length).toBeTruthy()
+
+            subCanvas.outputs.forEach((output) => {
+                const exportedOutput = subCanvas.modules.find((m) => m.outputs.find((o) => o.id === output.id))
+                expect(exportedOutput).toBeTruthy()
+            })
+            subCanvas.inputs.forEach((input) => {
+                const exportedInput = subCanvas.modules.find((m) => m.inputs.find((i) => i.id === input.id))
+                expect(exportedInput).toBeTruthy()
+            })
+        })
+
+        it('clears connected ports on reload', () => {
+            let canvas = State.emptyCanvas()
+            canvas = State.addModule(canvas, Mocks.CanvasWithSelected())
+            canvas = State.addModule(canvas, Mocks.Table())
+            let subCanvas = canvas.modules.find((m) => m.jsModule === 'CanvasModule')
+            const table = canvas.modules.find((m) => m.name === 'Table')
+            expect(subCanvas).toBeTruthy()
+            expect(table).toBeTruthy()
+            expect(subCanvas.hash).not.toEqual(table.hash)
+            const fromPort = State.findModulePort(canvas, subCanvas.hash, (p) => p.name === 'timestamp')
+            const toPort = State.findModulePort(canvas, table.hash, (p) => p.variadic.index === 1)
+            expect(fromPort).toBeTruthy()
+            expect(toPort).toBeTruthy()
+
+            canvas = State.updateCanvas(State.connectPorts(canvas, fromPort.id, toPort.id))
+            expect(State.isPortConnected(canvas, fromPort.id)).toBeTruthy()
+            expect(State.isPortConnected(canvas, toPort.id)).toBeTruthy()
+
+            const newModule = Mocks.CanvasWithSelected2()
+            canvas = State.updateCanvas(State.updateModule(canvas, subCanvas.hash, () => newModule))
+            subCanvas = canvas.modules.find((m) => m.jsModule === 'CanvasModule')
+
+            const fromPort2 = State.findModulePort(canvas, subCanvas.hash, (p) => p.name === 'out')
+            const toPort2 = State.findModulePort(canvas, table.hash, (p) => p.variadic.index === 1)
+
+            canvas = State.updateCanvas(State.connectPorts(canvas, fromPort2.id, toPort2.id))
+            expect(State.isPortConnected(canvas, fromPort2.id)).toBeTruthy()
+            expect(State.isPortConnected(canvas, toPort2.id)).toBeTruthy()
         })
     })
 })
