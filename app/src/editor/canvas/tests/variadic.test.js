58c39a30c480dff762d2d8d37c93a51b480107ef
Tim Oxley <secoif@gmail.com>
2019-03-21 16:57:08 +0800

    [editor canvas] Use live module definitions for variadic tests.

--- app/src/editor/canvas/tests/variadic.test.js
+++ app/src/editor/canvas/tests/variadic.test.js
@@ -1,11 +1,32 @@
 import * as State from '../state'
-import * as Mocks from './mocks'
+import * as Services from '../services'
+import { loadModuleDefinition, setup } from './utils'
 
 describe('Variadic Port Handling', () => {
+    let teardown
+
+    beforeAll(async () => {
+        teardown = await setup(Services.API)
+    }, 60000)
+
+    afterAll(async () => {
+        await teardown()
+    })
+
+    let Clock
+    let Table
+
+    beforeAll(async () => {
+        Clock = await loadModuleDefinition('Clock')
+        Table = await loadModuleDefinition('Table')
+        expect(Clock).toHaveProperty('id')
+        expect(Table).toHaveProperty('id')
+    })
+
     it('can add/connect modules', () => {
         let canvas = State.emptyCanvas()
-        canvas = State.addModule(canvas, Mocks.Clock())
-        canvas = State.addModule(canvas, Mocks.Table())
+        canvas = State.addModule(canvas, Clock)
+        canvas = State.addModule(canvas, Table)
         const clock = canvas.modules.find((m) => m.name === 'Clock')
         const table = canvas.modules.find((m) => m.name === 'Table')
         expect(clock).toBeTruthy()
