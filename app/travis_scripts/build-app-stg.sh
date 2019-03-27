62bba2cc680a704dfad4da793a9efbc6c8a027b2
Mikhael <mikhaelsantos@gmail.com>
2019-03-27 16:12:58 +0200

    chore: DEVOPS-69 - Setup Codeploy for marketplace Staging: (#296)
    * Add codedeploy files
    * Add building tar to travis

--- app/travis_scripts/build-app-stg.sh
+++ app/travis_scripts/build-app-stg.sh
@@ -13,3 +13,15 @@ echo $PLATFORM_ORIGIN_URL
 echo $STORYBOOK_BASE_PATH
 npm run build
 $(dirname $0)/build-storybook.sh
+
+echo "Code Deploy setup"
+mkdir $TRAVIS_BUILD_DIR/build
+cp $TRAVIS_BUILD_DIR/app/dist/index.html $TRAVIS_BUILD_DIR/build
+# Set .appspec in root
+mv $TRAVIS_BUILD_DIR/.codedeploy/.appspec.yml $TRAVIS_BUILD_DIR/build/appspec.yml
+# Copy bash scripts to be deployed in the tar
+mv  $TRAVIS_BUILD_DIR/.codedeploy  $TRAVIS_BUILD_DIR/build/
+tar -czvf $TRAVIS_BUILD_DIR/build/marketplace.tar -C  $TRAVIS_BUILD_DIR/build index.html appspec.yml .codedeploy
+rm -rf $TRAVIS_BUILD_DIR/build/*.html
+rm -rf $TRAVIS_BUILD_DIR/build/*.yml
+rm -rf $TRAVIS_BUILD_DIR/build/.codedeploy/
