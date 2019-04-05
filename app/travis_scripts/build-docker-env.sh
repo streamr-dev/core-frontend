5938118145d0c3a3b2dedf86262538e75bcb6b7b
Tim Oxley <secoif@gmail.com>
2019-04-05 17:29:31 +0800

    Follow docker logs for debugging travis.

--- app/travis_scripts/build-docker-env.sh
+++ app/travis_scripts/build-docker-env.sh
@@ -20,29 +20,27 @@ RETRIES=30;
 RETRY_DELAY=5s;
 
 # wait for E&E to come up
-waitFor $RETRIES $RETRY_DELAY checkHTTP "engine-and-editor" 200 http://localhost:8081/streamr-core/login/auth
+waitFor $RETRIES $RETRY_DELAY checkHTTP "engine-and-editor" 200 http://localhost:8081/streamr-core/login/auth;
+$streamr_docker_dev log -f &;
 
 # exit if E&E never comes up
 if [ $? -eq 1 ] ; then
     echo "engine-and-editor never up";
     $streamr_docker_dev ps;
-    $streamr_docker_dev log engine-and-editor | tail -n200;
     exit 1;
 fi
 
-streamr-docker-dev/streamr-docker-dev/bin.sh restart data-api # let's restart it for good measure (?!)
+streamr-docker-dev/streamr-docker-dev/bin.sh restart data-api; # let's restart it for good measure (?!)
 
 # wait briefly for data-api to come up. it probably needs restarting again.
-waitFor 15 3s checkHTTP "data-api" 401 http://localhost:8890/
+waitFor 15 3s checkHTTP "data-api" 401 http://localhost:8890/;
 
 # try restarting data-api again if still not up
 if [ $? -eq 1 ] ; then
     echo "data-api still not up"
     $streamr_docker_dev ps;
-    $streamr_docker_dev log data-api | tail -n200;
-    $streamr_docker_dev restart data-api
     # try waiting again
-    waitFor $RETRIES $RETRY_DELAY checkHTTP "data-api" 404 http://localhost:8890/
+    waitFor $RETRIES $RETRY_DELAY checkHTTP "data-api" 404 http://localhost:8890/;
     # exit if data-api ever came up (ffs)
     if [ $? -eq 1 ] ; then
         echo "data-api never up.";
