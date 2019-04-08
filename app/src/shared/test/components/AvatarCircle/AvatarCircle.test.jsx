be2570abd36f5730f397be41e75c7cb850c26acb
Matthew <mr.matto@gmail.com>
2019-04-08 20:37:07 +0200

    tidy test

--- app/src/shared/test/components/AvatarCircle/AvatarCircle.test.jsx
+++ app/src/shared/test/components/AvatarCircle/AvatarCircle.test.jsx
@@ -13,19 +13,16 @@ describe('getInitialsFromName', () => {
     describe('getInitialsFromName', () => {
         it('it gets the initials from the user\'s name', () => {
             const name = 'Ned Isakov'
-            const initials = getInitialsFromName(name)
-            assert.deepStrictEqual(initials, 'NI')
-        })
-
-        it('it gets the initials from the user\'s name', () => {
-            const name = 'Ned Isakov'
             assert.deepStrictEqual(getInitialsFromName(name), 'NI')
 
             const name2 = 'Elaine'
             assert.deepStrictEqual(getInitialsFromName(name2), 'E')
 
-            const name3 = ''
-            assert.deepStrictEqual(getInitialsFromName(name3), '')
+            const name3 = 'Jerry'
+            assert.deepStrictEqual(getInitialsFromName(name3), 'J')
+
+            const name4 = ''
+            assert.deepStrictEqual(getInitialsFromName(name4), '')
         })
     })
 })
