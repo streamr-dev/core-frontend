dd3a34169c7b1eb435af98a1bf018510c79a5eba
Mariusz Roliński <mariusz@mondoreale.pl>
2019-04-19 00:23:38 +0200

    Try parsing login failure messages

--- app/src/auth/utils/getSessionToken.js
+++ app/src/auth/utils/getSessionToken.js
@@ -2,9 +2,22 @@
 
 import StreamrClient from 'streamr-client'
 
-export default async (auth: Object): Promise<?string> => (
-    new StreamrClient({
-        restUrl: process.env.STREAMR_API_URL,
-        auth,
-    }).session.getSessionToken()
-)
+const parseError = async (error) => {
+    try {
+        return new Error(JSON.parse(error.body).message)
+    } catch (e) {
+        // Noop. Will rethrow.
+    }
+    return error
+}
+
+export default async (auth: Object): Promise<?string> => {
+    try {
+        return await new StreamrClient({
+            restUrl: process.env.STREAMR_API_URL,
+            auth,
+        }).session.getSessionToken()
+    } catch (e) {
+        throw await parseError(e)
+    }
+}
