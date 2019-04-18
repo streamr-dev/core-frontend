94b9026fd041e61ff8b66b5ba0840856caa8535b
Juha Haavisto <juha.haavisto@sujuwa.fi>
2019-04-18 15:50:02 +0300

    Add analytics class, move Sentry and LogRocket urls to .env

--- app/src/analytics.js
+++ app/src/analytics.js
@@ -1,7 +1,81 @@
 // @flow
 
 import Raven from 'raven-js'
+import LogRocket from 'logrocket'
 
-Raven.config('https://e012761929d24baa96ca82772f60b395@sentry.io/1203154', {
-    release: process.env.VERSION || 'development',
-}).install()
+type ErrorServiceId = string
+type ErrorService = {
+    id: ErrorServiceId,
+    init?: Function,
+    reportError?: Function,
+    getMiddleware?: Function,
+}
+
+class Analytics {
+    services = {}
+
+    register = ({ id, init, reportError, getMiddleware }: ErrorService) => {
+        if (this.services[id]) {
+            throw new Error(`Service ${id} already exists!`)
+        }
+
+        this.services[id] = {
+            id,
+            init: (typeof init === 'function') ? init : undefined,
+            reportError: (typeof reportError === 'function') ? reportError : undefined,
+            getMiddleware: (typeof getMiddleware === 'function') ? getMiddleware : undefined,
+        }
+
+        if (this.services[id].init) {
+            this.services[id].init()
+        }
+    }
+
+    deregister = (id: ErrorServiceId) => {
+        delete this.services[id]
+    }
+
+    reportError = (error: Error, extra: Object = {}) => {
+        Object.keys(this.services).forEach((id) => this.services[id].reportError && this.services[id].reportError(error, extra))
+    }
+
+    getMiddlewares = () => Object.keys(this.services).reduce((result, id) => ([
+        ...result,
+        ...(this.services[id].getMiddleware ? [this.services[id].getMiddleware()] : []),
+    ]), [])
+}
+
+const analytics = new Analytics()
+
+if (process.env.SENTRY_URL) {
+    analytics.register({
+        id: 'Sentry',
+        init: () => {
+            Raven.config(process.env.SENTRY_URL, {
+                release: process.env.VERSION || 'development',
+            }).install()
+        },
+        reportError: (error: Error, extra: Object = {}) => {
+            Raven.captureException(error, {
+                extra,
+            })
+        },
+    })
+}
+
+if (process.env.LOGROCKET_SLUG) {
+    analytics.register({
+        id: 'LogRocket',
+        init: () => {
+            LogRocket.init(process.env.LOGROCKET_SLUG)
+        },
+        reportError: (error: Error, extra: Object = {}) => {
+            LogRocket.captureException(error, {
+                extra,
+            })
+        },
+        getMiddleware: () => LogRocket.reduxMiddleware(),
+    })
+}
+
+export default analytics
--- app/src/shared/utils/withErrorBoundary.jsx
+++ app/src/shared/utils/withErrorBoundary.jsx
@@ -1,7 +1,7 @@
 // @flow
 
 import React, { Component, type ComponentType } from 'react'
-import Raven from 'raven-js'
+import analytics from '../../analytics'
 
 type Props = {
     path?: string,
@@ -28,7 +28,7 @@ const withErrorBoundary = (ErrorComponent: ComponentType<any>) => (
 
             componentDidCatch(error: Error, extra: any) {
                 console.error(error)
-                Raven.captureException(error, {
+                analytics.reportError(error, {
                     extra,
                 })
                 this.setState({
