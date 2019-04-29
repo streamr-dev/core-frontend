d9aa5312ceb1d10da4d5e7a9aa98dc440a9616a2
Matthew <mr.matto@gmail.com>
2019-04-29 17:02:12 +0200

    typos, fix mistakes

--- app/src/marketplace/flowtype/global-types.js
+++ app/src/marketplace/flowtype/global-types.js
@@ -6,6 +6,7 @@ declare class process {
         NODE_ENV: string,
         PLATFORM_ORIGIN_URL: string,
         STREAMR_API_URL: string,
+        STREAMR_URL: string,
         STREAMR_WS_URL: string,
         GOOGLE_ANALYTICS_ID: string,
         [key: string]: ?string
