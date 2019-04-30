a3afcef3cfbf1f6c98dca665da3af9dcbd6a45ef
Matthew <mr.matto@gmail.com>
2019-04-30 10:07:39 +0200

    remove eslint error: import Onboarding from '$shared/components/Onboarding' unused var

--- app/src/userpages/components/CanvasPage/List/index.jsx
+++ app/src/userpages/components/CanvasPage/List/index.jsx
@@ -37,7 +37,6 @@ import type { User } from '$shared/flowtype/user-types'
 import { selectUserData } from '$shared/modules/user/selectors'
 import { RunStates } from '$editor/canvas/state'
 import DocsShortcuts from '$userpages/components/DocsShortcuts'
-import Onboarding from '$shared/components/Onboarding'
 import CanvasPreview from '$editor/canvas/components/Preview'
 
 import styles from './canvasList.pcss'
