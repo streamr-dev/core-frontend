ed76363e344c0f43f416343d8c936ba749de3b34
Tuomas Koponen <tuomas.koponen@streamr.com>
2019-05-08 15:39:26 +0300

    Focus input on mount

--- app/src/editor/canvas/components/ModuleSearch.jsx
+++ app/src/editor/canvas/components/ModuleSearch.jsx
@@ -158,6 +158,10 @@ export class ModuleSearch extends React.PureComponent<Props, State> {
         window.addEventListener('keydown', this.onKeyDown)
         this.addOrRemoveDropListener(true)
         this.load()
+
+        if (this.input) {
+            this.input.focus()
+        }
     }
 
     componentWillUnmount() {
