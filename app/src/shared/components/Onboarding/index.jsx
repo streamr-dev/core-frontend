69377260b6e149c567ecc9836e90a3cdde311e93
Mariusz Roliński <mariusz@mondoreale.pl>
2019-05-08 23:00:27 +0200

    [onboarding] Fix for ff and safari

--- app/src/shared/components/Onboarding/index.jsx
+++ app/src/shared/components/Onboarding/index.jsx
@@ -1,6 +1,7 @@
 // @flow
 
 import React, { type Element, type ChildrenArray } from 'react'
+import cx from 'classnames'
 import Link from '$shared/components/Link'
 import SvgIcon from '$shared/components/SvgIcon'
 import styles from './onboarding.pcss'
@@ -21,15 +22,16 @@ const Onboarding = ({ children, title }: Props) => (
                 )}
                 {React.Children.map(children, (child) => child || <div className={styles.separator} />)}
             </div>
-            <button type="button">
+            {/* eslint-disable-next-line jsx-a11y/no-noninteractive-tabindex */}
+            <div className={styles.toggle} tabIndex="0">
                 <SvgIcon name="questionMark" className={styles.icon} />
-            </button>
+            </div>
         </div>
         {/* The following element is visible only when `.inner` is/has `:focus-within`. CSS makes
             it cover the actual popup toggle. Clicking it "steals" focus from whoever has it within
-            `.inner`. This allows us to trick ppl into believing that "?" button is a js-driven
-            toggle which it is not. Magic. */}
-        <button type="button" className={styles.focusCatcher} tabIndex="-1" />
+            `.inner`. This allows us to trick ppl into believing that "?" is a js-driven toggle
+            which it is not. Magic. */}
+        <div className={cx(styles.toggle, styles.focusCatcher)} tabIndex="-1" />
     </div>
 )
 
