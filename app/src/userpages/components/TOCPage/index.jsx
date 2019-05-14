29029d8f9f7417799912f8b45a01fb4a758595e4
Mariusz Roliński <mariusz@mondoreale.pl>
2019-05-14 12:46:12 +0200

    [TOCPage] Rebuild as a functional component

--- app/src/userpages/components/TOCPage/index.jsx
+++ app/src/userpages/components/TOCPage/index.jsx
@@ -1,6 +1,6 @@
 // @flow
 
-import React, { Component, type Node } from 'react'
+import React, { type Node } from 'react'
 import { Col, Row } from 'reactstrap'
 import { withRouter } from 'react-router-dom'
 import cx from 'classnames'
@@ -16,46 +16,40 @@ type Props = {
     },
 }
 
-class TOCPage extends Component<Props> {
-    static Section = TOCSection
+const TOCPage = withRouter(({ children, location: { hash }, title }: Props) => (
+    <Row>
+        <Col xs={12} sm={12} md={3}>
+            <ul className={styles.tocList}>
+                {React.Children.map(children, (child) => {
+                    if (child.type === TOCSection) {
+                        return (
+                            <li
+                                key={child.props.id}
+                                className={cx(styles.tocListItem, {
+                                    [styles.hideTablet]: child.props.customStyled,
+                                })}
+                            >
+                                <a
+                                    href={`#${child.props.id}`}
+                                    className={cx({
+                                        [styles.active]: hash.substr(1) === child.props.id,
+                                    })}
+                                >
+                                    {child.props.linkTitle || child.props.title}
+                                </a>
+                            </li>
+                        )
+                    }
+                })}
+            </ul>
+        </Col>
+        <Col xs={12} sm={12} md={9} >
+            {!!title && (<h1 className={styles.pageTitle}>{title}</h1>)}
+            {children}
+        </Col>
+    </Row>
+))
 
-    parseMenu = () => React.Children.map(this.props.children, (child) => {
-        if (child.type === TOCSection) {
-            return (
-                <li
-                    key={child.props.id}
-                    className={cx(styles.tocListItem, {
-                        [styles.hideTablet]: child.props.customStyled,
-                    })}
-                >
-                    <a
-                        href={`#${child.props.id}`}
-                        className={this.props.location.hash.substr(1) === child.props.id ? styles.active : ''}
-                    >
-                        {child.props.linkTitle || child.props.title}
-                    </a>
-                </li>
-            )
-        }
-    })
+TOCPage.Section = TOCSection
 
-    render() {
-        const { title, children } = this.props
-
-        return (
-            <Row>
-                <Col xs={12} sm={12} md={3} >
-                    <ul className={styles.tocList}>
-                        {this.parseMenu()}
-                    </ul>
-                </Col>
-                <Col xs={12} sm={12} md={9} >
-                    {!!title && (<h1 className={styles.pageTitle}>{title}</h1>)}
-                    {children}
-                </Col>
-            </Row>
-        )
-    }
-}
-
-export default withRouter(TOCPage)
+export default TOCPage
