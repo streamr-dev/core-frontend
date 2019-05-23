2cd96db6a185e5880d21e5dd4282beef3ee54e83
Mariusz Roliński <mariusz@mondoreale.pl>
2019-05-23 12:24:31 +0200

    Make the logo a link to /core/canvases on the canvas page

--- app/src/shared/components/Nav/LogoItem/index.jsx
+++ app/src/shared/components/Nav/LogoItem/index.jsx
@@ -1,17 +1,27 @@
 // @flow
 
 import React from 'react'
-
+import { withRouter, type Location } from 'react-router-dom'
+import Link from '$shared/components/Link'
 import Logo from '$shared/components/Logo'
+import routes from '$routes'
 import styles from './logoItem.pcss'
 
-const LogoItem = () => (
-    <a
-        href={process.env.PLATFORM_ORIGIN_URL}
-        className={styles.root}
-    >
-        <Logo />
-    </a>
-)
+type Props = {
+    location: Location,
+}
+
+const LogoItem = withRouter(({ location: { pathname } }: Props) => {
+    const location = /^\/canvas\/editor\//.test(pathname) ? routes.canvases() : process.env.PLATFORM_ORIGIN_URL
+
+    return (
+        <Link
+            to={location}
+            className={styles.root}
+        >
+            <Logo />
+        </Link>
+    )
+})
 
 export default LogoItem
