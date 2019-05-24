d79204f468577788f874d0fc5a4b559d40d93d56
Tim Oxley <secoif@gmail.com>
2019-05-24 20:18:29 +0800

    [webpack] Remove @sentry/webpack-plugin for now. Could not make work.

--- app/webpack.config.js
+++ app/webpack.config.js
@@ -15,7 +15,6 @@ const cssProcessor = require('cssnano')
 const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const GitRevisionPlugin = require('git-revision-webpack-plugin')
-const SentryPlugin = require('@sentry/webpack-plugin')
 
 const dotenv = require('./scripts/dotenv')
 
@@ -162,27 +161,6 @@ module.exports = {
             new BundleAnalyzerPlugin({
                 analyzerMode: 'static',
                 openAnalyzer: false,
-            }),
-        ] : []),
-        ...(process.env.SENTRY_DSN ? [
-            new SentryPlugin({
-                include: 'src',
-                validate: true,
-                ignore: [
-                    '.cache',
-                    '.DS_STORE',
-                    '.env',
-                    '.storybook',
-                    'bin',
-                    'coverage',
-                    'node_modules',
-                    'scripts',
-                    'stories',
-                    'test',
-                    'travis_scripts',
-                    'webpack.config.js',
-                ],
-                release: process.env.VERSION,
             }),
         ] : []),
     ].concat(isProduction() ? [
