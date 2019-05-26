6f59cda75890a2ca0c93cb88243934058fc86ff0
Tim Oxley <secoif@gmail.com>
2019-05-26 16:43:31 +0800

    [editor canvas] Add test for subcanvas exported port connection state handling.

--- app/src/editor/canvas/tests/canvas.test.js
+++ app/src/editor/canvas/tests/canvas.test.js
@@ -90,13 +90,62 @@ describe('Canvas Module', () => {
             value: subcanvas.id,
         }))
 
-        canvas = State.updateCanvas(await Services.saveNow(canvas))
-
+        canvas = await Services.saveNow(State.updateCanvas(canvas))
+        canvas = State.updateCanvas(canvas)
         canvasModule = canvas.modules.find((m) => m.jsModule === 'CanvasModule')
 
         expect(clock.params.every(({ name }) => !!canvasModule.params.find((p) => p.name === name))).toBeTruthy()
         expect(clock.inputs.every(({ name }) => !!canvasModule.inputs.find((p) => p.name === name))).toBeTruthy()
         expect(clock.outputs.every(({ name }) => !!canvasModule.outputs.find((p) => p.name === name))).toBeTruthy()
+    })
+
+    it('"fixes" subcanvas connection states', async () => {
+        // exported ports show connection status from the subcanvas
+        // rather than the connection status in the canvas it's loaded into
+        // sourceId will reference a port that on the subcanvas that doesn't exist
+        // on the parent canvas
+        // note nothing can be done in the case that an exported port is connected to
+        // another exported port, since the port will actually exist in the current canvas
+
+        let subcanvas = await Services.create()
+        subcanvas = State.addModule(subcanvas, await loadModuleDefinition('Constant'))
+        subcanvas = State.addModule(subcanvas, await loadModuleDefinition('Abs'))
+
+        const constant = subcanvas.modules.find((m) => m.name === 'Constant')
+        const constantOut = State.findModulePort(subcanvas, constant.hash, (p) => p.name === 'out')
+        const abs = subcanvas.modules.find((m) => m.name === 'Abs')
+        const absIn = State.findModulePort(subcanvas, abs.hash, (p) => p.name === 'in')
+
+        subcanvas = State.updateCanvas(State.connectPorts(subcanvas, constantOut.id, absIn.id))
+        subcanvas = State.setPortOptions(subcanvas, absIn.id, {
+            export: true,
+        })
+
+        await Services.saveNow(subcanvas)
+
+        let canvas = await State.emptyCanvas()
+        canvas = State.addModule(canvas, await loadModuleDefinition('Canvas'))
+        let canvasModule = canvas.modules.find((m) => m.jsModule === 'CanvasModule')
+
+        // select the subcanvas
+        const canvasPort = State.findModulePort(canvas, canvasModule.hash, (p) => p.name === 'canvas')
+        canvas = State.updatePort(canvas, canvasPort.id, (port) => ({
+            ...port,
+            value: subcanvas.id,
+        }))
+
+        canvas = State.updateCanvas(canvas)
+
+        const serverCanvasBeforeFix = await Services.create(canvas)
+        // wrap in updateCanvas to get the "fix"
+        canvas = State.updateCanvas(serverCanvasBeforeFix)
+
+        canvasModule = canvas.modules.find((m) => m.jsModule === 'CanvasModule')
+        const exportedAbsIn = canvasModule.inputs[0]
+        expect(State.isPortConnected(canvas, exportedAbsIn.id)).not.toBeTruthy()
+        const serverCanvasAfterFix = await Services.saveNow(canvas)
+        // ensure the "fix" didn't mutate canvas
+        expect(serverCanvasAfterFix).toMatchCanvas(serverCanvasBeforeFix)
     })
 
     it('works when exported inputs are variadic', async () => {
@@ -138,14 +187,14 @@ describe('Canvas Module', () => {
         subcanvas = State.addModule(subcanvas, await loadModuleDefinition('Abs'))
 
         const passThrough = subcanvas.modules.find((m) => m.name === 'PassThrough')
-        const constant1 = subcanvas.modules.find((m) => m.name === 'Constant')
+        const constant = subcanvas.modules.find((m) => m.name === 'Constant')
         const abs = subcanvas.modules.find((m) => m.name === 'Abs')
-        // connect constant1 to passthrough
-        const constant1Out = State.findModulePort(subcanvas, constant1.hash, (p) => p.name === 'out')
+        // connect constant to passthrough
+        const constantOut = State.findModulePort(subcanvas, constant.hash, (p) => p.name === 'out')
         const passThroughIn1 = State.findModulePort(subcanvas, passThrough.hash, (p) => p.longName === 'PassThrough.in1')
-        subcanvas = State.updateCanvas(State.connectPorts(subcanvas, constant1Out.id, passThroughIn1.id))
+        subcanvas = State.updateCanvas(State.connectPorts(subcanvas, constantOut.id, passThroughIn1.id))
 
-        // connect passthrough to constant2
+        // connect passthrough to abs
         const passThroughOut1 = State.findModulePort(subcanvas, passThrough.hash, (p) => p.longName === 'PassThrough.out1')
         const absIn = State.findModulePort(subcanvas, abs.hash, (p) => p.name === 'in')
         subcanvas = State.updateCanvas(State.connectPorts(subcanvas, passThroughOut1.id, absIn.id))
