b8fea1dd1ba8dece0e87a4dacd58ad0cad291eb0
Tuomas Koponen <tumppi.k@gmail.com>
2019-05-28 11:04:09 +0300

    Add embed route handler

--- app/src/app/index.jsx
+++ app/src/app/index.jsx
@@ -149,6 +149,7 @@ const UserpagesRouter = () => ([
 const EditorRouter = () => ([
     <Route exact path="/" component={Products} key="root" />, // edge case for localhost
     <Route exact path={formatPath(editor.canvasEditor, ':id?')} component={CanvasEditorAuth} key="CanvasEditor" />,
+    <Route exact path={formatPath(editor.canvasEmbed, ':id?')} component={CanvasEditorAuth} key="CanvasEmbed" />,
     <Route exact path={formatPath(editor.dashboardEditor, ':id?')} component={DashboardEditorAuth} key="DashboardEditor" />,
 ])
 
