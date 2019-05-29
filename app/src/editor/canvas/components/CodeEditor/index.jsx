ed11fb0175c5acd69716dcf81fabe01eb428e63b
Juha Haavisto <juha.haavisto@sujuwa.fi>
2019-05-29 15:11:08 +0300

    Add size constraint to dialog, hooks to code editor

--- app/src/editor/canvas/components/CodeEditor/index.jsx
+++ app/src/editor/canvas/components/CodeEditor/index.jsx
@@ -1,101 +1,78 @@
-import React from 'react'
+import React, { useState, useCallback } from 'react'
 
 import CodeEditorWindow from './CodeEditorWindow'
 import DebugWindow from './DebugWindow'
 
-export default class CodeEditor extends React.Component {
-    state = {
-        editorOpen: false,
-        debugOpen: false,
-        editorPosition: {
-            x: 0,
-            y: 0,
-        },
-        debugPosition: {
-            x: 0,
-            y: 0,
-        },
-    }
-
-    onShowEditor = () => {
-        this.setState({
-            editorOpen: true,
+export const CodeEditor = ({
+    children,
+    code,
+    readOnly,
+    onApply,
+    onChange,
+    debugMessages,
+    onClearDebug,
+}) => {
+    const [editorOpen, setEditorOpen] = useState(false)
+    const [debugOpen, setDebugOpen] = useState(false)
+    const [editorPosition, setEditorPosition] = useState({
+        x: 0,
+        y: 0,
+    })
+    const [debugPosition, setDebugPosition] = useState({
+        x: 0,
+        y: 0,
+    })
+    const onShowEditor = useCallback(() => {
+        setEditorOpen(true)
+    }, [setEditorOpen])
+    const onCloseEditor = useCallback(() => {
+        setEditorOpen(false)
+    }, [setEditorOpen])
+    const onShowDebug = useCallback(() => {
+        setDebugOpen(true)
+    }, [setDebugOpen])
+    const onCloseDebug = useCallback(() => {
+        setDebugOpen(false)
+    }, [setDebugOpen])
+    const onEditorPositionUpdate = useCallback((x, y) => {
+        setEditorPosition({
+            x,
+            y,
         })
-    }
-
-    onCloseEditor = () => {
-        this.setState({
-            editorOpen: false,
+    }, [setEditorPosition])
+    const onDebugPositionUpdate = useCallback((x, y) => {
+        setDebugPosition({
+            x,
+            y,
         })
-    }
+    }, [setDebugPosition])
 
-    onShowDebug = () => {
-        this.setState({
-            debugOpen: true,
-        })
-    }
-
-    onCloseDebug = () => {
-        this.setState({
-            debugOpen: false,
-        })
-    }
-
-    onEditorPositionUpdate = (x, y) => {
-        this.setState({
-            editorPosition: {
-                x,
-                y,
-            },
-        })
-    }
-
-    onDebugPositionUpdate = (x, y) => {
-        this.setState({
-            debugPosition: {
-                x,
-                y,
-            },
-        })
-    }
-
-    render() {
-        const {
-            readOnly,
-            code,
-            debugMessages,
-            onApply,
-            onChange,
-            onClearDebug,
-            children,
-        } = this.props
-        const { editorOpen, debugOpen, editorPosition, debugPosition } = this.state
-
-        return (
-            <React.Fragment>
-                {children && children(this.onShowEditor)}
-                {!!editorOpen && (
-                    <CodeEditorWindow
-                        position={editorPosition}
-                        onPositionUpdate={this.onEditorPositionUpdate}
-                        code={code}
-                        readOnly={readOnly}
-                        onClose={this.onCloseEditor}
-                        onApply={onApply}
-                        onChange={onChange}
-                        onShowDebug={this.onShowDebug}
-                    />
-                )}
-                {!!debugOpen && (
-                    <DebugWindow
-                        onPositionUpdate={this.onDebugPositionUpdate}
-                        position={debugPosition}
-                        messages={debugMessages}
-                        onClear={onClearDebug}
-                        onClose={this.onCloseDebug}
-                    />
-                )}
-            </React.Fragment>
-        )
-    }
+    return (
+        <React.Fragment>
+            {children && children(onShowEditor)}
+            {!!editorOpen && (
+                <CodeEditorWindow
+                    position={editorPosition}
+                    onPositionUpdate={onEditorPositionUpdate}
+                    code={code}
+                    readOnly={readOnly}
+                    onClose={onCloseEditor}
+                    onApply={onApply}
+                    onChange={onChange}
+                    onShowDebug={onShowDebug}
+                />
+            )}
+            {!!debugOpen && (
+                <DebugWindow
+                    onPositionUpdate={onDebugPositionUpdate}
+                    position={debugPosition}
+                    messages={debugMessages}
+                    onClear={onClearDebug}
+                    onClose={onCloseDebug}
+                />
+            )}
+        </React.Fragment>
+    )
 }
+
+export default CodeEditor
