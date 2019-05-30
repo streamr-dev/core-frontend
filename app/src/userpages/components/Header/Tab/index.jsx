6d38f95ff49b9103e40487c4ed77b08e8c68cafc
Mariusz Roliński <mariusz@mondoreale.pl>
2019-05-30 12:17:16 +0200

    Use rr’s NavLink instead of withRouter

--- app/src/userpages/components/Header/Tab/index.jsx
+++ app/src/userpages/components/Header/Tab/index.jsx
@@ -2,25 +2,23 @@
 
 import React, { type Node } from 'react'
 import cx from 'classnames'
-import { Link, type Location, withRouter } from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
 import styles from './tab.pcss'
 
 type Props = {
     children: Node,
     className?: ?string,
-    location: Location,
     to: string,
 }
 
-const Tab = withRouter(({ className, to, location: { pathname }, children }: Props) => (
-    <Link
-        className={cx(styles.root, className, {
-            [styles.active]: pathname.includes(to),
-        })}
+const Tab = ({ className, to, children }: Props) => (
+    <NavLink
+        activeClassName={styles.active}
+        className={cx(styles.root, className)}
         to={to}
     >
         {children}
-    </Link>
-))
+    </NavLink>
+)
 
 export default Tab
