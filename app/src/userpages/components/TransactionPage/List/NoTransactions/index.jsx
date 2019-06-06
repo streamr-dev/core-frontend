38de1d2d264d031922a21419f9e0695a91b92e1c
Juha Haavisto <juha.haavisto@sujuwa.fi>
2019-06-06 15:59:01 +0300

    Update transactions empty state

--- app/src/userpages/components/TransactionPage/List/NoTransactions/index.jsx
+++ app/src/userpages/components/TransactionPage/List/NoTransactions/index.jsx
@@ -2,12 +2,20 @@
 
 import React from 'react'
 import { Translate, I18n } from 'react-redux-i18n'
+import { Link } from 'react-router-dom'
 
 import EmptyState from '$shared/components/EmptyState'
 import emptyStateIcon from '$shared/assets/images/empty_state_icon.png'
 import emptyStateIcon2x from '$shared/assets/images/empty_state_icon@2x.png'
 
-const NoTransactionsView = () => (
+import routes from '$routes'
+
+type Props = {
+    accountsExist?: boolean,
+    accountLinked?: boolean,
+}
+
+const NoTransactionsView = ({ accountsExist, accountLinked }: Props) => (
     <EmptyState
         image={(
             <img
@@ -16,9 +24,39 @@ const NoTransactionsView = () => (
                 alt={I18n.t('error.notFound')}
             />
         )}
+        link={(!accountsExist || !accountLinked) && (
+            <Link
+                className="btn btn-special"
+                to={routes.editProfile({}, 'ethereum-accounts')}
+            >
+                <Translate value="userpages.transactions.noAccountsLinked.linkAccount" />
+            </Link>
+        )}
     >
-        <Translate value="userpages.transactions.noTransactions.title" />
-        <Translate value="userpages.transactions.noTransactions.message" tag="small" />
+        {(() => {
+            if (!accountsExist) {
+                return (
+                    <React.Fragment>
+                        <Translate value="userpages.transactions.noAccountsLinked.title" />
+                        <Translate value="userpages.transactions.noAccountsLinked.message" tag="small" />
+                    </React.Fragment>
+                )
+            } else if (!accountLinked) {
+                return (
+                    <React.Fragment>
+                        <Translate value="userpages.transactions.currentAccountNotLinked.title" />
+                        <Translate value="userpages.transactions.currentAccountNotLinked.message" tag="small" />
+                    </React.Fragment>
+                )
+            }
+
+            return (
+                <React.Fragment>
+                    <Translate value="userpages.transactions.noTransactions.title" />
+                    <Translate value="userpages.transactions.noTransactions.message" tag="small" />
+                </React.Fragment>
+            )
+        })()}
     </EmptyState>
 )
 
