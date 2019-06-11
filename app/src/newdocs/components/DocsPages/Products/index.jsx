fbb55f9490a16024ced9d0c42ef7d03fd6364fe4
Matthew <mr.matto@gmail.com>
2019-06-11 12:17:38 +0200

    flow/eslint $newdocs alias, newDocs routing and placeholder pages/sections, remove streams page from existing docs

---
+++ app/src/newdocs/components/DocsPages/Products/index.jsx
@@ -0,0 +1,19 @@
+// @flow
+
+import React from 'react'
+import { Helmet } from 'react-helmet'
+
+import DocsLayout from '../../DocsLayout'
+import { subNav } from '../../DocsLayout/Navigation/navLinks'
+import ProductsContent from '$newdocs/content/products.mdx'
+
+const Products = () => (
+    <DocsLayout subNav={subNav.products}>
+        <Helmet>
+            <title>Streamr Docs | Products</title>
+        </Helmet>
+        <ProductsContent />
+    </DocsLayout>
+)
+
+export default Products
