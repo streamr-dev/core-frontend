9e519c8d75131f5a2d72f7f05eb95f1692bb506a
Mariusz Roliński <mariusz@mondoreale.pl>
2019-06-12 12:08:02 +0200

    Add an explanatory note to `ums` postcss plugin

--- app/scripts/postcss/ums.js
+++ app/scripts/postcss/ums.js
@@ -1,5 +1,9 @@
 const postcss = require('postcss')
 
+// `um` is a unit representing 16px (`--um` variable). It's temporary. Current `rem` is
+// contextual (depends on screen size). Once we fix that (make `rem` constant) we can
+// replace all ums with rems. Simple as that.
+
 module.exports = postcss.plugin('postcss-reverse-props', () => (root) => {
     root.walkDecls((decl) => {
         // eslint-disable-next-line no-param-reassign
