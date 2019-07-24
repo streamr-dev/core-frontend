708f3a00a071b6e6579cd04e7817b8c37c0056ed
Juha Haavisto <juha.haavisto@sujuwa.fi>
2019-07-24 11:31:20 +0300

    Start new stream selector

--- app/src/marketplace/containers/EditProductPage2/StreamSelector.jsx
+++ app/src/marketplace/containers/EditProductPage2/StreamSelector.jsx
@@ -2,12 +2,20 @@
 
 import React from 'react'
 
+import StreamSelectorComponent from '$mp/components/StreamSelector'
+
 const StreamSelector = () => (
     <div>
         <h1>Add streams</h1>
         <p>Products can contain a range of streams, or a single &quot;firehose&quot; type stream, it&apos;s up to you.
             If you haven&apos;t made any streams yet, you can create one here. For help creating streams, see the docs.
         </p>
+        <StreamSelectorComponent
+            availableStreams={[]}
+            fetchingStreams={false}
+            onEdit={() => {}}
+            streams={[]}
+        />
     </div>
 )
 
