b142a42547604c57e4cad2419ecf1c5f169af0f6
Tim Oxley <secoif@gmail.com>
2019-08-08 15:47:02 +0800

    [newdocs CanvasModules] Sort modules & categories consistently.

--- app/src/newdocs/components/DocsPages/CanvasModules/data.js
+++ app/src/newdocs/components/DocsPages/CanvasModules/data.js
@@ -4,7 +4,17 @@ const importAll = (r) => r.keys().map(r).map(({ default: m }) => m)
 
 // $FlowFixMe
 export const canvasModules = importAll(require.context('$newdocs/content/canvasModules', false, /\.jsx$/))
-export const canvasModulesCategorised = groupBy(canvasModules, ({ path }) => path.split(':')[0])
+
+export const canvasModulesCategorised = Object.entries((
+    groupBy(canvasModules, ({ path }) => (
+        // collapse category, only use first part e.g. Time Series: Random -> Time Series
+        path.split(':')[0].trim()
+    ))
+))
+    .sort((a, b) => a[0].localeCompare(b[0])) // sort category names
+    .reduce((o, [k, v]) => Object.assign(o, {
+        [k]: v.sort((a, b) => a.name.localeCompare(b.name)), // sort modules by name
+    }), {})
 
 /**
  * Convert category name into suitable anchor
