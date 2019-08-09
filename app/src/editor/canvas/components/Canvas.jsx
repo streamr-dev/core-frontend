21447451aee8dc58c77107dad5989f91d92b0ff4
Tim Oxley <secoif@gmail.com>
2019-08-09 16:28:44 +0800

    [editor/canvas] Implement minimal canvas zoom functionality via scrollwheel.

--- app/src/editor/canvas/components/Canvas.jsx
+++ app/src/editor/canvas/components/Canvas.jsx
@@ -10,6 +10,7 @@ import { CanvasWindowProvider } from './CanvasWindow'
 import Cables from './Cables'
 
 import styles from './Canvas.pcss'
+import Zoom from './Zoom'
 
 export default class Canvas extends React.PureComponent {
     setPortUserValue = (portId, value, done) => {
@@ -155,7 +156,7 @@ function CanvasElements(props) {
     if (!canvas) { return null }
 
     return (
-        <div className={styles.CanvasElements}>
+        <Zoom canvas={canvas} className={styles.CanvasElements}>
             <DragDropProvider>
                 <div
                     className={styles.Modules}
@@ -183,6 +184,6 @@ function CanvasElements(props) {
                     selectedModuleHash={selectedModuleHash}
                 />
             </DragDropProvider>
-        </div>
+        </Zoom>
     )
 }
---
+++ app/src/editor/canvas/components/Zoom.jsx
@@ -0,0 +1,100 @@
+import React, { useRef, useState, useCallback, useEffect } from 'react'
+import cx from 'classnames'
+import styles from './Zoom.pcss'
+
+function clamp(value, min, max) {
+    return Math.max(min, Math.min(max, value))
+}
+
+export default function Zoom({ className, children }) {
+    const elRef = useRef()
+    const [{
+        x,
+        y,
+        zoom,
+        xImage,
+        yImage,
+    }, setZoom] = useState({
+        zoom: 1,
+        x: 0,
+        y: 0,
+        xLast: 0,
+        yLast: 0,
+        xImage: 0,
+        yImage: 0,
+    })
+
+    const onWheel = useCallback((event) => {
+        const el = elRef.current
+        event.preventDefault()
+
+        const { left, top } = el.getBoundingClientRect()
+        const { deltaY } = event
+        // find current location on screen
+        const xScreen = Math.round(event.clientX - left)
+        const yScreen = Math.round(event.clientY - top)
+
+        setZoom(({
+            zoom,
+            xImage,
+            yImage,
+            xLast,
+            yLast,
+        }) => {
+            // find current location on the image at the current scale
+            xImage += Math.round((xScreen - xLast) / zoom)
+            yImage += Math.round((yScreen - yLast) / zoom)
+
+            // determine the new scale
+            if (deltaY < 0) {
+                zoom *= 1.05
+            } else {
+                zoom *= 0.95
+            }
+            zoom = Math.round(clamp(zoom, 0.1, 3) * 100) / 100
+
+            // determine the location on the screen at the new scale
+            const xNew = Math.round((xScreen - xImage) / zoom)
+            const yNew = Math.round((yScreen - yImage) / zoom)
+
+            // save the current screen location
+            xLast = xScreen
+            yLast = yScreen
+
+            return {
+                zoom,
+                xImage,
+                yImage,
+                x: xNew,
+                y: yNew,
+                xLast,
+                yLast,
+            }
+        })
+    }, [setZoom])
+
+    useEffect(() => {
+        const el = elRef.current
+        el.addEventListener('wheel', onWheel)
+        return () => {
+            el.removeEventListener('wheel', onWheel)
+        }
+    }, [elRef, onWheel])
+
+    return (
+        <div
+            ref={elRef}
+            className={cx(className, styles.root)}
+        >
+            <div
+                className={styles.zoomLayer}
+                style={{
+                    transform: `scale(${zoom}) translate(${x}px, ${y}px)`,
+                    transformOrigin: `${xImage}px ${yImage}px`,
+                }}
+            >
+                {children}
+            </div>
+        </div>
+    )
+}
