9bd765b12690a1b054eb831c32154e63f8b16985
Tuomas Koponen <tuomas.koponen@streamr.com>
2019-08-22 14:28:22 +0300

    Fix menu alignment

--- app/src/editor/shared/components/Sidebar/Select.jsx
+++ app/src/editor/shared/components/Sidebar/Select.jsx
@@ -50,20 +50,22 @@ const customStyles = {
         margin: 0,
         borderRadius: '4px',
         boxShadow: '0px 0px 6px 0px rgba(0, 0, 0, 0.1)',
+        left: '-20px',
     }),
     option: (provided, state) => ({
         ...provided,
         fontSize: '14px',
         padding: '0.5rem 1rem',
-        paddingRight: '2rem',
+        paddingRight: '3rem',
         lineHeight: 'normal',
         color: state.isSelected ? 'white' : '#323232',
         position: 'relative',
         // eslint-disable-next-line no-nested-ternary
-        backgroundColor: state.isSelected ? '#0324ff' : null || state.isFocused ? '#f8f8f8' : null,
+        backgroundColor: state.isSelected ? '#0324ff' : state.isFocused ? '#f8f8f8' : null,
         '&:active': {
             backgroundColor: '#f8f8f8',
         },
+        whiteSpace: 'nowrap',
     }),
 }
 
