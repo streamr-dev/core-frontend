34e51d1ac3383b501f538f73a7762ef15e4681d2
Tim Oxley <secoif@gmail.com>
2019-09-02 14:28:37 +0800

    [editor/canvas ShareDialog, userpages CanvasPage/List] Remove canvas embed feature flag. Enable by default.

--- app/src/userpages/components/CanvasPage/List/index.jsx
+++ app/src/userpages/components/CanvasPage/List/index.jsx
@@ -239,7 +239,7 @@ class CanvasList extends Component<Props, State> {
                         resourceType="CANVAS"
                         resourceId={shareDialogCanvas.id}
                         onClose={this.onCloseShareDialog}
-                        allowEmbed={!!process.env.CANVAS_EMBED} // TODO: remove when embed is ready
+                        allowEmbed
                     />
                 )}
                 <ListContainer className={styles.corepageContentContainer}>
