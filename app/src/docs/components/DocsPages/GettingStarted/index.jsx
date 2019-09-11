c067fa7589d621a3009e068c109e40ec5dcdb479
Matthew <mr.matto@gmail.com>
2019-09-11 12:44:06 +0200

    Docs formatting and styles, new Getting started text and image content

--- app/src/docs/components/DocsPages/GettingStarted/index.jsx
+++ app/src/docs/components/DocsPages/GettingStarted/index.jsx
@@ -6,29 +6,17 @@ import { Helmet } from 'react-helmet'
 import DocsLayout from '../../DocsLayout'
 import { subNav } from '../../DocsLayout/Navigation/navLinks'
 
-import GettingStartedIntro from '$docs/content/gettingStarted/intro.mdx'
-import GetYourApiKeys from '$docs/content/gettingStarted/getYourApiKeys.mdx'
-import ConnectEthereumIdentity from '$docs/content/gettingStarted/connectEthereumIdentity.mdx'
+import GettingStartedContent from '$docs/content/gettingStarted/gettingStarted.mdx'
 import GetBuilding from '$docs/content/gettingStarted/getBuilding.mdx'
-import UsefulLinks from '$docs/content/gettingStarted/usefulLinks.mdx'
 
 const GettingStarted = () => (
     <DocsLayout subNav={subNav.gettingStarted}>
         <Helmet title="Getting Started | Streamr Docs" />
         <section>
-            <GettingStartedIntro />
-        </section>
-        <section id="get-api-keys">
-            <GetYourApiKeys />
-        </section>
-        <section id="connecting-ethereum-identity">
-            <ConnectEthereumIdentity />
+            <GettingStartedContent />
         </section>
         <section id="get-building">
             <GetBuilding />
-        </section>
-        <section id="useful-links">
-            <UsefulLinks />
         </section>
     </DocsLayout>
 )
