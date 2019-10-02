eb0dfda38dcd4c0b8e7dac3c6422f47178858762
Tim Oxley <secoif@gmail.com>
2019-10-02 12:16:58 +0800

    [userpages StreamPage/SecurityView] Slider WIP.

---
+++ app/src/userpages/components/StreamPage/Show/SecurityView/Icons.jsx
@@ -0,0 +1,48 @@
+import React from 'react'
+
+export function LockOutlineIcon(props) {
+    return (
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 18 24" {...props}>
+            <g fill="none" fillRule="evenodd" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.5">
+                <rect width="16.5" height="13.5" x="0.75" y="9.75" rx="1.5" />
+                <path d="M3.75 9.75V6a5.25 5.25 0 1110.5 0v3.75M9 15v3" />
+            </g>
+        </svg>
+    )
+}
+
+export function LockIcon(props) {
+    return (
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 19 25" {...props}>
+            <g fill="none" fillRule="evenodd" strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.5" transform="translate(1 1)">
+                <path stroke="currentColor" d="M3 9V5.25a5.25 5.25 0 1110.5 0" />
+                <rect width="16.5" height="13.5" y="9" fill="currentColor" stroke="currentColor" rx="1.5" />
+                <path stroke="#FFF" d="M8.5 14v3" />
+            </g>
+        </svg>
+    )
+}
+
+export function CheckBadgeOutlineIcon(props) {
+    return (
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" {...props}>
+            <g fill="none" fillRule="evenodd" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.5">
+                <path d="M15.75 9.749l-3.981 5.308a.75.75 0 01-1.13.08L8.25 12.749" />
+                {/* eslint-disable-next-line max-len */}
+                <path d="M10.73 1.357a1.63 1.63 0 012.54 0l1.512 1.881c.348.434.893.66 1.446.6l2.4-.261a1.63 1.63 0 011.8 1.8l-.261 2.4c-.06.553.166 1.098.6 1.446l1.881 1.512a1.63 1.63 0 010 2.54l-1.887 1.505a1.63 1.63 0 00-.6 1.447l.261 2.4a1.629 1.629 0 01-1.8 1.8l-2.4-.261a1.628 1.628 0 00-1.446.6L13.27 22.64a1.629 1.629 0 01-2.54 0l-1.511-1.88a1.631 1.631 0 00-1.447-.6l-2.4.261a1.628 1.628 0 01-1.8-1.8l.261-2.4a1.631 1.631 0 00-.6-1.447l-1.88-1.511a1.629 1.629 0 010-2.54l1.88-1.512c.434-.348.66-.893.6-1.446l-.261-2.4a1.629 1.629 0 011.8-1.8l2.4.261a1.632 1.632 0 001.447-.6l1.511-1.869z" />
+            </g>
+        </svg>
+    )
+}
+
+export function CheckBadgeIcon(props) {
+    return (
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" {...props}>
+            <g fill="none" fillRule="evenodd" strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.5">
+                {/* eslint-disable-next-line max-len */}
+                <path fill="currentColor" stroke="currentColor" d="M10.73 1.357a1.63 1.63 0 012.54 0l1.512 1.881c.348.434.893.66 1.446.6l2.4-.261a1.63 1.63 0 011.8 1.8l-.261 2.4c-.06.553.166 1.098.6 1.446l1.881 1.512a1.63 1.63 0 010 2.54l-1.887 1.505a1.63 1.63 0 00-.6 1.447l.261 2.4a1.629 1.629 0 01-1.8 1.8l-2.4-.261a1.628 1.628 0 00-1.446.6L13.27 22.64a1.629 1.629 0 01-2.54 0l-1.511-1.88a1.631 1.631 0 00-1.447-.6l-2.4.261a1.628 1.628 0 01-1.8-1.8l.261-2.4a1.631 1.631 0 00-.6-1.447l-1.88-1.511a1.629 1.629 0 010-2.54l1.88-1.512c.434-.348.66-.893.6-1.446l-.261-2.4a1.629 1.629 0 011.8-1.8l2.4.261a1.632 1.632 0 001.447-.6l1.511-1.869z" />
+                <path stroke="#FFF" d="M15.75 9.749l-3.981 5.308a.75.75 0 01-1.13.08L8.25 12.749" />
+            </g>
+        </svg>
+    )
+}
--- app/src/userpages/components/StreamPage/Show/SecurityView/index.jsx
+++ app/src/userpages/components/StreamPage/Show/SecurityView/index.jsx
@@ -1,6 +1,6 @@
 // @flow
 
-import React, { useCallback } from 'react'
+import React, { useCallback, useLayoutEffect, useRef, useState } from 'react'
 import { connect } from 'react-redux'
 import { I18n } from 'react-redux-i18n'
 import cx from 'classnames'
@@ -9,6 +9,8 @@ import type { Stream } from '$shared/flowtype/stream-types'
 import type { StoreState } from '$shared/flowtype/store-state'
 import { updateEditStream } from '$userpages/modules/userPageStreams/actions'
 import { selectEditedStream } from '$userpages/modules/userPageStreams/selectors'
+
+import * as Icons from './Icons'
 
 import styles from './securityView.pcss'
 
@@ -31,6 +33,10 @@ const securityLevels = {
         title: 'userpages.streams.security.basic.title',
         shortDescription: 'userpages.streams.security.basic.shortDescription',
         longDescription: 'userpages.streams.security.basic.longDescription',
+        icons: {
+            normal: Icons.LockOutlineIcon,
+            selected: Icons.LockOutlineIcon,
+        },
         config: {
             requireSignedData: false,
             requireEncryptedData: false,
@@ -40,6 +46,10 @@ const securityLevels = {
         title: 'userpages.streams.security.signed.title',
         shortDescription: 'userpages.streams.security.signed.shortDescription',
         longDescription: 'userpages.streams.security.signed.longDescription',
+        icons: {
+            normal: Icons.CheckBadgeOutlineIcon,
+            selected: Icons.CheckBadgeIcon,
+        },
         config: {
             requireSignedData: true,
             requireEncryptedData: false,
@@ -49,6 +59,10 @@ const securityLevels = {
         title: 'userpages.streams.security.encrypted.title',
         shortDescription: 'userpages.streams.security.encrypted.shortDescription',
         longDescription: 'userpages.streams.security.encrypted.longDescription',
+        icons: {
+            normal: Icons.LockIcon,
+            selected: Icons.LockIcon,
+        },
         config: {
             requireSignedData: true,
             requireEncryptedData: true,
@@ -98,6 +112,89 @@ function SecurityViewMaybe(props: Props) {
     return <SecurityView {...props} />
 }
 
+function Slider({ index: selectedIndex = 0 } = {}) {
+    const elRef = useRef()
+    const [positions, setPositions] = useState()
+
+    useLayoutEffect(() => {
+        if (!elRef.current) { return }
+        const target = elRef.current.parentElement
+        const inputs = Array.from(target.querySelectorAll('input'))
+        // $FlowFixMe
+        const parent = target.getBoundingClientRect()
+        const subtract = (a, b) => ({
+            width: a.width,
+            height: a.height,
+            left: a.left - b.left,
+            top: a.top - b.top,
+        })
+        setPositions(() => {
+            const rects = inputs.map((input) => input.getBoundingClientRect())
+            const positions = rects.map((rect) => subtract(rect, parent))
+            const first = positions[0]
+            const last = positions[positions.length - 1]
+            const left = first.left + (first.width * 0.5)
+            const width = (last.left + (last.width * 0.5)) - left
+
+            return {
+                positions,
+                left,
+                width,
+            }
+        })
+    }, [])
+
+    if (!positions) {
+        // $FlowFixMe
+        return <div className={styles.SliderBackground} ref={elRef} />
+    }
+
+    const selectedPosition = positions.positions[selectedIndex]
+    console.log({ selectedPosition, positions })
+
+    return (
+        // $FlowFixMe
+        <div className={styles.SecuritySlider} ref={elRef}>
+            <div
+                className={styles.SliderTrack}
+                style={{
+                    left: positions.left,
+                    width: positions.width,
+                }}
+            >
+                {positions.positions.map((p, index) => (
+                    <div
+                        key={p.left}
+                        className={cx(styles.SliderStop, {
+                            [styles.highlighted]: index <= selectedIndex,
+                        })}
+                        style={{
+                            left: (p.left + (p.width * 0.5)) - positions.left,
+                        }}
+                    />
+                ))}
+            </div>
+            <div
+                className={cx(styles.SliderTrack, styles.SliderProgress)}
+                style={{
+                    left: positions.left,
+                    width: (selectedPosition.left + (selectedPosition.width * 0.5)) - positions.left,
+                }}
+            >
+                <div className={styles.SliderThumb}>
+                    <div className={cx(styles.SliderStop, styles.highlighted)} />
+                </div>
+            </div>
+        </div>
+    )
+}
+
+function Icon({ level, selected, ...props }) {
+    const { icons } = securityLevels[level]
+    const CurrentIcon = selected ? icons.selected : icons.normal
+    return <CurrentIcon className={styles.Icon} {...props} />
+}
+
 export function SecurityView(props: Props) {
     const { stream, updateEditStream } = props
     // $FlowFixMe
@@ -116,16 +213,24 @@ export function SecurityView(props: Props) {
                 <strong>{I18n.t(detail.shortDescription)}</strong>&nbsp;
                 {I18n.t(detail.longDescription)}
             </p>
-            <div className={styles.SecuritySlider}>
+            {/* $FlowFixMe */}
+            <div className={cx(styles.SecurityOptions, styles[level])}>
+                <Slider index={levelIndex} />
                 {Object.keys(securityLevels).map((key, index) => (
                     <label
-                        className={cx(styles.SecuritySliderItem, {
+                        key={key}
+                        className={cx(styles.SecurityOption, {
                             [styles.highlighted]: index <= levelIndex,
                             [styles.selected]: index === levelIndex,
                         })}
                     >
+                        <input
+                            type="radio"
+                            checked={index === levelIndex}
+                            onChange={(event) => onChange(event, key)}
+                        />
+                        <Icon level={key} selected={index === levelIndex} />
                         <span>{I18n.t(securityLevels[key].title)}</span>
-                        <input type="radio" checked={index === levelIndex} onChange={(event) => onChange(event, key)} />
                     </label>
                 ))}
             </div>
