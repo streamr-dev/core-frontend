8b92dee929e8d7a277dde9d36c5f28e20012bb8e
Juha Haavisto <juha.haavisto@sujuwa.fi>
2019-10-17 13:53:30 +0300

    Refactor icons component

--- app/src/marketplace/components/ProductPage/EditorNav/Icons.jsx
+++ app/src/marketplace/components/ProductPage/EditorNav/Icons.jsx
@@ -1,83 +1,75 @@
 // @flow
 
 import React from 'react'
+import cx from 'classnames'
 
-const sources = {
-    default: (
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16">
-            <g fill="none" fillRule="evenodd">
-                <circle stroke="#E7E7E7" strokeWidth="1.5" fill="#F8F8F8" cx="8" cy="8" r="7.25" />
-                <circle fill="#E7E7E7" cx="8" cy="8" r="4" />
-            </g>
-        </svg>
-    ),
-    seen: (
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16">
-            <g fill="none" fillRule="evenodd">
-                <circle stroke="#0324FF" strokeWidth="1.5" fill="#F8F8F8" cx="8" cy="8" r="7.25" />
-                <circle fill="#E7E7E7" cx="8" cy="8" r="4" />
-            </g>
-        </svg>
-    ),
-    active: (
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16">
-            <g fill="none" fillRule="evenodd">
-                <circle stroke="#0324FF" strokeWidth="1.5" fill="#F8F8F8" cx="8" cy="8" r="7.25" />
-                <circle fill="#0324FF" cx="8" cy="8" r="4" />
-            </g>
-        </svg>
-    ),
-    activeError: (
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16">
-            <g fill="none" fillRule="evenodd">
-                <circle stroke="#FF5C00" strokeWidth="1.5" fill="#F8F8F8" cx="8" cy="8" r="7.25" />
-                <circle fill="#FF5C00" cx="8" cy="8" r="4" />
-            </g>
-        </svg>
-    ),
-    valid: (
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16">
-            <g fill="none" fillRule="evenodd">
-                <g strokeWidth="1.5" stroke="#0324FF">
-                    <circle cx="8" cy="8" r="7.25" fill="#F8F8F8" />
-                    <path strokeLinecap="round" strokeLinejoin="round" d="M5.306 8.19l1.65 1.792 4.243-4.243" />
-                </g>
-            </g>
-        </svg>
-    ),
-    error: (
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16">
-            <g fill="none" fillRule="evenodd">
-                <g strokeWidth="1.5" stroke="#FF5C00">
-                    <circle fill="#F8F8F8" cx="8" cy="8" r="7.25" />
-                    <path d="M8 5v3.918M7.986 10.904l.014.014" strokeLinecap="round" />
-                </g>
-            </g>
-        </svg>
-    ),
-    unpublished: (
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16">
-            <g fill="none" fillRule="evenodd">
-                <g stroke="#0324FF">
-                    <circle strokeWidth="1.5" fill="#F8F8F8" cx="8" cy="8" r="7.25" />
-                    <g strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.3">
-                        <path d="M10.762 5.612a3.638 3.638 0 00-6.35 2.426v.304M5.34 10.464a3.638 3.638 0 006.348-2.425v-.303" />
-                        <path d="M10.779 8.644l.91-.91.909.91M5.322 7.431l-.91.91-.909-.91" />
-                    </g>
-                </g>
-            </g>
-        </svg>
-    ),
-}
-
-export type IconName = $Keys<typeof sources>
+import styles from './icons.pcss'
 
 type Props = {
-    name: IconName,
+    name: string,
 }
 
-const Icons = ({ name, ...props }: Props) => React.cloneElement(sources[name], {
-    ...props,
-})
+const InnerIcon = ({ name }: Props) => {
+    switch (name) {
+        case 'default':
+        case 'seen':
+        case 'active':
+        case 'activeError':
+            return (
+                <circle
+                    cx="8"
+                    cy="8"
+                    r="4"
+                    className={cx(styles.inner, {
+                        [styles.innerActive]: name === 'active',
+                        [styles.innerError]: name === 'activeError',
+                    })}
+                />
+            )
+
+        case 'valid':
+            return (
+                <path strokeLinecap="round" strokeLinejoin="round" d="M5.306 8.19l1.65 1.792 4.243-4.243" />
+            )
+
+        case 'error':
+            return (
+                <path d="M8 5v3.918M7.986 10.904l.014.014" strokeLinecap="round" />
+            )
+
+        case 'unpublished':
+            return (
+                <g strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.3">
+                    <path d="M10.762 5.612a3.638 3.638 0 00-6.35 2.426v.304M5.34 10.464a3.638 3.638 0 006.348-2.425v-.303" />
+                    <path d="M10.779 8.644l.91-.91.909.91M5.322 7.431l-.91.91-.909-.91" />
+                </g>
+            )
+
+        default:
+            return null
+    }
+}
+
+const Icons = ({ name, ...props }: Props) => (
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" {...props}>
+        <g
+            fill="currentFill"
+            fillRule="evenodd"
+            strokeWidth="1.5"
+            stroke="currentStroke"
+            className={cx(styles.outer, {
+                [styles.outerActive]: !!(name && name !== 'default'),
+                [styles.outerError]: !!(name === 'activeError' || name === 'error'),
+            })}
+        >
+            <circle
+                cx="8"
+                cy="8"
+                r="7.25"
+            />
+            <InnerIcon name={name} />
+        </g>
+    </svg>
+)
 
 export default Icons
