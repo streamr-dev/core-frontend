ed3da39d4ceb07b4204c445de8f6a9467c860732
Tim Oxley <secoif@gmail.com>
2019-10-24 14:02:53 +0800

    [shared/utils getAuthorizationHeader] Send no Bearer header when no session token.

--- app/src/shared/utils/getAuthorizationHeader.js
+++ app/src/shared/utils/getAuthorizationHeader.js
@@ -4,7 +4,8 @@ import SessionProvider from '$auth/components/SessionProvider'
 
 export default () => {
     const token: ?string = SessionProvider.token()
-
+    // no auth header if no token
+    if (!token) { return {} }
     return {
         Authorization: `Bearer ${token || 0}`,
     }
