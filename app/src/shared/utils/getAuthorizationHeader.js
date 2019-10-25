1fab602f0d7b6467d5517ae0813037a703e7e123
Matthew <mr.matto@gmail.com>
2019-10-25 10:21:37 +0200

    Don't send a token if we don't have one. EE API change of behaviour.

--- app/src/shared/utils/getAuthorizationHeader.js
+++ app/src/shared/utils/getAuthorizationHeader.js
@@ -5,6 +5,9 @@ import SessionProvider from '$auth/components/SessionProvider'
 export default () => {
     const token: ?string = SessionProvider.token()
 
+    // no auth header if no token
+    if (!token) { return {} }
+
     return {
         Authorization: `Bearer ${token || 0}`,
     }
