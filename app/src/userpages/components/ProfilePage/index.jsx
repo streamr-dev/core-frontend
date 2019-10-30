cb49cf39d08a2f26813611ce4fd9355444e7a39a
Tim Oxley <secoif@gmail.com>
2019-10-30 12:20:16 +0800

    [editor, userpages, marketplace] Add shadow under nav for canvas, product, stream, profile editors.

--- app/src/userpages/components/ProfilePage/index.jsx
+++ app/src/userpages/components/ProfilePage/index.jsx
@@ -80,6 +80,7 @@ export class ProfilePage extends Component<Props, State> {
                 hideNavOnDesktop
                 navComponent={(
                     <Toolbar
+                        className={Toolbar.styles.shadow}
                         altMobileLayout
                         actions={{
                             cancel: {
