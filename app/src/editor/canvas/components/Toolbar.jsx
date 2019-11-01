2857c1d680963e0587af4742e888fdc297816335
Tim Oxley <secoif@gmail.com>
2019-11-01 16:17:05 +0800

    [editor/canvas Toolbar CanvasSearch] Tune toolbar menu styling.

--- app/src/editor/canvas/components/Toolbar.jsx
+++ app/src/editor/canvas/components/Toolbar.jsx
@@ -35,51 +35,51 @@ function ZoomButtons({ canvas }) {
     const canvasCamera = useCanvasCamera({ canvas })
     const metaKeyLabel = getKeyLabel('meta')
     return (
-        <div className={styles.ZoomButtons}>
-            <button
-                className={cx(styles.ToolbarButton, styles.ZoomButton)}
-                type="button"
-                onClick={() => camera.zoomOut()}
-            >
-                <SvgIcon name="minusSmall" className={styles.icon} />
-            </button>
-            <DropdownActions
-                title={
+        <DropdownActions
+            className={cx(styles.ZoomMenu, styles.DropdownMenu)}
+            noCaret
+            menuProps={{
+                className: styles.DropdownMenuMenu,
+            }}
+            title={
+                <div className={styles.ZoomButtons}>
+                    <button
+                        className={cx(styles.ToolbarButton, styles.ZoomButton)}
+                        type="button"
+                        onClick={(event) => { event.stopPropagation(); camera.zoomOut() }}
+                    >
+                        <SvgIcon name="minusSmall" className={styles.icon} />
+                    </button>
                     <button className={cx(styles.ZoomMenuTrigger)} type="button">
                         {Math.round(camera.scale * 100)}%
                     </button>
-                }
-                noCaret
-                className={cx(styles.ZoomMenu, styles.DropdownMenu)}
-                menuProps={{
-                    className: styles.DropdownMenuMenu,
-                }}
-            >
-                <DropdownActions.Item onClick={() => camera.setScale(0.25)}>25%</DropdownActions.Item>
-                <DropdownActions.Item onClick={() => camera.setScale(0.5)}>50%</DropdownActions.Item>
-                <DropdownActions.Item onClick={() => camera.setScale(1)}>100%</DropdownActions.Item>
-                <DropdownActions.Item divider />
-                <DropdownActions.Item onClick={() => camera.zoomIn()}>
-                    Zoom In <span className={styles.menuShortcut}>{metaKeyLabel}=</span>
-                </DropdownActions.Item>
-                <DropdownActions.Item onClick={() => camera.zoomOut()}>
-                    Zoom Out <span className={styles.menuShortcut}>{metaKeyLabel}-</span>
-                </DropdownActions.Item>
-                <DropdownActions.Item onClick={() => camera.setScale(1)}>
-                    Full Size <span className={styles.menuShortcut}>{metaKeyLabel}0</span>
-                </DropdownActions.Item>
-                <DropdownActions.Item onClick={() => canvasCamera.fitCanvas()}>
-                    Fit Screen <span className={styles.menuShortcut}>{metaKeyLabel}1</span>
-                </DropdownActions.Item>
-            </DropdownActions>
-            <button
-                className={cx(styles.ToolbarButton, styles.ZoomButton)}
-                type="button"
-                onClick={() => camera.zoomIn()}
-            >
-                <SvgIcon name="plusSmall" className={styles.icon} />
-            </button>
-        </div>
+                    <button
+                        className={cx(styles.ToolbarButton, styles.ZoomButton)}
+                        type="button"
+                        onClick={(event) => { event.stopPropagation(); camera.zoomIn() }}
+                    >
+                        <SvgIcon name="plusSmall" className={styles.icon} />
+                    </button>
+                </div>
+            }
+        >
+            <DropdownActions.Item onClick={() => camera.setScale(0.25)}>25%</DropdownActions.Item>
+            <DropdownActions.Item onClick={() => camera.setScale(0.5)}>50%</DropdownActions.Item>
+            <DropdownActions.Item onClick={() => camera.setScale(1)}>100%</DropdownActions.Item>
+            <DropdownActions.Item divider />
+            <DropdownActions.Item onClick={() => camera.zoomIn()}>
+                Zoom In <span className={styles.menuShortcut}>{metaKeyLabel}=</span>
+            </DropdownActions.Item>
+            <DropdownActions.Item onClick={() => camera.zoomOut()}>
+                Zoom Out <span className={styles.menuShortcut}>{metaKeyLabel}-</span>
+            </DropdownActions.Item>
+            <DropdownActions.Item onClick={() => camera.setScale(1)}>
+                Full Size <span className={styles.menuShortcut}>{metaKeyLabel}0</span>
+            </DropdownActions.Item>
+            <DropdownActions.Item onClick={() => canvasCamera.fitCanvas()}>
+                Fit Screen <span className={styles.menuShortcut}>{metaKeyLabel}1</span>
+            </DropdownActions.Item>
+        </DropdownActions>
     )
 }
 
@@ -233,6 +233,7 @@ export default withErrorBoundary(ErrorComponentView)(class CanvasToolbar extends
                                         Open
                                     </R.Button>
                                     <CanvasSearch
+                                        className={styles.CanvasSearch}
                                         canvas={canvas}
                                         isOpen={canvasSearchIsOpen}
                                         open={this.canvasSearchOpen}
