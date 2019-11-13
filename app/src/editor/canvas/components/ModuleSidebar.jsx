c666d73ad89aae7fbea927e8b2a0235de1ea0c1d
Tim Oxley <secoif@gmail.com>
2019-11-13 15:41:11 +0800

    [editor/canvas ModuleSidebar] Coerce NaN option values to 0.

--- app/src/editor/canvas/components/ModuleSidebar.jsx
+++ app/src/editor/canvas/components/ModuleSidebar.jsx
@@ -21,11 +21,11 @@ export default function ModuleSidebar({ canvas, selectedModuleHash, setModuleOpt
         // format value based on option.type
         let value = _value
         if (option.type === 'int') {
-            value = parseInt(value, 10)
+            value = parseInt(value, 10) || 0
         }
 
         if (option.type === 'double') {
-            value = Number(value)
+            value = Number(value) || 0
         }
 
         if (option.type === 'string' || option.type === 'color') {
