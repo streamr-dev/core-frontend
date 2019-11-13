482bdb946bd637085286687c759841920a0536a4
Tim Oxley <secoif@gmail.com>
2019-11-13 13:11:00 +0800

    [userpages LoadingIndicator] Transition in/out with fade.

--- app/src/userpages/components/LoadingIndicator/index.jsx
+++ app/src/userpages/components/LoadingIndicator/index.jsx
@@ -2,20 +2,38 @@
 
 import React from 'react'
 import cx from 'classnames'
+import { useTransition, animated } from 'react-spring'
 
 import styles from './loadingIndicator.pcss'
 
 type Props = {
-    loading: boolean,
+    loading?: boolean,
     className?: string
 }
 
-const LoadingIndicator = (props: Props) => (
-    <div
-        className={cx(styles.loadingIndicator, props.className, {
-            [styles.loading]: props.loading,
-        })}
-    />
-)
+const LoadingIndicator = ({ loading, className }: Props) => {
+    const transitions = useTransition(!!loading, null, {
+        from: {
+            opacity: 0,
+        },
+        enter: {
+            opacity: 1,
+        },
+        leave: {
+            opacity: 0,
+        },
+    })
+    return transitions.map(({ item, key, props }) => (
+        item && (
+            <animated.div
+                key={key}
+                style={props}
+                className={cx(styles.loadingIndicator, className, {
+                    [styles.loading]: loading,
+                })}
+            />
+        )
+    ))
+}
 
 export default LoadingIndicator
