6e2c8a110d87e239851a409694700958a6e3c104
Tim Oxley <secoif@gmail.com>
2019-11-13 15:53:18 +0800

    [userpages LoadingIndicator] Remove wait, debounce loading flag changes instead.

--- app/src/userpages/components/LoadingIndicator/index.jsx
+++ app/src/userpages/components/LoadingIndicator/index.jsx
@@ -1,8 +1,9 @@
 // @flow
 
-import React from 'react'
+import React, { useState, useEffect, useCallback } from 'react'
 import cx from 'classnames'
 import { useTransition, animated } from 'react-spring'
+import { useDebounced } from '$shared/hooks/wrapCallback'
 
 import styles from './loadingIndicator.pcss'
 
@@ -12,7 +13,19 @@ type Props = {
 }
 
 const LoadingIndicator = ({ loading, className }: Props) => {
-    const transitions = useTransition(!!loading, null, {
+    loading = !!loading
+    const [loadingState, setLoadingState] = useState(loading)
+
+    // debounce loading flag changes to avoid flickering loading indicator
+    const updateLoading = useDebounced(useCallback((value) => {
+        setLoadingState(value)
+    }, []), 1000)
+
+    useEffect(() => {
+        updateLoading(loading)
+    }, [loading, setLoadingState, updateLoading])
+
+    const transitions = useTransition(loadingState, null, {
         config: {
             tension: 500,
             friction: 50,
@@ -28,13 +41,14 @@ const LoadingIndicator = ({ loading, className }: Props) => {
             opacity: 0,
         },
     })
+
     return transitions.map(({ item, key, props }) => (
         item && (
             <animated.div
                 key={key}
                 style={props}
                 className={cx(styles.loadingIndicator, className, {
-                    [styles.loading]: loading,
+                    [styles.loading]: loadingState,
                 })}
             />
         )
