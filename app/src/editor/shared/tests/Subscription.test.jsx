68e4bbc3e4f5af4d8630c3a268894bb2cf9788e6
Tim Oxley <secoif@gmail.com>
2019-11-29 13:50:43 +0800

    [editor/shared/tests Subscription] Increase Subscription test timeout.

--- app/src/editor/shared/tests/Subscription.test.jsx
+++ app/src/editor/shared/tests/Subscription.test.jsx
@@ -215,7 +215,7 @@ describe('Subscription', () => {
                         resendLast={0}
                         onSubscribed={async () => {
                             await stream.publish(msg3)
-                            await wait(5000)
+                            await wait(15000)
                             expect(onResending).not.toHaveBeenCalled()
                             expect(messages).toEqual([msg3])
                             result.unmount()
@@ -230,6 +230,6 @@ describe('Subscription', () => {
                     />
                 </ClientProviderComponent>
             ))
-        }, 20000)
+        }, 30000)
     })
 })
