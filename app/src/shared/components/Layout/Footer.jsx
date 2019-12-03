3c5fcacfda926e940437cc72dd322d4c4b42cbf4
Mariusz Roliński <mariusz@mondoreale.pl>
2019-12-03 16:10:25 +0100

    Replace the old footer with the new, shared one
    The new one comes from @streamr/streamr-layout and
    the setup is very minimal.

--- app/src/shared/components/Layout/Footer.jsx
+++ app/src/shared/components/Layout/Footer.jsx
@@ -2,111 +2,80 @@
 
 import React from 'react'
 import { Translate, I18n } from 'react-redux-i18n'
-import FrameFooter, { FooterColumn } from '$shared/components/Footer'
-import { connect } from 'react-redux'
-import { withRouter } from 'react-router-dom'
-import { push } from 'connected-react-router'
+import { Footer as LayoutFooter, FooterColumn } from '@streamr/streamr-layout'
 
-import withI18n, { type I18nProps } from '$shared/containers/WithI18n'
-import { formatPath } from '$shared/utils/url'
 import links from '$shared/../links'
 
-export type DispatchProps = {
-    pushLocation: (string) => void,
-}
+const Footer = () => (
+    <LayoutFooter>
+        <FooterColumn title="Learn">
+            <a href="/">
+                Top
+            </a>
+            <a href="/learn/network">
+                Network
+            </a>
+            <a href="/learn/marketplace">
+                Marketplace
+            </a>
+            <a href="/learn/core">
+                Core
+            </a>
+        </FooterColumn>
+        <FooterColumn title={I18n.t('general.company')}>
+            <a href="/about">
+                <Translate value="general.aboutUs" />
+            </a>
+            <a href="/design">
+                Design Assets
+            </a>
+            <a href={links.blog}>
+                <Translate value="general.blog" />
+            </a>
+        </FooterColumn>
+        <FooterColumn title="Docs">
+            <a href={links.docs.streams}>
+                Streams
+            </a>
+            <a href={links.docs.canvases}>
+                Canvases
+            </a>
+            <a href={links.docs.dashboards}>
+                Dashboards
+            </a>
+            <a href={links.docs.products}>
+                Products
+            </a>
+        </FooterColumn>
+        <FooterColumn title={I18n.t('general.contact')}>
+            <a href={links.contact.general}>
+                <Translate value="general.general" />
+            </a>
+            <a href={links.contact.media}>
+                <Translate value="general.media" />
+            </a>
+            <a href={links.contact.jobs}>
+                <Translate value="general.jobs" />
+            </a>
+            <a href={links.contact.labs}>
+                <Translate value="general.labs" />
+            </a>
+        </FooterColumn>
+        <FooterColumn title={I18n.t('general.documents')}>
+            <a href={I18n.t('urls.whitepaper')}>
+                <Translate value="general.whitepaper" />
+            </a>
+            <a href={links.contributionSummary}>
+                <Translate value="general.contributionSummary" />
+            </a>
+            <a href="https://s3.amazonaws.com/streamr-public/streamr-terms-of-use.pdf">
+                <Translate value="general.terms" />
+            </a>
+            <a href="https://s3.amazonaws.com/streamr-public/streamr-privacy-policy.pdf">
+                <Translate value="general.privacy" />
+            </a>
+        </FooterColumn>
+    </LayoutFooter>
+)
 
-type Props = I18nProps & DispatchProps & {
-    location: {
-        pathname: string,
-    },
-}
-
-class Footer extends React.Component<Props> {
-    onSelectLanguage = (locale: string) => {
-        const { pushLocation, location: { pathname } } = this.props
-
-        pushLocation(formatPath(pathname, {
-            locale,
-        }))
-    }
-
-    render() {
-        const { language, translations } = this.props
-        const languages = Object.keys(translations).map((lang) => ({
-            name: typeof translations[lang] === 'object' ? translations[lang].language.name : '',
-            lang,
-        }))
-
-        return (
-            <FrameFooter
-                currentLanguage={language}
-                languages={languages}
-                localeUrlFormatter={(pathname: ?string, locale: string) => (
-                    formatPath(pathname || '', {
-                        locale,
-                    })
-                )}
-                onSelectLanguage={this.onSelectLanguage}
-            >
-                <FooterColumn title="Learn">
-                    <a href="/">
-                        Top
-                    </a>
-                    <a href="/learn/network">
-                        Network
-                    </a>
-                    <a href="/learn/marketplace">
-                        Marketplace
-                    </a>
-                    <a href="/learn/core">
-                        Core
-                    </a>
-                </FooterColumn>
-                <FooterColumn title={I18n.t('general.company')}>
-                    <a href="/about">
-                        <Translate value="general.aboutUs" />
-                    </a>
-                    <a href={links.blog}>
-                        <Translate value="general.blog" />
-                    </a>
-                </FooterColumn>
-                <FooterColumn title={I18n.t('general.contact')}>
-                    <a href={links.contact.general}>
-                        <Translate value="general.general" />
-                    </a>
-                    <a href={links.contact.media}>
-                        <Translate value="general.media" />
-                    </a>
-                    <a href={links.contact.jobs}>
-                        <Translate value="general.jobs" />
-                    </a>
-                    <a href={links.contact.labs}>
-                        <Translate value="general.labs" />
-                    </a>
-                </FooterColumn>
-                <FooterColumn title={I18n.t('general.documents')}>
-                    <a href={I18n.t('urls.whitepaper')}>
-                        <Translate value="general.whitepaper" />
-                    </a>
-                    <a href={links.contributionSummary}>
-                        <Translate value="general.contributionSummary" />
-                    </a>
-                    <a href="https://s3.amazonaws.com/streamr-public/streamr-terms-of-use.pdf">
-                        <Translate value="general.terms" />
-                    </a>
-                    <a href="https://s3.amazonaws.com/streamr-public/streamr-privacy-policy.pdf">
-                        <Translate value="general.privacy" />
-                    </a>
-                </FooterColumn>
-            </FrameFooter>
-        )
-    }
-}
-
-const mapDispatchToProps = (dispatch: Function): DispatchProps => ({
-    pushLocation: (location: string) => (
-        dispatch(push(location))
-    ),
-})
-
-export default connect(null, mapDispatchToProps)(withRouter(withI18n(Footer)))
+export default Footer
