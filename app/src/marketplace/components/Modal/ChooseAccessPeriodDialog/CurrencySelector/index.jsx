c770ad726a28b2f2e451e8772344eecfb893b32a
Matthew <mr.matto@gmail.com>
2019-12-20 12:33:08 +0200

    currency selector component

---
+++ app/src/marketplace/components/Modal/ChooseAccessPeriodDialog/CurrencySelector/index.jsx
@@ -0,0 +1,43 @@
+// @flow
+
+import React from 'react'
+import cx from 'classnames'
+
+import type { PaymentCurrency } from '$shared/flowtype/common-types'
+import SvgIcon from '$shared/components/SvgIcon'
+
+import styles from './currencySelector.pcss'
+
+type Props = {
+    onChange: (currency: PaymentCurrency) => void,
+    paymentCurrency: PaymentCurrency,
+}
+
+const CurrencySelector = ({ onChange, paymentCurrency }: Props) => (
+    <div className={styles.root}>
+        {['DATA', 'ETH', 'DAI'].map((currency, index) => // eslint-disable-next-line react/no-array-index-key
+            <CurrencyButton key={index} currency={currency} onChange={onChange} selected={currency === paymentCurrency} />)}
+    </div>
+)
+
+type CurrencyButtonProps = {
+    onChange: (currency: PaymentCurrency) => void,
+    selected: boolean,
+    currency: PaymentCurrency,
+}
+
+const CurrencyButton = ({ currency, selected, onChange }: CurrencyButtonProps) => (
+    // eslint-disable-next-line jsx-a11y/click-events-have-key-events, jsx-a11y/no-static-element-interactions
+    <div
+        key={currency}
+        className={cx(styles.currencyButton, {
+            [styles.selected]: selected,
+        })}
+        onClick={() => onChange(currency)}
+    >
+        <SvgIcon name={currency} className={styles.currencyIcon} />
+        <span className={styles.name}>{currency}</span>
+    </div>
+)
+
+export default CurrencySelector
