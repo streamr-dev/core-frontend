53181ef6e037c2bd3666335651dbcf03b4a17fcc
Mariusz Roliński <mariusz@mondoreale.pl>
2020-01-07 14:52:52 +0100

    Protect from updating the unmounted

--- app/src/shared/hooks/useAccountAddress.js
+++ app/src/shared/hooks/useAccountAddress.js
@@ -3,9 +3,12 @@
 import { useState, useEffect } from 'react'
 import { getWeb3 } from '$shared/web3/web3Provider'
 import Web3Poller from '$shared/web3/web3Poller'
+import useIsMounted from '$shared/hooks/useIsMounted'
 
 export default (): ?string => {
     const [address, setAddress] = useState()
+
+    const isMounted = useIsMounted()
 
     useEffect(() => {
         (async () => {
@@ -14,7 +17,11 @@ export default (): ?string => {
             } catch (e) {
                 return undefined
             }
-        })().then(setAddress)
+        })().then((addr) => {
+            if (isMounted()) {
+                setAddress(addr)
+            }
+        })
 
         Web3Poller.subscribe(Web3Poller.events.ACCOUNT, setAddress)
 
