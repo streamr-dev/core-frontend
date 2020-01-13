c5341fed8ca509d8d07c575a7af6445e65375672
Matthew <fonty1@users.noreply.github.com>
2020-01-13 11:00:51 +0200

    Extend streamr.info() (#772)
    * pull package versions and streamrclient version from package json in streamr.info()
    
    * streamr-client version from pkg lock

--- app/src/shared/utils/diagnostics.js
+++ app/src/shared/utils/diagnostics.js
@@ -1,15 +1,17 @@
 // You should init the Sentry browser SDK as soon as possible during your application load up, before initializing React
 // https://docs.sentry.io/platforms/javascript/react/
 import '../../analytics'
+import { dependencies } from '$app/package-lock'
 
-/* Attach global diagnostic object */
 const navigator = global.navigator || {}
+const streamrClientVersion = dependencies['streamr-client'].version
 
 global.streamr = Object.assign(global.streamr || {}, {
     info() {
         const info = {
             userAgent: navigator.userAgent,
             environment: process.env.NODE_ENV,
+            streamrClientVersion,
         }
 
         if (process.env.TRAVIS_TAG) {
