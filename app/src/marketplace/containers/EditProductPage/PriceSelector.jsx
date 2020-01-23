6552179ea295b5127f5ccddd0ec10325c847afbc
Mariusz Roliński <mariusz@mondoreale.pl>
2020-01-23 10:04:18 +0100

    Remove pre-fill for now

--- app/src/marketplace/containers/EditProductPage/PriceSelector.jsx
+++ app/src/marketplace/containers/EditProductPage/PriceSelector.jsx
@@ -1,6 +1,6 @@
 // @flow
 
-import React, { useState, useCallback, useContext, useEffect } from 'react'
+import React, { useState, useCallback, useContext } from 'react'
 import { useSelector } from 'react-redux'
 import cx from 'classnames'
 import { Translate } from 'react-redux-i18n'
@@ -13,7 +13,6 @@ import RadioButtonGroup from '$shared/components/RadioButtonGroup'
 import SetPrice from '$mp/components/SetPrice'
 import Toggle from '$shared/components/Toggle'
 import { selectContractProduct } from '$mp/modules/contractProduct/selectors'
-import useAccountAddress from '$shared/hooks/useAccountAddress'
 
 import { Context as ValidationContext } from '../ProductController/ValidationContextProvider'
 import useEditableProduct from '../ProductController/useEditableProduct'
@@ -65,14 +64,6 @@ const PriceSelector = () => {
     const isFreeProduct = !!product.isFree
 
     const { isValid, message } = useValidation('pricePerSecond')
-
-    const accountAddress = useAccountAddress()
-
-    useEffect(() => {
-        if (!product.beneficiaryAddress && accountAddress) {
-            updateBeneficiaryAddress(accountAddress)
-        }
-    }, [product.beneficiaryAddress, updateBeneficiaryAddress, accountAddress])
 
     return (
         <section id="price" className={cx(styles.root, styles.PriceSelector)}>
