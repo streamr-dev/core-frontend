7e05078a327c583dedcf8ee99fd5d08f2e0a24e6
Henri Pihkala <hpihkala@users.noreply.github.com>
2020-02-21 15:54:28 +0100

    validate tests using cleanup branch of streamr-docker-dev

---
+++ app/travis_scripts/start-docker-env.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+set -e
+##
+## Pulls down streamr-docker-dev and starts the stack except the frontend ##
+##
+
+source "${BASH_SOURCE%/*}/utils.sh"
+
+cd "$TRAVIS_BUILD_DIR" || exit 1
+sudo /etc/init.d/mysql stop
+sudo sysctl fs.inotify.max_user_watches=524288; sudo sysctl -p
+sudo ifconfig docker0 10.200.10.1/24
+
+# TODO: remove "--branch cleanup" before merging
+git clone --branch cleanup https://github.com/streamr-dev/streamr-docker-dev.git
+
+streamr_docker_dev='streamr-docker-dev/streamr-docker-dev/bin.sh'
+
+# start everything except the frontend
+$streamr_docker_dev start --except platform --wait
