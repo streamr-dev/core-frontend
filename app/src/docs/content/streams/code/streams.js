b9d48794a43cd9876e6bcf9f78b6f5f3ce71a131
mthambipillai <melchior.thambipillai@streamr.com>
2020-03-26 18:16:37 +0100

    apply pr comments

--- app/src/docs/content/streams/code/streams.js
+++ app/src/docs/content/streams/code/streams.js
@@ -54,3 +54,58 @@ msg.put("random", Math.random());
 
 // Then publish it!
 client.publish(stream, msg);`
+
+export const encryptionJavaClient =
+`Stream stream = new Stream("my-data-stream");
+
+//---- PUBLISHER SIDE ----
+
+// an AES 256 bits key that is initially known only to the publisher
+UnencryptedGroupKey groupKey = '0x...';
+// streamId → group key used to encrypt data
+HashMap<String, UnencryptedGroupKey> publisherGroupKeys = new HashMap<>();
+publisherGroupKeys.put(stream.getId(), groupKey);
+EncryptionOptions encryptionOptions = new EncryptionOptions(publisherGroupKeys, …);
+StreamrClient publisher = new StreamrClient(new StreamrClientOptions(..., encryptionOptions, …))
+
+Map<String, Object> messageContent = …;
+// going to be published encrypted with ‘groupKey’
+publisher.publish(stream, messageContent);
+
+//---- SUBSCRIBER SIDE ----
+
+StreamrClient subscriber = new StreamrClient(...);
+// under the hood, it will use the protocol to securely obtain
+// ‘groupKey’ to then decrypt the received data
+subscriber.subscribe(stream, …);`
+
+export const encryptionJavascriptClient =
+`//---- PUBLISHER SIDE ----
+
+// an AES 256 bits key that is initially known only to the publisher
+const groupKey = '0x...'
+const publisher = new StreamrClient({
+    publisherGroupKeys: {
+        'my-stream-id': groupKey
+    }
+})
+const msg = {
+    ...
+}
+// going to be published encrypted with ‘groupKey’
+publisher.publish('my-stream-id', msg)
+
+//---- SUBSCRIBER SIDE ----
+
+const subscriber = new StreamrClient(...)
+// under the hood, it will use the protocol to securely obtain
+// ‘groupKey’ to then decrypt the received data
+subscriber.subscribe('my-stream-id', …);`
+
+export const rekeyJavaClient =
+`Stream stream = new Stream(...);
+client.rekey(stream);`
+
+export const rekeyJavascriptClient =
+`client.rekey('my-stream-id')
+`
