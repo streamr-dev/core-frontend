0e120754280c7061cec201df4d0844de4703c3b7
Matthew <mr.matto@gmail.com>
2020-04-01 15:11:03 +0300

    tests come back 🎉

---
+++ app/src/editor/shared/tests/ModuleSubscription.test.jsx
@@ -0,0 +1,133 @@
+import React from 'react'
+import { mount } from 'enzyme'
+
+import { setupAuthorizationHeader, loadModuleDefinition } from '$editor/shared/tests/utils'
+import api from '$editor/shared/utils/api'
+import { ClientProviderComponent, createClient } from '$shared/contexts/StreamrClient'
+import ModuleSubscription from '$editor/shared/components/ModuleSubscription'
+import * as State from '$editor/canvas/state'
+import * as Services from '$editor/canvas/services'
+
+function throwError(err) {
+    throw err
+}
+
+function wait(delay) {
+    return new Promise((resolve) => setTimeout(resolve, delay))
+}
+
+describe('Canvas Subscriptions', () => {
+    let teardown
+    let apiKey
+
+    beforeAll(async () => {
+        teardown = await setupAuthorizationHeader()
+    }, 60000)
+
+    afterAll(async () => {
+        await teardown()
+    })
+
+    beforeAll(async () => {
+        const [key] = await api().get(`${process.env.STREAMR_API_URL}/users/me/keys`).then(({ data }) => data)
+        apiKey = key.id
+    })
+
+    describe('create subscription', () => {
+        let client
+
+        async function setup() {
+            client = await createClient(apiKey)
+            client.on('error', throwError)
+        }
+
+        async function teardown() {
+            if (client) {
+                await client.ensureDisconnected()
+                client.off('error', throwError)
+                client = undefined
+            }
+        }
+
+        beforeEach(async () => {
+            await teardown()
+            await setup()
+        })
+
+        xit('should get canvas module subscription messages', async (done) => {
+            let canvas = await Services.create()
+            canvas = State.addModule(canvas, await loadModuleDefinition('Clock'))
+            const clock = canvas.modules.find((m) => m.name === 'Clock')
+            canvas = State.addModule(canvas, await loadModuleDefinition('Table'))
+            const table = canvas.modules.find((m) => m.name === 'Table')
+            const clockDateOut = State.findModulePort(canvas, clock.hash, (p) => p.name === 'date')
+            const tableIn1 = State.findModulePort(canvas, table.hash, (p) => p.displayName === 'in1')
+            canvas = State.updateCanvas(State.connectPorts(canvas, clockDateOut.id, tableIn1.id))
+            const startedCanvas = State.updateCanvas(await Services.start(canvas))
+            const runningTable = startedCanvas.modules.find((m) => m.name === 'Table')
+            const messages = []
+
+            const result = mount((
+                <ClientProviderComponent apiKey={apiKey}>
+                    <ModuleSubscription
+                        module={runningTable}
+                        onMessage={(message) => {
+                            messages.push(message)
+                        }}
+                        isSubscriptionActive
+                    />
+                </ClientProviderComponent>
+            ))
+
+            await wait(10000)
+            const receivedMessages = messages.slice() // copy before unmounting
+            result.unmount()
+            await Services.stop(canvas)
+            const newRowMessages = receivedMessages.filter(({ nr }) => nr)
+            // should have roughly 10 new row messages
+            expect(newRowMessages.length).toBeTruthy()
+            expect(newRowMessages.length).toBeGreaterThanOrEqual(8)
+            expect(newRowMessages.length).toBeLessThanOrEqual(13)
+            done()
+        }, 15000)
+
+        it('should get canvas module subscription messages in restarted canvas', async (done) => {
+            let canvas = await Services.create()
+            canvas = State.addModule(canvas, await loadModuleDefinition('Clock'))
+            const clock = canvas.modules.find((m) => m.name === 'Clock')
+            canvas = State.addModule(canvas, await loadModuleDefinition('Table'))
+            const table = canvas.modules.find((m) => m.name === 'Table')
+            const clockDateOut = State.findModulePort(canvas, clock.hash, (p) => p.name === 'date')
+            const tableIn1 = State.findModulePort(canvas, table.hash, (p) => p.displayName === 'in1')
+            canvas = State.updateCanvas(State.connectPorts(canvas, clockDateOut.id, tableIn1.id))
+            canvas = State.updateCanvas(await Services.start(canvas))
+            const messages = []
+
+            const result = mount((
+                <ClientProviderComponent apiKey={apiKey}>
+                    <ModuleSubscription
+                        module={canvas.modules.find((m) => m.name === 'Table')}
+                        onMessage={(message) => {
+                            messages.push(message)
+                        }}
+                        isSubscriptionActive
+                    />
+                </ClientProviderComponent>
+            ))
+
+            canvas = State.updateCanvas(await Services.stop(canvas))
+            canvas = State.updateCanvas(await Services.start(canvas))
+
+            await wait(10000)
+            const receivedMessages = messages.slice() // copy before unmounting
+            result.unmount()
+            await Services.stop(canvas)
+            const newRowMessages = receivedMessages.filter(({ nr }) => nr)
+            // should have roughly 10 new row messages
+            expect(newRowMessages.length).toBeTruthy()
+            expect(newRowMessages.length).toBeGreaterThanOrEqual(8)
+            expect(newRowMessages.length).toBeLessThanOrEqual(13)
+            done()
+        }, 20000)
+    })
+})
---
+++ app/src/editor/shared/tests/Subscription.test.jsx
@@ -0,0 +1,235 @@
+import React, { useState } from 'react'
+import { mount } from 'enzyme'
+import { setupAuthorizationHeader } from '$editor/shared/tests/utils'
+import { act } from 'react-dom/test-utils'
+import uniqueId from 'lodash/uniqueId'
+
+import api from '../utils/api'
+import * as Services from '../services'
+import { ClientProviderComponent, createClient } from '$shared/contexts/StreamrClient'
+import Subscription from '$shared/components/Subscription'
+
+function throwError(err) {
+    throw err
+}
+
+function wait(delay) {
+    return new Promise((resolve) => setTimeout(resolve, delay))
+}
+
+describe('Subscription', () => {
+    let teardown
+    let apiKey
+
+    beforeAll(async () => {
+        teardown = await setupAuthorizationHeader()
+    }, 60000)
+
+    afterAll(async () => {
+        await teardown()
+    })
+
+    beforeAll(async () => {
+        const [key] = await api().get(`${process.env.STREAMR_API_URL}/users/me/keys`).then(Services.getData)
+        apiKey = key.id
+    })
+
+    describe('create subscription', () => {
+        let client
+        let stream
+
+        async function setup() {
+            client = await createClient(apiKey)
+            client.on('error', throwError)
+            stream = await client.getOrCreateStream({
+                name: uniqueId(),
+            })
+        }
+
+        async function teardown() {
+            if (client) {
+                await client.ensureDisconnected()
+                client.off('error', throwError)
+                client = undefined
+            }
+        }
+
+        beforeEach(async () => {
+            await teardown()
+            await setup()
+        })
+
+        afterEach(async () => {
+            await teardown()
+        })
+
+        it('unsubscribes on isActive false', async (done) => {
+            const Test = () => {
+                const [isActive, setIsActive] = useState(true)
+                return (
+                    <ClientProviderComponent apiKey={apiKey}>
+                        <Subscription
+                            uiChannel={stream}
+                            onSubscribed={() => {
+                                act(() => {
+                                    setIsActive(false)
+                                })
+                            }}
+                            onUnsubscribed={() => {
+                                done()
+                            }}
+                            isActive={isActive}
+                        />
+                    </ClientProviderComponent>
+                )
+            }
+
+            mount(<Test />)
+        }, 15000)
+
+        it('can create subscription', async (done) => {
+            const msg = { test: uniqueId() }
+            const result = mount((
+                <ClientProviderComponent apiKey={apiKey}>
+                    <Subscription
+                        uiChannel={stream}
+                        onSubscribed={() => {
+                            stream.publish(msg)
+                        }}
+                        onMessage={(received) => {
+                            expect(received).toEqual(msg)
+                            result.unmount()
+                            done()
+                        }}
+                        isActive
+                    />
+                </ClientProviderComponent>
+            ))
+        })
+
+        it('unsubscribes on unmount', async (done) => {
+            const sub = React.createRef()
+            const result = mount((
+                <ClientProviderComponent apiKey={apiKey}>
+                    <Subscription
+                        ref={sub}
+                        uiChannel={stream}
+                        resendLast={1}
+                        onResent={() => {
+                            // don't unmount on subscribed as this
+                            // breaks the client
+                            result.unmount()
+                        }}
+                        onNoResend={() => {
+                            sub.current.subscription.once('unsubscribed', () => {
+                                done()
+                            })
+                            // don't care if resent or not, just unmount
+                            result.unmount()
+                        }}
+                        isActive
+                    />
+                </ClientProviderComponent>
+            ))
+        })
+
+        it('onNoResend works', async (done) => {
+            const messages = []
+            const onResending = jest.fn()
+            const result = mount((
+                <ClientProviderComponent apiKey={apiKey}>
+                    <Subscription
+                        uiChannel={stream}
+                        resendLast={2}
+                        onMessage={(message) => {
+                            messages.push(message)
+                        }}
+                        onResending={onResending}
+                        onNoResend={() => {
+                            expect(onResending).not.toHaveBeenCalled()
+                            expect(messages).toEqual([])
+                            result.unmount()
+                            done()
+                        }}
+                        isActive
+                    />
+                </ClientProviderComponent>
+            ))
+        })
+
+        it('can use resendLast values', async (done) => {
+            const msg1 = { msg: uniqueId() }
+            const msg2 = { msg: uniqueId() }
+            const msg3 = { msg: uniqueId() }
+            await stream.publish(msg1)
+            await stream.publish(msg2)
+            await stream.publish(msg3)
+            const messages = []
+            const onResending = jest.fn()
+
+            await wait(10000) // wait for above messages to flush
+
+            const result = mount((
+                <ClientProviderComponent apiKey={apiKey}>
+                    <Subscription
+                        uiChannel={stream}
+                        resendLast={2}
+                        onMessage={(message) => {
+                            messages.push(message)
+                        }}
+                        onResending={onResending}
+                        onResent={() => {
+                            // wait for messages to onMessage
+                            setTimeout(() => {
+                                expect(onResending).toHaveBeenCalled()
+                                expect(messages).toEqual([
+                                    msg2,
+                                    msg3,
+                                ])
+                                result.unmount()
+                                done()
+                            }, 1000)
+                        }}
+                        isActive
+                    />
+                </ClientProviderComponent>
+            ))
+        }, 15000)
+
+        xit('can use resendLast 0', async (done) => {
+            const msg1 = { msg: uniqueId() }
+            const msg2 = { msg: uniqueId() }
+            const msg3 = { msg: uniqueId() }
+            await stream.publish(msg1)
+            await stream.publish(msg2)
+            const messages = []
+            const onResending = jest.fn()
+            const onNoResend = jest.fn()
+
+            await wait(10000) // wait for above messages to flush
+
+            const result = mount((
+                <ClientProviderComponent apiKey={apiKey}>
+                    <Subscription
+                        uiChannel={stream}
+                        resendLast={0}
+                        onSubscribed={async () => {
+                            await stream.publish(msg3)
+                            await wait(15000)
+                            expect(onResending).not.toHaveBeenCalled()
+                            expect(messages).toEqual([msg3])
+                            result.unmount()
+                            done()
+                        }}
+                        onMessage={(message) => {
+                            messages.push(message)
+                        }}
+                        onResending={onResending}
+                        onNoResend={onNoResend}
+                        isActive
+                    />
+                </ClientProviderComponent>
+            ))
+        }, 30000)
+    })
+})
