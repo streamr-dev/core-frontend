9c3a335822b5388ee5c4ac7cf9151d9c4efb7de5
Mariusz Roliński <mariusz@mondoreale.pl>
2020-04-23 15:27:33 +0200

    Reuse scrollTo for integrity
    `scrollTo` checks if target exists first allowing us to drop even
    more code.

--- app/src/shared/utils/scrollTo.js
+++ app/src/shared/utils/scrollTo.js
@@ -1,11 +1,12 @@
 import scrollIntoView from 'smooth-scroll-into-view-if-needed'
 
-export default (el) => {
+export default (el, ifNeeded = false) => {
     if (el) {
         scrollIntoView(el, {
             behavior: 'smooth',
             block: 'start',
             inline: 'nearest',
+            scrollMode: ifNeeded ? 'if-needed' : 'always',
         })
     }
 }
