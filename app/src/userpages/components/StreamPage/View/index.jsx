9630bf9098b6508e127e43ab34e9197a83085b12
Mariusz Roliński <mariusz@mondoreale.pl>
2020-04-27 13:26:56 +0200

    Convert to TOCPage

--- app/src/userpages/components/StreamPage/View/index.jsx
+++ app/src/userpages/components/StreamPage/View/index.jsx
@@ -6,38 +6,12 @@ import { Translate, I18n } from 'react-redux-i18n'
 import Layout from '$shared/components/Layout/Core'
 import Label from '$ui/Label'
 import UnstyledText from '$ui/Text'
-import { REGULAR, MEDIUM } from '$shared/utils/styled'
+import { MEDIUM } from '$shared/utils/styled'
+import TOCPage from '$shared/components/TOCPage'
+import TOCSection from '$shared/components/TOCPage/TOCSection'
 import Preview, { Controls } from '../Edit/PreviewView'
 import { getSecurityLevelConfig } from '../Edit/SecurityView'
 import { convertFromStorageDays } from '../Edit/HistoryView'
-
-const BusStop = () => null
-
-const Body = styled.div`
-    margin: 0 auto;
-    padding-bottom: 104px;
-    width: 680px;
-
-    p {
-        line-height: 1.5em;
-        margin: 0;
-    }
-
-    strong {
-        font-weight: ${MEDIUM};
-    }
-
-    ${Controls} {
-        border: solid #ebebeb;
-        border-width: 0 1px 1px;
-    }
-`
-
-const Title = styled.h1`
-    font-size: 36px;
-    font-weight: ${REGULAR};
-    margin: 0 0 64px;
-`
 
 const Details = styled.div`
     border: solid #e7e7e7;
@@ -63,17 +37,12 @@ const UnstyledDetail = ({ title, children, ...props }: any) => (
 )
 
 const Detail = styled(UnstyledDetail)`
+    padding-top: 2px;
+
     & + & {
         width: 144px;
     }
 `
-
-const UnstyledSection = ({ title, children, ...props }: any) => (
-    <div {...props}>
-        <h2>{title}</h2>
-        {children}
-    </div>
-)
 
 const Text = styled(UnstyledText)`
     &[disabled] {
@@ -85,22 +54,6 @@ const Text = styled(UnstyledText)`
     ${({ centered }) => !!centered && css`
         text-align: center;
     `}
-`
-
-const Section = styled(UnstyledSection)`
-    margin-top: 104px;
-
-    h2 {
-        border-bottom: 1px solid #e7e7e7;
-        font-size: 24px;
-        font-weight: ${REGULAR};
-        margin: 0 0 32px;
-        padding-bottom: 20px;
-    }
-
-    ${Text} + * {
-        margin-left: 16px;
-    } 
 `
 
 const Field = styled.div`
@@ -140,42 +93,40 @@ const Field = styled.div`
     `}
 `
 
-const View = ({ stream, currentUser }: any) => {
+const UnstyledView = ({ stream, currentUser, ...props }: any) => {
     const { amount: storagePeriod, unit } = convertFromStorageDays(stream.storageDays)
 
     const { shortDescription, longDescription } = getSecurityLevelConfig(stream)
 
     return (
-        <Layout>
-            <Body>
-                <Title>
-                    {stream.name}
-                </Title>
-                <BusStop id="details" />
-                <Details>
-                    <Detail title="Description">
-                        {stream.description}
-                    </Detail>
-                </Details>
-                <Details>
-                    <Detail title="Stream ID">
-                        {stream.id}
-                    </Detail>
-                    <Detail title="Partitions">
-                        {stream.partitions}
-                    </Detail>
-                </Details>
-                <Section title="Preview">
+        <Layout {...props}>
+            <TOCPage title={stream.name}>
+                <TOCSection id="details" linkTitle="Details">
+                    <Details>
+                        <Detail title="Description">
+                            {stream.description}
+                        </Detail>
+                    </Details>
+                    <Details>
+                        <Detail title="Stream ID">
+                            {stream.id}
+                        </Detail>
+                        <Detail title="Partitions">
+                            {stream.partitions}
+                        </Detail>
+                    </Details>
+                </TOCSection>
+                <TOCSection id="preview" title="Preview">
                     <Preview currentUser={currentUser} stream={stream} />
-                </Section>
-                <Section title="Security">
+                </TOCSection>
+                <TOCSection id="security" title="Security">
                     <p>
                         <Translate value={shortDescription} tag="strong" />
                         {' '}
                         <Translate value={longDescription} />
                     </p>
-                </Section>
-                <Section title="Fields">
+                </TOCSection>
+                <TOCSection id="fields" title="Fields">
                     <Field head>
                         <Label>Field name</Label>
                         <Label>Data type</Label>
@@ -190,8 +141,8 @@ const View = ({ stream, currentUser }: any) => {
                             />
                         </Field>
                     ))}
-                </Section>
-                <Section title="Historical Data">
+                </TOCSection>
+                <TOCSection id="historicalData" title="Historical Data">
                     <Label>Stored data</Label>
                     <Field>
                         <Text
@@ -217,10 +168,30 @@ const View = ({ stream, currentUser }: any) => {
                             disabled
                         />
                     </Field>
-                </Section>
-            </Body>
+                </TOCSection>
+            </TOCPage>
         </Layout>
     )
 }
+
+const View = styled(UnstyledView)`
+    p {
+        line-height: 1.5em;
+        margin: 0;
+    }
+
+    strong {
+        font-weight: ${MEDIUM};
+    }
+
+    ${Controls} {
+        border: solid #ebebeb;
+        border-width: 0 1px 1px;
+    }
+
+    ${Text} + * {
+        margin-left: 16px;
+    }
+`
 
 export default View
