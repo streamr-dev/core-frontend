300e09835feea2bd083d8bf5bea14c5f2a4ddd37
juhah <juha.a.haavisto@gmail.com>
2020-05-07 08:52:35 +0300

    Update app/src/docs/components/Pages/DataUnions/UXBestPractices/index.jsx
    Co-authored-by: Matthew <fonty1@users.noreply.github.com>

--- app/src/docs/components/Pages/DataUnions/UXBestPractices/index.jsx
+++ app/src/docs/components/Pages/DataUnions/UXBestPractices/index.jsx
@@ -5,7 +5,7 @@ import DocsHelmet from '$docs/components/DocsHelmet'
 import DocsLayout from '$docs/components/DocsLayout'
 import UXBestPracticesContent from '$docs/content/dataUnions/uxBestPractices.mdx'
 
-const UXBestPractises = () => (
+const UXBestPractices = () => (
     <DocsLayout>
         <DocsHelmet pageTitle="Data Unions UX best practices" />
         <section>
