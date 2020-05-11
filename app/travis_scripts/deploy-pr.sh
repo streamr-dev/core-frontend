314b3b0e23a308e7dffd418bdfc347e18c6366b8
Matthew <mr.matto@gmail.com>
2020-05-11 16:29:38 +0300

    build-index to travis scripts

--- app/travis_scripts/deploy-pr.sh
+++ app/travis_scripts/deploy-pr.sh
@@ -28,6 +28,7 @@ echo $WEB_ACL_ID
 docker run -e AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID -e AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY -e AWS_DEFAULT_REGION=eu-west-1 streamr/infra-marketplace-pr:stg  "(terraform init;terraform apply -auto-approve -var 'bucket_name=streamr-marketplace-pr-$TRAVIS_PULL_REQUEST_SHA' -var 'waf_acl_id=$WEB_ACL_ID')"
 CONTAINER_ID=$(docker ps -a -q)
 docker commit $CONTAINER_ID streamr/infra-marketplace-pr:stg
+npm run build-index
 npm run build
 $(dirname $0)/build-storybook.sh
 aws s3 sync --region eu-west-1 dist s3://streamr-marketplace-pr-$TRAVIS_PULL_REQUEST_SHA
