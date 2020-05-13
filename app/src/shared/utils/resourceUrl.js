bb8d11bbadfb3b889469f80b55f83de26544fd46
Mariusz Roliński <mariusz@mondoreale.pl>
2020-05-13 12:30:47 +0200

    Restructure useEmbed hook
    I define 2 new utilities:
    - canvasEmbedCode
    - resourceUrl

---
+++ app/src/shared/utils/resourceUrl.js
@@ -0,0 +1,30 @@
+// @flow
+
+import routes from '$routes'
+
+export const ResourceType = {
+    CANVAS: 'CANVAS',
+    DASHBOARD: 'DASHBOARD',
+    STREAM: 'STREAM',
+}
+
+export type ResourceID = string | number | null | void
+
+export default (resourceType: $Keys<typeof ResourceType>, id: ResourceID) => {
+    switch (resourceType) {
+        case ResourceType.CANVAS:
+            return routes.canvases.public.edit({
+                id,
+            })
+        case ResourceType.DASHBOARD:
+            return routes.dashboards.public.edit({
+                id,
+            })
+        case ResourceType.STREAM:
+            return routes.userpages.public.stream({
+                id,
+            })
+        default:
+            throw new Error(`Invalid resource type "${resourceType}"`)
+    }
+}
