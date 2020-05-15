7be11bf89228d6b310ceb14f1ddba804521c363f
Mariusz Roliński <mariusz@mondoreale.pl>
2020-05-15 19:10:27 +0200

    Reindent / unwrap

--- app/cypress/integration/auth/LoginPage.spec.js
+++ app/cypress/integration/auth/LoginPage.spec.js
@@ -1,45 +1,43 @@
 describe('Login page', () => {
-    describe('logging in', () => {
-        it('lets users log in', () => {
-            cy.visit('/login')
-            cy.get('[type=email]').type('tester1@streamr.com')
-            cy.get('button').click()
-            cy.get('[type=password]').type('tester1TESTER1')
-            cy.get('button').click()
-            cy.location('pathname').should('eq', '/core/streams')
-        })
+    it('lets users log in', () => {
+        cy.visit('/login')
+        cy.get('[type=email]').type('tester1@streamr.com')
+        cy.get('button').click()
+        cy.get('[type=password]').type('tester1TESTER1')
+        cy.get('button').click()
+        cy.location('pathname').should('eq', '/core/streams')
+    })
 
-        it('complains on invalid credentials', () => {
-            cy.visit('/login')
-            cy.get('[type=email]').type('tester1@streamr.com')
-            cy.get('button').click()
-            cy.get('[type=password]').type('invalid password')
-            cy.get('button').click()
-            cy.contains(/invalid username or password/i)
-            cy.location('pathname').should('eq', '/login')
-        })
+    it('complains on invalid credentials', () => {
+        cy.visit('/login')
+        cy.get('[type=email]').type('tester1@streamr.com')
+        cy.get('button').click()
+        cy.get('[type=password]').type('invalid password')
+        cy.get('button').click()
+        cy.contains(/invalid username or password/i)
+        cy.location('pathname').should('eq', '/login')
+    })
 
-        it('complains on invalid email address', () => {
-            cy.visit('/login')
-            cy.get('button').click()
-            cy.contains(/email is required/i)
-            cy.get('[type=email]').type('tester1')
-            cy.get('button').click()
-            cy.get('[type="email"]').then((i) => {
-                // eslint-disable-next-line no-unused-expressions
-                expect(i[0].validationMessage).to.not.be.empty
-            })
-            cy.location('pathname').should('eq', '/login')
+    it('complains on invalid email address', () => {
+        cy.visit('/login')
+        cy.get('button').click()
+        cy.contains(/email is required/i)
+        cy.get('[type=email]').type('tester1')
+        cy.get('button').click()
+        cy.get('[type="email"]').then((i) => {
+            // eslint-disable-next-line no-unused-expressions
+            expect(i[0].validationMessage).to.not.be.empty
         })
+        cy.location('pathname').should('eq', '/login')
+    })
 
-        it('complains on invalid password', () => {
-            cy.visit('/login')
-            cy.get('[type=email]').type('tester1@streamr.com')
-            cy.get('button').click()
-            cy.get('button').click()
-            cy.contains(/password is required/i)
-            cy.location('pathname').should('eq', '/login')
-        })
+    it('complains on invalid password', () => {
+        cy.visit('/login')
+        cy.get('[type=email]').type('tester1@streamr.com')
+        cy.get('button').click()
+        cy.get('button').click()
+        cy.contains(/password is required/i)
+        cy.location('pathname').should('eq', '/login')
     })
 
     describe('Navigation / logo', () => {
