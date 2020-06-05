aff368a477fc8244e94fcb9f77733186aa676b1c
Juha Haavisto <juha.haavisto@sujuwa.fi>
2020-06-05 16:38:31 +0300

    Refactor transaction list, fix transaction fetching

--- app/src/userpages/modules/transactionHistory/services.js
+++ app/src/userpages/modules/transactionHistory/services.js
@@ -9,98 +9,93 @@ import { getPublicWeb3 } from '$shared/web3/web3Provider'
 import getConfig from '$shared/web3/config'
 import type { HashList, TransactionEntityList, TransactionEntity, EventLog, EventLogList } from '$shared/flowtype/web3-types'
 import TransactionError from '$shared/errors/TransactionError'
-// import { getUnprefixedHexString } from '$mp/utils/smartContract'
+import type { ProductIdList } from '$mp/flowtype/product-types'
+
+const eventTypeToTransactionType = {
+    ProductCreated: transactionTypes.CREATE_CONTRACT_PRODUCT,
+    ProductUpdated: transactionTypes.UPDATE_CONTRACT_PRODUCT,
+    ProductDeleted: transactionTypes.UNDEPLOY_PRODUCT,
+    ProductRedeployed: transactionTypes.REDEPLOY_PRODUCT,
+}
 
 const getInputValues = (type, logs) => {
-    const logValues = abiDecoder.decodeLogs(logs)
+    const logValues = (abiDecoder.decodeLogs(logs) || []).filter(Boolean)
 
     switch (type) {
-        case 'PaymentReceived':
-            // hack to handle minting coins to account (also a transfer event)
-            if (logValues[1].name === 'Minted') {
-                return {
-                    productId: '0x0',
-                    type: transactionTypes.PAYMENT,
-                    value: logValues[1].events[0].value,
-                }
-            }
+        case 'PaymentReceived': {
+            const { events: subscribedEvents } = logValues.find(({ name }) => name === 'Subscribed') || {}
+            const { events: transferEvents } = logValues.find(({ name }) => name === 'Transfer') || {}
+            const { value: productId } = (subscribedEvents && subscribedEvents.find(({ name }) => name === 'productId')) || {}
+            const { value: tokens } = (transferEvents && transferEvents.find(({ name }) => name === 'tokens')) || {}
 
             return {
-                productId: logValues[1].events[0].value,
+                productId,
                 type: transactionTypes.PAYMENT,
-                value: logValues[2].events[2].value,
+                value: tokens,
             }
+        }
 
-        case 'PaymentSent':
+        case 'PaymentSent': {
+            const { events: subscribedEvents } = logValues.find(({ name }) => name === 'Subscribed') || {}
+            const { events: transferEvents } = logValues.find(({ name }) => name === 'Transfer') || {}
+            const { value: productId } = (subscribedEvents && subscribedEvents.find(({ name }) => name === 'productId')) || {}
+            const { value: tokens } = (transferEvents && transferEvents.find(({ name }) => name === 'tokens')) || {}
+
             return {
-                productId: logValues[1].events[0].value,
+                productId,
                 type: transactionTypes.PURCHASE,
-                value: BN(logValues[2].events[2].value).negated(),
+                value: BN(tokens).negated(),
             }
+        }
 
         case 'ProductCreated':
-            return {
-                productId: logValues[0].events[1].value,
-                type: transactionTypes.CREATE_CONTRACT_PRODUCT,
-                value: '0',
-            }
-
         case 'ProductUpdated':
-            return {
-                productId: logValues[0].events[1].value,
-                type: transactionTypes.UPDATE_CONTRACT_PRODUCT,
-                value: '0',
-            }
-
         case 'ProductDeleted':
-            return {
-                productId: logValues[0].events[1].value,
-                type: transactionTypes.UNDEPLOY_PRODUCT,
-                value: '0',
-            }
+        case 'ProductRedeployed': {
+            const { events } = logValues.find(({ name }) => name === type) || {}
+            const { value: productId } = (events && events.find(({ name }) => name === 'id')) || {}
 
-        case 'ProductRedeployed':
             return {
-                productId: logValues[0].events[1].value,
-                type: transactionTypes.REDEPLOY_PRODUCT,
+                productId,
+                type: eventTypeToTransactionType[type],
                 value: '0',
             }
+        }
 
         default:
             return {}
     }
 }
 
-export const getTransactionEvents = (addresses: HashList): Promise<EventLogList> => {
+export const getTransactionEvents = (addresses: HashList, products: ProductIdList): Promise<EventLogList> => {
     const web3 = getPublicWeb3()
     const { marketplace, dataToken } = getConfig()
-    const marketPlaceContract = new web3.eth.Contract(marketplace.abi, marketplace.address)
-    const tokenContract = new web3.eth.Contract(dataToken.abi, dataToken.address)
+
+    // these are needed to decode log values
     abiDecoder.addABI(marketplace.abi)
     abiDecoder.addABI(dataToken.abi)
 
+    const marketPlaceContract = new web3.eth.Contract(marketplace.abi, marketplace.address)
     const rawEvents = []
 
     // Get past events by filtering with the indexed address parameter.
     const eventNames = [{
-        contract: tokenContract, name: 'Transfer', filter: 'to', type: 'PaymentReceived',
+        contract: marketPlaceContract, name: 'Subscribed', filter: { productId: products }, type: 'PaymentReceived',
     }, {
-        contract: tokenContract, name: 'Transfer', filter: 'from', type: 'PaymentSent',
+        contract: marketPlaceContract, name: 'Subscribed', filter: { subscriber: addresses }, type: 'PaymentSent',
     }, {
-        contract: marketPlaceContract, name: 'ProductCreated', filter: 'owner', type: 'ProductCreated',
+        contract: marketPlaceContract, name: 'ProductCreated', filter: { owner: addresses }, type: 'ProductCreated',
     }, {
-        contract: marketPlaceContract, name: 'ProductUpdated', filter: 'owner', type: 'ProductUpdated',
+        contract: marketPlaceContract, name: 'ProductUpdated', filter: { owner: addresses }, type: 'ProductUpdated',
     }, {
-        contract: marketPlaceContract, name: 'ProductDeleted', filter: 'owner', type: 'ProductDeleted',
+        contract: marketPlaceContract, name: 'ProductDeleted', filter: { owner: addresses }, type: 'ProductDeleted',
     }, {
-        contract: marketPlaceContract, name: 'ProductRedeployed', filter: 'owner', type: 'ProductRedeployed',
+        contract: marketPlaceContract, name: 'ProductRedeployed', filter: { owner: addresses }, type: 'ProductRedeployed',
     }]
     const eventTypes = eventNames.map(({ type }) => type)
     const eventPromises = eventNames.map(({ contract, name, filter }) => contract.getPastEvents(name, {
         fromBlock: 1,
-        filter: {
-            [(filter: string)]: addresses,
-        },
+        filter,
     }))
     const transactionCounts = {}
 
@@ -138,25 +133,31 @@ export const getTransactionsFromEvents = (events: EventLogList): Promise<Transac
         web3.eth.getTransactionReceipt(event.transactionHash),
         web3.eth.getBlock(event.blockHash),
     ])))
-        .then(([...transactions]) =>
-            transactions.map(([event, tx, receipt, block]): TransactionEntity => {
-                const rest = {}
+        .then(([...transactions]) => transactions.map(([event, tx, receipt, block]): TransactionEntity => {
+            const rest = {}
 
-                if (receipt.status === true) {
-                    rest.receipt = receipt
-                } else {
-                    rest.error = new TransactionError(I18n.t('error.txFailed'), receipt)
-                }
+            if (receipt.status === true) {
+                rest.receipt = receipt
+            } else {
+                rest.error = new TransactionError(I18n.t('error.txFailed'), receipt)
+            }
 
-                return {
-                    id: event.id,
-                    hash: tx.hash,
-                    state: 'completed',
-                    gasUsed: receipt.gasUsed,
-                    gasPrice: tx.gas,
-                    timestamp: block.timestamp,
-                    ...getInputValues(event.type, receipt.logs),
-                    ...rest,
-                }
-            }))
+            let inputValues
+            try {
+                inputValues = getInputValues(event.type, receipt.logs)
+            } catch (e) {
+                console.warn(e)
+            }
+
+            return {
+                id: event.id,
+                hash: tx.hash,
+                state: 'completed',
+                gasUsed: receipt.gasUsed,
+                gasPrice: tx.gas,
+                timestamp: block.timestamp,
+                ...(inputValues || {}),
+                ...rest,
+            }
+        }))
 }
