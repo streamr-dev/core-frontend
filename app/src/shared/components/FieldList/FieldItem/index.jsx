a45e2fde054dc3f1fd3533aa3d079be13c932405
Matthew <mr.matto@gmail.com>
2020-07-03 16:48:34 +0200

    Fix fields section alignment, replace meatball with trash can

--- app/src/shared/components/FieldList/FieldItem/index.jsx
+++ app/src/shared/components/FieldList/FieldItem/index.jsx
@@ -1,19 +1,53 @@
 // @flow
 
 import React, { type Node } from 'react'
+import styled from 'styled-components'
 
 import Handle from '../Handle'
+import SvgIcon from '$shared/components/SvgIcon'
 import styles from './fieldItem.pcss'
+
+const Trash = styled.button`
+    display: none;
+    position: absolute;
+    width: 40px;
+    top: 20px;
+    right: 8px;
+    border: none;
+    background-color: transparent;
+    border-radius: 4px;
+
+    & > svg {
+        width: 18px;
+
+        > path {
+            stroke: #A3A3A3;
+        }
+    }
+
+    &:hover,
+    &:active {
+        background-color: #E7E7E7;
+
+        & > svg > path {
+            stroke: #323232;
+        }
+    }
+`
 
 type Props = {
     children: Node,
+    onDelete: () => void,
 }
 
-const FieldItem = ({ children }: Props) => (
+const FieldItem = ({ children, onDelete }: Props) => (
     <div className={styles.root}>
         <div className={styles.inner}>
             <Handle className={styles.handle} />
             {children}
+            <Trash onClick={onDelete} className={styles.trashIcon}>
+                <SvgIcon name="trash" />
+            </Trash>
         </div>
     </div>
 )
