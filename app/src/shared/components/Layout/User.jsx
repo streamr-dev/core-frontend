06db69b3c6c1d804d1f7409cf614ee0fe26f645e
Mariusz Roliński <mariusz@mondoreale.pl>
2020-07-20 13:52:43 +0200

    Add user avatar to narrow nav; more tweaks

--- app/src/shared/components/Layout/User.jsx
+++ app/src/shared/components/Layout/User.jsx
@@ -2,22 +2,30 @@ import React from 'react'
 import styled from 'styled-components'
 import { isEthereumAddress } from '$mp/utils/validate'
 import { truncate } from '$shared/utils/text'
-import { MEDIUM } from '$shared/utils/styled'
+import { MEDIUM, MD as TABLET } from '$shared/utils/styled'
+import Avatar from '$shared/components/Avatar'
 
 const Name = styled.div`
-    font-size: 14px;
     font-weight: ${MEDIUM};
+    line-height: 1em;
 `
 
 const Username = styled.div`
     color: #a3a3a3;
-    font-size: 12px;
+    line-height: 1em;
 `
 
-const UnstyledUser = ({ source, ...props }) => (
+const EmptyUser = {
+    imageUrlSmall: undefined,
+    name: '',
+    username: '',
+}
+
+const UnstyledAvatarless = ({ source = EmptyUser, ...props }) => (
     <div {...props}>
         <Name>
             {source.name}
+            &zwnj;
         </Name>
         <Username>
             {isEthereumAddress(source.username) ? (
@@ -27,15 +35,56 @@ const UnstyledUser = ({ source, ...props }) => (
             ) : (
                 source.username
             )}
+            &zwnj;
         </Username>
     </div>
 )
 
+const Avatarless = styled(UnstyledAvatarless)``
+
+const UnstyledUser = ({ source = EmptyUser, ...props }) => (
+    <div {...props}>
+        <Avatar
+            alt={source.name}
+            src={source.imageUrlSmall}
+            css={typeof source.imageUrlSmall === 'undefined' && `
+                visibility: hidden;
+            `}
+        />
+        <Avatarless
+            source={source}
+        />
+    </div>
+)
+
 const User = styled(UnstyledUser)`
-    line-height: 20px;
+    align-items: center;
+    display: flex;
+
+    ${Avatar} {
+        border-radius: 50%;
+        flex: 0 0 40px;
+        overflow: hidden;
+    }
+
+    ${Avatarless} {
+        flex-grow: 1;
+        margin-left: 16px;
+    }
+
+    @media (min-width: ${TABLET}px) {
+        ${Avatar} {
+            flex-basis: 80px;
+        }
+
+        ${Avatarless} {
+            margin-left: 40px;
+        }
+    }
 `
 
 Object.assign(User, {
+    Avatarless,
     Name,
     Username,
 })
