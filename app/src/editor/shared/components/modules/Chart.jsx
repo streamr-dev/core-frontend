7a43788e0c3ef271b0e61069dd56498961ac438d
Mariusz Roliński <mariusz@mondoreale.pl>
2020-08-10 12:03:08 +0200

    Unused

--- app/src/editor/shared/components/modules/Chart.jsx
+++ app/src/editor/shared/components/modules/Chart.jsx
@@ -1,4 +1,4 @@
-import React, { useRef, useCallback, useState, useEffect } from 'react'
+import React, { useRef, useCallback, useState } from 'react'
 import throttle from 'lodash/throttle'
 
 import useIsMounted from '$shared/hooks/useIsMounted'
