8056467e199bc3e3c07af0cc7cf919d37e3c5f4f
Mariusz Roliński <mariusz@mondoreale.pl>
2020-09-16 13:23:03 +0200

    Too much powahhh!

--- app/src/userpages/components/ShareSidebar/ErrorMessage.jsx
+++ app/src/userpages/components/ShareSidebar/ErrorMessage.jsx
@@ -1,10 +1,15 @@
 import styled from 'styled-components'
+import { MEDIUM } from '$shared/utils/styled'
 
 const ErrorMessage = styled.div`
     background-color: #fff4f5;
     color: #ff0f2d;
     font-size: 14px;
     line-height: 20px;
+
+    strong {
+        font-weight: ${MEDIUM};
+    }
 `
 
 export default ErrorMessage
