fe348e03a48c12b5823d5129e10edb06779ab986
Mariusz Roliński <mariusz@mondoreale.pl>
2020-11-11 15:56:38 +0100

    Optimize permission sorting
    How about that boiz?

--- app/src/userpages/components/ShareSidebar/Sidebar.jsx
+++ app/src/userpages/components/ShareSidebar/Sidebar.jsx
@@ -181,29 +181,17 @@ const UnstyledShareSidebar = (({ className, ...props }) => {
     const editableUsers = Object.assign({}, currentUsers)
     delete editableUsers.anonymous
 
-    // current user (own permission) is displayed on top
-    let currentUserId = []
+    const userSet = new Set([
+        currentUser,
+        ...newUserIdList,
+        ...Object.keys(editableUsers).sort(),
+    ])
 
-    if (editableUsers[currentUser]) {
-        currentUserId = [currentUser]
+    if (!editableUsers[currentUser]) {
+        userSet.delete(currentUser)
     }
 
-    // users are listed in order:
-    // current user, if exists
-    // new users in order added
-    // old users in alphabetical order
-    const oldUserIdList = Object.keys(editableUsers)
-        .filter((userId) => !newUserIdList.includes(userId) && userId !== currentUser)
-        .sort()
-
-    // remove current user
-    const newUserIdListFiltered = newUserIdList.filter((userId) => userId !== currentUser)
-
-    const userEntries = [
-        ...currentUserId,
-        ...newUserIdListFiltered,
-        ...oldUserIdList,
-    ].map((userId) => [userId, editableUsers[userId]])
+    const userEntries = [...userSet].map((userId) => [userId, editableUsers[userId]])
 
     return (
         <div className={className}>
