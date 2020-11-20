9ef27a7a191d59f8bf16f2eaa925c23381d96e85
Mariusz Roliński <mariusz@mondoreale.pl>
2020-11-20 06:50:48 +0100

    count → countOperations

--- app/src/shared/components/PermissionsProvider/utils/identifyGroup.js
+++ app/src/shared/components/PermissionsProvider/utils/identifyGroup.js
@@ -1,11 +1,9 @@
 import groups, { DEFAULTS_KEYS } from '../groups'
-import count from './count'
+import countOperations from './countOperations'
 
 export default function identifyGroup(resourceType, combination) {
-    return (
-        Object.entries(groups[resourceType]).reduce((memo, [group, groupCombination]) => (
-            // eslint-disable-next-line no-bitwise
-            count(combination & groupCombination) >= count(groupCombination) ? group : memo
-        ), DEFAULTS_KEYS[resourceType])
-    )
+    return Object.entries(groups[resourceType]).reduce((memo, [group, groupCombination]) => (
+        // eslint-disable-next-line no-bitwise
+        countOperations(combination & groupCombination) === countOperations(groupCombination) ? group : memo
+    ), DEFAULTS_KEYS[resourceType])
 }
