08d6d9da9bbc75c16129643e90fc699355c3509c
Mariusz Roliński <mariusz@mondoreale.pl>
2021-01-12 15:20:32 +0100

    Tweaks

--- app/travis_scripts/e2e-tests.sh
+++ app/travis_scripts/e2e-tests.sh
@@ -6,9 +6,13 @@ npm run build
 
 $(dirname $0)/start-docker-env.sh
 
-npx node-static -p 3333 -a 0.0.0.0 ./dist
+npx node-static -p 3333 -a 0.0.0.0 ./dist &
 
+# Wait for Core
 npx wait-on http://localhost:3333
+
+# Wait for e&e
+npx wait-on http://localhost/api/v1/categories
 
 npm run cypress
 
