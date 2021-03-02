Paul-Rogen/streamr-platform

Use streamr-client alpha-9

app/src/marketplace/containers/ProductController/useEditableProductActions.js
@@ -122,13 +122,18 @@ export function useEditableProductActions() {
         }))
         touch('pricePerSecond')
     }, [commit, touch])
-    const updateBeneficiaryAddress = useCallback((beneficiaryAddress: $ElementType<Product, 'beneficiaryAddress'>) => {
-        commit('Update beneficiary address', (p) => ({
-            ...p,
-            beneficiaryAddress,
-        }))
-        touch('beneficiaryAddress')
-    }, [commit, touch])
+    const updateBeneficiaryAddress = useCallback(
+        (beneficiaryAddress: $ElementType<Product, 'beneficiaryAddress'>, didTouch: boolean = true) => {
+            commit('Update beneficiary address', (p) => ({
+                ...p,
+                beneficiaryAddress,
+            }))
+            if (didTouch) {
+                touch('beneficiaryAddress')
+            }
+        },
+        [commit, touch],
+    )
     const updateType = useCallback((type: $ElementType<Product, 'type'>) => {
         commit('Update type', (p) => ({
             ...p,

streamr-dev/core-frontend

app/src/marketplace/containers/ProductController/useEditableProductActions.js
@@ -121,14 +121,19 @@ export function useEditableProductActions() {
             timeUnit,
         }))
         setTouched('pricePerSecond')
-    }, [commit, setTouched])
-    const updateBeneficiaryAddress = useCallback((beneficiaryAddress: $ElementType<Product, 'beneficiaryAddress'>) => {
-        commit('Update beneficiary address', (p) => ({
-            ...p,
-            beneficiaryAddress,
-        }))
-        setTouched('beneficiaryAddress')
-    }, [commit, setTouched])
+    }, [commit, touch])
+    const updateBeneficiaryAddress = useCallback(
+        (beneficiaryAddress: $ElementType<Product, 'beneficiaryAddress'>, didTouch: boolean = true) => {
+            commit('Update beneficiary address', (p) => ({
+                ...p,
+                beneficiaryAddress,
+            }))
+            if (didTouch) {
+                setTouched('beneficiaryAddress')
+            }
+        },
+        [commit, setTouched],
+    )
     const updateType = useCallback((type: $ElementType<Product, 'type'>) => {
         commit('Update type', (p) => ({
             ...p,