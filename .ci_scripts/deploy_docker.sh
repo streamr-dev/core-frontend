6a90d8e83ecfffeddba417b7f7f3572cbaab87d0
Tuomas Koponen <tuomas.koponen@streamr.com>
2021-05-21 12:28:31 +0300

    Fix tagged builds

--- .ci_scripts/deploy_docker.sh
+++ .ci_scripts/deploy_docker.sh
@@ -1,28 +1,20 @@
 #!/bin/bash
 if [ "$1" == "dev" ]; then
-    # If the build is a cron build then it should tag and push a nightly build but if it is not a cronjob
-    # then it is just another dev tag and push
-    if [ "$TRAVIS_EVENT_TYPE" == "cron" ]; then
-        # The script detects that there is a cron job through the variable TRAVIS_EVENT_TYPE which will be
-        # 'cron' if the build is triggered by a cron job
-        echo "Tag Nightly"
-        nightly_build=nightly-$(date '+%Y-%m-%d')
-        docker tag "$OWNER/$IMAGE_NAME:local" "$OWNER/$IMAGE_NAME:$nightly_build"
-        docker tag "$OWNER/$IMAGE_NAME:local" "$OWNER/$IMAGE_NAME:nightly"
-        ## Push Nightly
-        docker push "$OWNER/$IMAGE_NAME:$nightly_build"
-        docker push "$OWNER/$IMAGE_NAME:nightly"
-    else
-        echo "Tag dev"
-        docker tag "$OWNER/$IMAGE_NAME:local" "$OWNER/$IMAGE_NAME:$1"
-        ## Push dev
-        docker push "$OWNER/$IMAGE_NAME:$1"
-    fi
+    echo "Tag dev"
+    docker tag "$OWNER/$IMAGE_NAME:local" "$OWNER/$IMAGE_NAME:$1"
+    ## Push dev
+    docker push "$OWNER/$IMAGE_NAME:$1"
+elif [ "$1" == "nightly" ]; then
+    echo "Tag Nightly"
+    nightly_build=nightly-$(date '+%Y-%m-%d')
+    docker tag "$OWNER/$IMAGE_NAME:local" "$OWNER/$IMAGE_NAME:$nightly_build"
+    docker tag "$OWNER/$IMAGE_NAME:local" "$OWNER/$IMAGE_NAME:nightly"
+    ## Push Nightly
+    docker push "$OWNER/$IMAGE_NAME:$nightly_build"
+    docker push "$OWNER/$IMAGE_NAME:nightly"
 elif [ "$1" == "production" ]; then
     echo "Tag Production latest/tag"
-    docker tag "$OWNER/$IMAGE_NAME:local" "$OWNER/$IMAGE_NAME:$TRAVIS_TAG"
-    docker tag "$OWNER/$IMAGE_NAME:local" "$OWNER/$IMAGE_NAME:$TAG"
+    docker tag "$OWNER/$IMAGE_NAME:local" "$OWNER/$IMAGE_NAME:$2"
     ## Push Production
-    docker push "$OWNER/$IMAGE_NAME:$TRAVIS_TAG"
-    docker push "$OWNER/$IMAGE_NAME:$TAG"
+    docker push "$OWNER/$IMAGE_NAME:$2"
 fi
