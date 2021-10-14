65f0c2fd8613bbdf2449ecd899bb69c1b7db448d
Henri Pihkala <hpihkala@users.noreply.github.com>
2021-10-14 17:38:21 +0300

    docs: using a light node

--- app/src/shared/snippets/streams.js
+++ app/src/shared/snippets/streams.js
@@ -1,7 +1,7 @@
 export const CreateJavascriptClient =
 `const streamr = new StreamrClient({
     auth: {
-        privateKey: 'YOUR-PRIVATE-KEY',
+        privateKey: 'your-ethereum-private-key',
     },
 })`
 
@@ -19,8 +19,8 @@ export const AuthJavaClient = 'StreamrClient client = new StreamrClient(new Ethe
 
 export const SubscribeJavascriptClient =
 `// Subscribe to a stream
-const subscription = client.subscribe({
-    stream: 'MY-STREAM-ID',
+const subscription = await client.subscribe({
+    stream: 'my-stream-id',
 },
 (message) => {
     // This function will be called when new messages occur
@@ -37,15 +37,14 @@ export const SubscribeJavaClient =
 })`
 
 export const PublishJavascriptClient =
-`// Here is the event we'll be sending
+`// Here is the message we'll be sending
 const msg = {
     hello: 'world',
     random: Math.random()
 }
 
-// Publish the event to the Stream
-client.publish('MY-STREAM-ID', msg)
-    .then(() => console.log('Sent successfully: ', msg))`
+// Publish the message to the Stream
+await client.publish('my-stream-id', msg)`
 
 export const PublishJavaClient =
 `// Create the message payload, which is represented as a Map
