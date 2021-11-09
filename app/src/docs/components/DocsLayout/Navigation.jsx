b62e25cc797910b143043c12333833b294992f72
Mariusz Roliński <mariusz@mondoreale.pl>
2021-11-09 12:02:37 +0100

    Tweak ToC

--- app/src/docs/components/DocsLayout/Navigation.jsx
+++ app/src/docs/components/DocsLayout/Navigation.jsx
@@ -2,20 +2,33 @@ import React, { useState, useMemo, useCallback } from 'react'
 import { Link, useLocation } from 'react-router-dom'
 import styled, { css } from 'styled-components'
 
-import { LG, MD, REGULAR, MEDIUM } from '$shared/utils/styled'
+import { LG as DESKTOP, MD as TABLET, REGULAR, MEDIUM } from '$shared/utils/styled'
 import scrollTo from '$shared/utils/scrollTo'
 import UnstyledElevatedContainer from '$shared/components/ElevatedContainer'
 import SvgIcon from '$shared/components/SvgIcon'
 
 import docsMap from '$docs/docsMap'
 
+const NavItemContent = styled.div`
+    align-items: center;
+    display: flex;
+`
+
+const CaretWrap = styled.div`
+    flex: 0;
+    padding: 0 9px; /* Caret is 6px wide. We need 24. */
+
+    svg {
+        display: block;
+    }
+`
+
 const SubNavList = styled.ul`
     border-left: 1px solid #D8D8D8;
     display: none;
     list-style-type: none;
-    margin-bottom: 0.2em;
-    margin-top: 1.2em;
-    padding-left: 1.2em;
+    margin: 8px 0 8px 11px; /* 1 (border) + 15 + 16 = 32. */
+    padding: 0 0 0 12px;
     position: sticky;
     text-align: left;
 `
@@ -44,14 +57,19 @@ const NavListItem = styled.li`
         line-height: 1.5em;
         list-style: none;
         margin-bottom: 0;
-        margin-bottom: 1em;
         padding: 0;
     }
 
+    ${SubNavList} &:first-child a {
+        padding-top: 0;
+    }
+
+    ${SubNavList} &:last-child a {
+        padding-bottom: 0;
+    }
+
     ${SubNavList} & a {
-        color: #A3A3A3;
         display: block;
-        padding: 0;
         text-decoration: none;
         width: 100%;
     }
@@ -72,6 +90,16 @@ const NavListItem = styled.li`
         ${SubNavList} & a {
             font-weight: ${MEDIUM};
         }
+
+        svg {
+            transform: rotate(90deg);
+        }
+    `}
+
+    ${({ $hasSubitems }) => !$hasSubitems && css`
+        svg {
+            visibility: hidden;
+        }
     `}
 `
 
@@ -91,8 +119,19 @@ const UnstyledTableOfContents = ({ children, ...props }) => {
 
                 return ( // eslint-disable-next-line react/no-array-index-key
                     <React.Fragment key={index}>
-                        <NavListItem $active={isActive}>
-                            <Link to={navItem.root.path}>{topLevelNav}</Link>
+                        <NavListItem $active={isActive} $hasSubitems={subItems.length}>
+                            <NavItemContent>
+                                <CaretWrap>
+                                    <svg width="6" height="6" viewBox="0 0 6 6" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                        <path
+                                            // eslint-disable-next-line max-len
+                                            d="M.75.486v5.028c0 .383.422.616.747.408l3.951-2.514c.3-.19.3-.626 0-.82L1.498.077A.484.484 0 0 0 .75.486z"
+                                            fill="#A3A3A3"
+                                        />
+                                    </svg>
+                                </CaretWrap>
+                                <Link to={navItem.root.path}>{topLevelNav}</Link>
+                            </NavItemContent>
                         </NavListItem>
                         {subItems.length > 0 && (
                             <SubNavList>
@@ -117,15 +156,16 @@ const UnstyledTableOfContents = ({ children, ...props }) => {
 }
 
 const TableOfContents = styled(UnstyledTableOfContents)`
+    list-style: none;
     position: relative;
-    margin: 0.5rem 0 1.5rem 0;
+    margin: 0.5rem 0 1.5rem;
     max-height: 100vh;
     padding: 0;
     overflow: scroll;
     overflow-x: hidden;
     scrollbar-width: none;
 
-    @media (min-width: ${LG}px) {
+    @media (min-width: ${DESKTOP}px) {
         display: block;
         max-height: calc(100vh - 32px - 4.5rem);
     }
@@ -143,10 +183,10 @@ const MobileHeader = styled(NavListItem)`
         cursor: pointer;
 
         a {
-        width: calc(100% - 60px);
-        overflow: hidden;
-        white-space: nowrap;
-        text-overflow: ellipsis;
+            width: calc(100% - 60px);
+            overflow: hidden;
+            white-space: nowrap;
+            text-overflow: ellipsis;
         }
 
         :empty::after {
@@ -167,7 +207,7 @@ const DropdownCaret = styled(SvgIcon)`
     transition: transform 180ms ease-in-out;
 
     &:hover {
-      cursor: pointer;
+        cursor: pointer;
     }
 `
 
@@ -251,12 +291,16 @@ const Responsive = styled(UnstyledResponsive)`
 
     && ${TableOfContents} {
         margin: 0 auto;
-        padding: 1em 0;
-        padding-left: 30px;
+        padding: 1em 0 48px;
+        padding-left: 24px;
         max-width: 540px;
 
-        @media (min-width: ${MD}px) {
+        @media (min-width: ${TABLET}px) {
             max-width: 720px;
+        }
+
+        @media (min-width: ${DESKTOP}px) {
+            padding-bottom: 1em;
         }
     }
 `
