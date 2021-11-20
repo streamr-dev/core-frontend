50224f92206d4c479d5c8102dc3e02b2d89ff897
Juha Haavisto <juha.haavisto@sujuwa.fi>
2021-11-20 14:54:23 +0200

    Remove permission reducer state

--- app/src/userpages/flowtype/states/store-state.js
+++ app/src/userpages/flowtype/states/store-state.js
@@ -1,9 +1,7 @@
 // @flow
 
-import type { PermissionState } from './permission-state'
 import type { TransactionHistoryState } from './transaction-history-state'
 
 export type StoreState = {
-    permission: PermissionState,
     transactionHistory: TransactionHistoryState,
 }
