bcd88211e81688e42b20b1f0be69b55606861571
Juha Haavisto <juha.haavisto@sujuwa.fi>
2021-12-22 16:16:47 +0200

    Fix auth entry being rewritten when session token is present

--- app/src/shared/components/StreamrClientProvider.jsx
+++ app/src/shared/components/StreamrClientProvider.jsx
@@ -11,6 +11,7 @@ export default function StreamrClientProvider({ children }) {
 
         if (token) {
             nextConfig.auth = {
+                ...nextConfig.auth,
                 sessionToken: token,
             }
         }
