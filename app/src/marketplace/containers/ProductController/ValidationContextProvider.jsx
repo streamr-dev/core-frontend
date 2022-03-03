6f0c3c771c06678dbb94fe93c71c9ae6b8fa1e23
Mariusz Roliński <mariusz@mondoreale.pl>
2022-03-03 03:05:04 +0100

    Flow started complaining out of thin air…

--- app/src/marketplace/containers/ProductController/ValidationContextProvider.jsx
+++ app/src/marketplace/containers/ProductController/ValidationContextProvider.jsx
@@ -1,6 +1,4 @@
-// @flow
-
-import React, { useMemo, useCallback, useState, type Node, type Context } from 'react'
+import React, { useMemo, useCallback, useState } from 'react'
 import get from 'lodash/get'
 import set from 'lodash/fp/set'
 import isPlainObject from 'lodash/isPlainObject'
@@ -14,25 +12,27 @@ export const INFO = 'info'
 export const WARNING = 'warning'
 export const ERROR = 'error'
 
-export type Level = 'info' | 'warning' | 'error'
+// @FIXME: Rewrite in typescript.
+//
+// export type Level = 'info' | 'warning' | 'error'
+//
+// type ContextProps = {
+//     setStatus: (string, Level, string) => void,
+//     clearStatus: (string) => void,
+//     status: Object,
+//     isValid: (string) => boolean,
+//     validate: (Object) => void,
+//     touched: Object,
+//     setTouched: (string, ?boolean) => void,
+//     isTouched: (string) => boolean,
+//     isAnyTouched: () => boolean,
+//     resetTouched: () => void,
+//     pendingChanges: Object,
+//     isPendingChange: (string) => boolean,
+//     isAnyChangePending: () => boolean,
+// }
 
-type ContextProps = {
-    setStatus: (string, Level, string) => void,
-    clearStatus: (string) => void,
-    status: Object,
-    isValid: (string) => boolean,
-    validate: (Object) => void,
-    touched: Object,
-    setTouched: (string, ?boolean) => void,
-    isTouched: (string) => boolean,
-    isAnyTouched: () => boolean,
-    resetTouched: () => void,
-    pendingChanges: Object,
-    isPendingChange: (string) => boolean,
-    isAnyChangePending: () => boolean,
-}
-
-const ValidationContext: Context<ContextProps> = React.createContext({})
+const ValidationContext = React.createContext({})
 
 const isEqual = (a, b) => JSON.stringify(a) === JSON.stringify(b)
 
@@ -54,19 +54,19 @@ const validationErrors = {
     'contact.email': 'Email address is required',
 }
 
-function useValidationContext(): ContextProps {
+function useValidationContext() {
     const [status, setStatusState] = useState({})
     const [pendingChanges, setPendingChanges] = useState({})
     const [touched, setTouchedState] = useState({})
     const { product: originalProduct } = useController()
 
-    const setTouched = useCallback((name: string, value = true) => {
+    const setTouched = useCallback((name, value = true) => {
         setTouchedState((existing) => ({
             ...existing,
             [name]: !!value,
         }))
     }, [setTouchedState])
-    const isTouched = useCallback((name: string) => !!touched[name], [touched])
+    const isTouched = useCallback((name) => !!touched[name], [touched])
 
     const isAnyTouched = useCallback(() => Object.values(touched).some(Boolean), [touched])
 
@@ -74,7 +74,7 @@ function useValidationContext(): ContextProps {
 
     const isMounted = useIsMounted()
 
-    const setPendingChange = useCallback((name: string, isPending: boolean = true): Object => {
+    const setPendingChange = useCallback((name, isPending = true) => {
         if (!isMounted()) { return }
         if (!name) {
             throw new Error('pending change needs a name')
@@ -83,7 +83,7 @@ function useValidationContext(): ContextProps {
         setPendingChanges((state) => set(name, isPending, state))
     }, [setPendingChanges, isMounted])
 
-    const isPendingChange = useCallback((name: string) => !!(get(pendingChanges, name)), [pendingChanges])
+    const isPendingChange = useCallback((name) => !!(get(pendingChanges, name)), [pendingChanges])
 
     const isAnyChangePending = useCallback(() => (
         // flatten nested values
@@ -96,7 +96,7 @@ function useValidationContext(): ContextProps {
             .some(Boolean)
     ), [pendingChanges])
 
-    const setStatus = useCallback((name: string, level: Level, message: string): Object => {
+    const setStatus = useCallback((name, level, message) => {
         if (!isMounted()) { return }
         if (!name) {
             throw new Error('validation needs a name')
@@ -111,7 +111,7 @@ function useValidationContext(): ContextProps {
         }))
     }, [setStatusState, isMounted])
 
-    const clearStatus = useCallback((name: string): Object => {
+    const clearStatus = useCallback((name) => {
         if (!isMounted()) { return }
         if (!name) {
             throw new Error('validation needs a name')
@@ -120,7 +120,7 @@ function useValidationContext(): ContextProps {
         setStatusState(({ [name]: _, ...newState }) => newState)
     }, [setStatusState, isMounted])
 
-    const isValid = useCallback((name: string) => !status[name], [status])
+    const isValid = useCallback((name) => !status[name], [status])
 
     const validate = useCallback((product) => {
         if (!isMounted() || !product) { return }
@@ -178,11 +178,7 @@ function useValidationContext(): ContextProps {
     ])
 }
 
-type Props = {
-    children?: Node,
-}
-
-function ValidationContextProvider({ children }: Props) {
+function ValidationContextProvider({ children }) {
     return (
         <ValidationContext.Provider value={useValidationContext()}>
             {children || null}
