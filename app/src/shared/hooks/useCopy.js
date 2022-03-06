e2440dfa992bbbb0f7120fea23853166035d6400
Mariusz Roliński <mariusz@mondoreale.pl>
2022-03-06 13:30:03 +0100

    Add `onAfterCopied` callback to `useCopy`
    It’ll make it easier to DRY things up this way. Nice feature… I think. XD

--- app/src/shared/hooks/useCopy.js
+++ app/src/shared/hooks/useCopy.js
@@ -1,9 +1,9 @@
-import { useState, useCallback, useEffect, useMemo, useReducer } from 'react'
+import { useState, useCallback, useEffect, useMemo, useReducer, useRef } from 'react'
 import copyToClipboard from 'copy-to-clipboard'
 
 const SUSTAIN_IN_MILLIS = 3000
 
-export default function useCopy() {
+export default function useCopy(onAfterCopied) {
     const [isCopied, setIsCopied] = useState(false)
 
     const [copiedAt, touch] = useReducer((current) => (
@@ -15,12 +15,22 @@ export default function useCopy() {
         touch()
     }, [])
 
+    const onAfterCopiedRef = useRef(onAfterCopied)
+
+    useEffect(() => {
+        onAfterCopiedRef.current = onAfterCopied
+    }, [onAfterCopied])
+
     useEffect(() => {
         if (Date.now() > copiedAt + SUSTAIN_IN_MILLIS) {
             return () => {}
         }
 
         setIsCopied(true)
+
+        if (typeof onAfterCopiedRef.current === 'function') {
+            onAfterCopiedRef.current()
+        }
 
         const timeout = setTimeout(() => {
             setIsCopied(false)
