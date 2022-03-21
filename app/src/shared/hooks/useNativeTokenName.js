ab4e42fb5d410838525f6a2194da0fae94379391
Mariusz Roliński <mariusz@mondoreale.pl>
2022-03-21 11:02:35 +0100

    Support `undefined` network ids

--- app/src/shared/hooks/useNativeTokenName.js
+++ app/src/shared/hooks/useNativeTokenName.js
@@ -3,7 +3,7 @@ import { useSelector } from 'react-redux'
 import { selectEthereumNetworkId } from '$mp/modules/global/selectors'
 
 const useNativeTokenName = () => {
-    const currentNetworkId = useSelector(selectEthereumNetworkId)
+    const currentNetworkId = useSelector(selectEthereumNetworkId) || ''
     let nativeTokenName = 'Ether'
 
     if (currentNetworkId.toString() === '137') {
