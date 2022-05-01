a1575703d439c430920061e9f72f78a2f21f15b5
Mariusz Roliński <mariusz@mondoreale.pl>
2022-05-01 19:14:10 +0200

    Retrieve previous ethereum provider

--- app/src/utils/web3/getWeb3.js
+++ app/src/utils/web3/getWeb3.js
@@ -1,6 +1,7 @@
 import Web3 from 'web3'
 import FakeProvider from 'web3-fake-provider'
 import getConfig from '$shared/web3/config'
+import { getRecentMethod } from '$shared/reducers/session/methods'
 
 let instance
 
@@ -8,7 +9,15 @@ export const defaultFallbackProvider = new FakeProvider()
 
 export default function getWeb3() {
     if (!instance) {
-        instance = new Web3(defaultFallbackProvider)
+        let provider
+
+        try {
+            provider = getRecentMethod().getEthereumProvider()
+        } catch (e) {
+            // No-op.
+        }
+
+        instance = new Web3(provider || defaultFallbackProvider)
 
         instance.transactionConfirmationBlocks = getConfig().mainnet.transactionConfirmationBlocks
     }
