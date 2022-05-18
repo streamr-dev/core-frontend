5a10a3d3af3c39e10d75b9c0c78953666bd5ff2c
Tim Oxley <secoif@gmail.com>
2022-05-18 10:38:56 -0400

    Fix up import * tests.

--- app/test/unit/modules/myPurchaseList/constants.test.js
+++ app/test/unit/modules/myPurchaseList/constants.test.js
@@ -3,6 +3,9 @@ import * as constants from '$mp/modules/myPurchaseList/constants'
 describe('global - constants', () => {
     it('is namespaced correctly', () => {
         Object.keys(constants).forEach((key) => {
+            if (key === '__esModule') {
+                return // ignore __esModule: true
+            }
             expect(constants[key]).toEqual(expect.stringMatching(/^marketplace\/myPurchaseList\//))
         })
     })
