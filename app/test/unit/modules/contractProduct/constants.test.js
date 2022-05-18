d476f097954c5d37d956def3805aaddd84bdea05
Tim Oxley <secoif@gmail.com>
2022-05-18 10:38:56 -0400

    Fix up import * tests.

--- app/test/unit/modules/contractProduct/constants.test.js
+++ app/test/unit/modules/contractProduct/constants.test.js
@@ -3,6 +3,9 @@ import * as constants from '$mp/modules/contractProduct/constants'
 describe('contractProduct - constants', () => {
     it('is namespaced correctly', () => {
         Object.keys(constants).forEach((key) => {
+            if (key === '__esModule') {
+                return // ignore __esModule: true
+            }
             expect(constants[key]).toEqual(expect.stringMatching(/^marketplace\/contractProduct\//))
         })
     })
--- app/test/unit/modules/transactions/constants.test.js
+++ app/test/unit/modules/transactions/constants.test.js
@@ -3,6 +3,9 @@ import * as constants from '$mp/modules/transactions/constants'
 describe('streams - constants', () => {
     it('is namespaced correctly', () => {
         Object.keys(constants).forEach((key) => {
+            if (key === '__esModule') {
+                return // ignore __esModule: true
+            }
             expect(constants[key]).toEqual(expect.stringMatching(/^marketplace\/transactions\//))
         })
     })
