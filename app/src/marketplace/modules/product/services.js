63296395c3754f361cfeb57a8f5f33379924941c
Tuomas Koponen <tuomas.koponen@streamr.com>
2022-10-12 13:57:55 +0300

    Products are created as Paid by default now

--- app/src/marketplace/modules/product/services.js
+++ app/src/marketplace/modules/product/services.js
@@ -63,6 +63,7 @@ export const postEmptyProduct = (type: ProductType): ApiResult<Product> => post(
     url: routes.api.products.index(),
     data: {
         type,
+        pricePerSecond: '277777777777778', // default to paid product by setting price to 1 data per hour
     },
 })
     .then(mapProductFromApi)
