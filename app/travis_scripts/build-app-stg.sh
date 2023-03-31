7a2a88dca138ba6f209f677c508ef2b0602f62e1
Mariusz Roliński <mariusz@mondoreale.pl>
2023-03-31 19:30:33 +0200

    Remove SENTRY_INDEXER_DSN
    With the indexer gone this env variable is no longer used.

--- app/travis_scripts/build-app-stg.sh
+++ app/travis_scripts/build-app-stg.sh
@@ -10,7 +10,6 @@ echo $PLATFORM_PUBLIC_PATH
 echo $GOOGLE_ANALYTICS_ID
 echo $STORYBOOK_BASE_PATH
 echo $SENTRY_DSN
-echo $SENTRY_INDEXER_DSN
 echo $LOGROCKET_SLUG
 npm run build-index
 npm run build
