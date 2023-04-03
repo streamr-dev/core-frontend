5363b750cedeaeabce946aad9e7328397a46b0fb
Mariusz Roliński <mariusz@mondoreale.pl>
2023-04-03 10:27:15 +0200

    Remove the ShareSidebar component
    Again, lotta noise on the repo for an outdated reason. Not used anymore!

--- app/src/shared/utils/sharing/isValidUserId.ts
+++
@@ -1,4 +0,0 @@
-export default function isValidUserId(value: string): boolean {
-    const v = typeof value === 'string' ? value.trim() : ''
-    return v === 'anonymous' || v.startsWith('0x') || v.includes('@')
-}
