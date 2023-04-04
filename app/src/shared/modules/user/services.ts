d6e56798f9d1f2b6bc3e314a9d4ae88819d50469
Piotr Michałowski <pmichalowski1@gmail.com>
2023-04-04 11:19:17 +0200

    tests fix

--- app/src/shared/modules/user/services.ts
+++ app/src/shared/modules/user/services.ts
@@ -2,7 +2,7 @@ import { $Values } from 'utility-types'
 import { post } from '$shared/utils/api'
 import { ApiResult } from '$shared/types/common-types'
 import { BalanceType } from '$shared/types/user-types'
-import { Address } from '$shared/types/web3-types'
+import {Abi, Address} from '$shared/types/web3-types'
 import { getDataTokenBalance, getNativeTokenBalance } from '$mp/utils/web3'
 import reverseRecordsAbi from '$shared/web3/abis/reverseRecords.json'
 import {call, getContract} from "$mp/utils/smartContract"
@@ -63,7 +63,7 @@ export const getEnsDomains = ({ addresses }: { addresses: Array<Address> }): Api
     })
 
 export const lookupEnsName = async (address: Address): Promise<string> => {
-    const contract = getContract({abi: reverseRecordsAbi, address: config.reverseRecordsAddress}, true, 1)
+    const contract = getContract({abi: reverseRecordsAbi as Abi, address: config.reverseRecordsAddress}, true, 1)
     const response = await call(contract.methods.getNames([address]))
     return response[0]
 }
