e0530dd0b1e14ee85ab076b2fe5ac1178e6102a2
Tuomas Koponen <432588+tumppi@users.noreply.github.com>
2023-04-14 16:16:10 +0300

    Fix SearchBar debouncing

--- app/src/shared/components/SearchBar/index.tsx
+++ app/src/shared/components/SearchBar/index.tsx
@@ -1,6 +1,6 @@
 import React, { ChangeEvent, FunctionComponent, MouseEventHandler, useEffect, useMemo, useState } from 'react'
 import styled from 'styled-components'
-import { debounce } from 'lodash'
+import { useDebouncedCallback } from 'use-debounce'
 import { COLORS, SM, MD } from '$shared/utils/styled'
 import searchIcon from './SearchIcon.svg'
 import closeIcon from './CloseIcon.svg'
@@ -13,19 +13,21 @@ type SearchBarProps = {
     autoFocus?: boolean
 }
 
-const SearchBar: FunctionComponent<SearchBarProps> = ({onChange, value, debounceTime = 300, placeholder= 'Search', autoFocus= false}) => {
+const SearchBar: FunctionComponent<SearchBarProps> = ({onChange, value, debounceTime = 250, placeholder= 'Search', autoFocus= false}) => {
     const [focused, setFocused] = useState<boolean>()
     const [inputValue, setInputValue] = useState<string>('')
 
     useEffect(() => {
-        setInputValue(value)
+        if (value != null) {
+            setInputValue(value)
+        }
     }, [value])
 
-    const debouncedChangeHandler = useMemo(() => {
-        return debounce((searchValue: string): void => {
-            onChange(searchValue)
-        }, debounceTime)
-    }, [debounceTime, onChange])
+    const debouncedChangeHandler = useDebouncedCallback((searchValue: string) => {
+        if (typeof onChange === 'function') {
+            return onChange(searchValue)
+        }
+    }, debounceTime)
 
     const handleChange = (event: ChangeEvent<HTMLInputElement>): void => {
         setInputValue(event.target.value)
@@ -35,7 +37,9 @@ const SearchBar: FunctionComponent<SearchBarProps> = ({onChange, value, debounce
     const clearSearch: MouseEventHandler<HTMLButtonElement> = (event): void => {
         event.preventDefault()
         setInputValue('')
-        onChange('')
+        if (typeof onChange === 'function') {
+            onChange('')
+        }
     }
 
     return <FormWrapper onSubmit={(event) => event.preventDefault()}>
@@ -57,41 +61,41 @@ const SearchBar: FunctionComponent<SearchBarProps> = ({onChange, value, debounce
 const transitionTime = 150
 
 const FormWrapper = styled.form`
-  display: flex;
-  width: 100%;
-  background-color: ${COLORS.secondary};
-  border-radius: 200px;
-  position: relative;
+    display: flex;
+    width: 100%;
+    background-color: ${COLORS.secondary};
+    border-radius: 200px;
+    position: relative;
 `
 
 const SearchInput = styled.input`
-  background-color: transparent;
-  border: none;
-  border-radius: 200px;
-  width: 100%;
-  line-height: 40px;
-  height: 40px;
-  font-size: 14px;
-  padding: 0 20px 0 45px;
-  color: ${COLORS.primary};
-  outline: 2px solid transparent;
-  transition: all ${transitionTime}ms ease-in;
-  
-  :focus {
-    outline-color: ${COLORS.focus};
-    padding-left: 20px;
-  }
-
-  ::placeholder {
-    color: ${COLORS.primaryLight};
-    opacity: 1;
-  }
-
-  ::selection {
-    background-color: ${COLORS.selection};
-    opacity: 1;
+    background-color: transparent;
+    border: none;
+    border-radius: 200px;
+    width: 100%;
+    line-height: 40px;
+    height: 40px;
+    font-size: 14px;
+    padding: 0 20px 0 45px;
     color: ${COLORS.primary};
-  }
+    outline: 2px solid transparent;
+    transition: all ${transitionTime}ms ease-in;
+
+    :focus {
+        outline-color: ${COLORS.focus};
+        padding-left: 20px;
+    }
+
+    ::placeholder {
+        color: ${COLORS.primaryLight};
+        opacity: 1;
+    }
+
+    ::selection {
+        background-color: ${COLORS.selection};
+        opacity: 1;
+        color: ${COLORS.primary};
+    }
 
     @media (min-width: ${SM}px) {
         line-height: 50px;
@@ -105,18 +109,18 @@ const SearchInput = styled.input`
 `
 
 const SearchIcon = styled.img`
-  position: absolute;
-  width: 16px;
-  top: 12px;
-  left: 20px;
-  transition: opacity ${transitionTime}ms ease-in, left 1ms linear;
-  opacity: 1;
-  
-  &.hidden {
-    transition: opacity ${transitionTime}ms ease-in, left 1ms linear ${transitionTime}ms;
-    opacity: 0;
-    left: 0;
-  }
+    position: absolute;
+    width: 16px;
+    top: 12px;
+    left: 20px;
+    transition: opacity ${transitionTime}ms ease-in, left 1ms linear;
+    opacity: 1;
+
+    &.hidden {
+        transition: opacity ${transitionTime}ms ease-in, left 1ms linear ${transitionTime}ms;
+        opacity: 0;
+        left: 0;
+    }
 
     @media (min-width: ${SM}px) {
         top: 17px;
@@ -131,25 +135,25 @@ const ClearButton = styled.button`
     display: flex;
     align-items: center;
     justify-content: center;
-  border: none;
-  width: 40px;
-  height: 40px;
-  position: absolute;
-  right: 0;
-  top: 50%;
+    border: none;
+    width: 40px;
+    height: 40px;
+    position: absolute;
+    right: 0;
+    top: 50%;
     transform: translateY(-50%);
-  background-color: transparent;
-  border-radius: 100%;
-  transition: opacity ${transitionTime}ms ease-in, background-color ${transitionTime}ms ease-in;
-  opacity: 1;
+    background-color: transparent;
+    border-radius: 100%;
+    transition: opacity ${transitionTime}ms ease-in, background-color ${transitionTime}ms ease-in;
+    opacity: 1;
 
-  &.hidden {
-    opacity: 0;
-  }
-  
-  :hover {
-    background-color: ${COLORS.secondaryHover};
-  }
+    &.hidden {
+        opacity: 0;
+    }
+
+    :hover {
+        background-color: ${COLORS.secondaryHover};
+    }
 
     @media (min-width: ${SM}px) {
         width: 50px;
