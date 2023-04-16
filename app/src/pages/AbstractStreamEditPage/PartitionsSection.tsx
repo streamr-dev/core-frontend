adeefeb2f98a85490665c169cb8954d6521af412
Mariusz Roliński <mariusz@mondoreale.pl>
2023-04-16 11:46:08 +0200

    Refactor `TOCSection`
    TOC (table of contents) concept is gone. New design does not have the "bus line" component on the left.
    
    `BusLine` component is on it's way out soon, too, unfortunatelly. Sweet little piece of code.

--- app/src/pages/AbstractStreamEditPage/PartitionsSection.tsx
+++ app/src/pages/AbstractStreamEditPage/PartitionsSection.tsx
@@ -1,31 +1,46 @@
-import React, { Fragment, useState } from 'react'
+import React, { useState } from 'react'
 import { StreamPermission } from 'streamr-client'
 import styled from 'styled-components'
-import StatusLabel from '$shared/components/StatusLabel'
+import { Advanced } from '$shared/components/StatusLabel'
 import { useCurrentAbility } from '$shared/stores/abilities'
 import Label from '$ui/Label'
 import Numeric from '$ui/Numeric'
-import { useIsWithinNav } from '$shared/components/TOCPage/TOCNavContext'
-import TOCSection from '$shared/components/TOCPage/TOCSection'
 import { useCurrentDraft, useUpdateCurrentMetadata } from '$shared/stores/streamEditor'
+import Section from './Section'
 
 const PartitionRange = {
     Min: 1,
-    Max: 99
+    Max: 99,
 }
 
-function UnwrappedPartitionsSection({ disabled = false }) {
-    const { metadata: { partitions } } = useCurrentDraft()
+const Partitions = styled.div`
+    max-width: 136px;
+`
+
+const Desc = styled.p`
+    margin-bottom: 3.125rem;
+    max-width: 660px;
+`
+
+export default function PartitionsSection({ disabled: disabledProp = false }) {
+    const canEdit = useCurrentAbility(StreamPermission.EDIT)
+
+    const disabled = disabledProp || !canEdit
+
+    const {
+        metadata: { partitions },
+    } = useCurrentDraft()
 
     const [value, setValue] = useState(`${partitions}`)
 
     const updateMetadata = useUpdateCurrentMetadata()
 
     return (
-        <Fragment>
+        <Section title="Stream partitions" status={<Advanced />}>
             <Desc>
-                Partitioning enables high-volume streams to scale beyond what a typical node can handle. If
-                you&apos;re not sure if your stream needs partitions, leave it set to 1.
+                Partitioning enables high-volume streams to scale beyond what a typical
+                node can handle. If you&apos;re not sure if your stream needs partitions,
+                leave it set to 1.
             </Desc>
             <Partitions>
                 <Label>Partitions</Label>
@@ -48,7 +63,10 @@ function UnwrappedPartitionsSection({ disabled = false }) {
                                 throw new Error('Not a number')
                             }
 
-                            sanitizedValue = Math.max(PartitionRange.Min, Math.min(PartitionRange.Max, n))
+                            sanitizedValue = Math.max(
+                                PartitionRange.Min,
+                                Math.min(PartitionRange.Max, n),
+                            )
                         } catch (e) {
                             // Do nothing.
                         }
@@ -65,33 +83,6 @@ function UnwrappedPartitionsSection({ disabled = false }) {
                     name="partitions"
                 />
             </Partitions>
-        </Fragment>
-    )
-}
-
-const Partitions = styled.div`
-    max-width: 136px;
-`
-const Desc = styled.p`
-    margin-bottom: 3.125rem;
-    max-width: 660px;
-`
-export default function PartitionsSection({ disabled: disabledProp, ...props }) {
-    const canEdit = useCurrentAbility(StreamPermission.EDIT)
-
-    const disabled = disabledProp || !canEdit
-
-    const isWithinNav = useIsWithinNav()
-
-    return (
-        <TOCSection
-            id="stream-partitions"
-            title="Stream partitions"
-            linkTitle="Partitions"
-            status={<StatusLabel.Advanced />}
-            disabled={disabled}
-        >
-            {!isWithinNav && <UnwrappedPartitionsSection {...props} disabled={disabled} />}
-        </TOCSection>
+        </Section>
     )
 }
