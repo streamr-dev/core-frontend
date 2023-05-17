10194f6a43cce03fc0e080473a2c181dd75d177c
Mariusz Roliński <mariusz@mondoreale.pl>
2023-05-17 14:11:13 +0200

    Format

--- app/src/shared/components/StreamConnect/index.tsx
+++ app/src/shared/components/StreamConnect/index.tsx
@@ -6,14 +6,18 @@ import SvgIcon from '$shared/components/SvgIcon'
 import Button from '$shared/components/Button'
 import SelectField2 from '$mp/components/SelectField2'
 import { StreamId } from '$shared/types/stream-types'
-import {truncateStreamName} from "$shared/utils/text"
+import { truncateStreamName } from '$shared/utils/text'
 import useCopy from '$shared/hooks/useCopy'
 
-export const StreamConnect: FunctionComponent<{streams: StreamId[]}> = ({streams}) => {
+export const StreamConnect: FunctionComponent<{ streams: StreamId[] }> = ({
+    streams,
+}) => {
     const [streamId, setSelectedStream] = useState<StreamId>(streams[0])
     const [action, setAction] = useState<'subscribe' | 'publish'>('subscribe')
     const [nodeType, setNodeType] = useState<'lightNode' | 'brokerNode'>('lightNode')
-    const [currentProtocol, setCurrentProtocol] = useState<'websocket' | 'http' | 'mqtt'>('websocket')
+    const [currentProtocol, setCurrentProtocol] = useState<'websocket' | 'http' | 'mqtt'>(
+        'websocket',
+    )
 
     const lightNodeSnippetHeader = useMemo<string>(() => {
         return `// Run a Streamr node right inside your JS app
@@ -42,8 +46,10 @@ streamr.publish('${streamId}', {
     }, [streamId])
 
     const lightNodeSnippet = useMemo(
-        () => lightNodeSnippetHeader + (action === 'publish' ? lightNodePublish : lightNodeSubscribe),
-        [action, lightNodeSnippetHeader, lightNodeSubscribe, lightNodePublish]
+        () =>
+            lightNodeSnippetHeader +
+            (action === 'publish' ? lightNodePublish : lightNodeSubscribe),
+        [action, lightNodeSnippetHeader, lightNodeSubscribe, lightNodePublish],
     )
 
     const websocketSnippetHeader = useMemo(() => {
@@ -70,12 +76,15 @@ sub.onmessage = (msg) => {
 }`
     }, [streamId])
     const websocketSnippet = useMemo<string>(() => {
-        return websocketSnippetHeader + (action === 'publish' ? websocketPublish : websocketSubscribe)
+        return (
+            websocketSnippetHeader +
+            (action === 'publish' ? websocketPublish : websocketSubscribe)
+        )
     }, [websocketSnippetHeader, websocketPublish, websocketSubscribe, action])
 
     const httpSnippet = useMemo(() => {
         if (action === 'subscribe') {
-            return '// The Subscribe over HTTP is not available, as in it\'s not a valid selection.'
+            return "// The Subscribe over HTTP is not available, as in it's not a valid selection."
         }
         return `// Use your favourite language and HTTP library!
 
@@ -139,18 +148,25 @@ mqtt.subscribe('${streamId}', (msg) => {
             <div className={'col-lg-7'}>
                 <StreamConnectHeader>Connect</StreamConnectHeader>
                 <StreamConnectText>
-                    Applications publish and subscribe to streams via Streamr nodes. In other words, nodes are the access points to the Streamr
-                    Network. To connect your application to streams, you interface it with a Streamr node.
+                    Applications publish and subscribe to streams via Streamr nodes. In
+                    other words, nodes are the access points to the Streamr Network. To
+                    connect your application to streams, you interface it with a Streamr
+                    node.
                 </StreamConnectText>
-                <StreamConnectSubHeader>There are two strategies for interfacing applications with Streamr nodes:</StreamConnectSubHeader>
+                <StreamConnectSubHeader>
+                    There are two strategies for interfacing applications with Streamr
+                    nodes:
+                </StreamConnectSubHeader>
                 <StreamConnectList>
                     <li>
                         <strong>Light node: </strong>
-                        the node is imported to your application as a library and runs locally as part of your application
+                        the node is imported to your application as a library and runs
+                        locally as part of your application
                     </li>
                     <li>
                         <strong>Broker node: </strong>
-                        the node runs separately, and your application connects to it remotely using one of the supported protocols
+                        the node runs separately, and your application connects to it
+                        remotely using one of the supported protocols
                     </li>
                 </StreamConnectList>
 
@@ -175,7 +191,10 @@ mqtt.subscribe('${streamId}', (msg) => {
                     <SelectContainer>
                         <SelectField2
                             placeholder={''}
-                            options={streams.map((streamId) => ({ value: streamId, label: truncateStreamName(streamId) }))}
+                            options={streams.map((streamId) => ({
+                                value: streamId,
+                                label: truncateStreamName(streamId),
+                            }))}
                             value={streamId}
                             isClearable={false}
                             onChange={(streamId) => {
@@ -206,14 +225,18 @@ mqtt.subscribe('${streamId}', (msg) => {
                 {nodeType === 'lightNode' && (
                     <>
                         <StreamConnectLightNodeSnippetContainer>
-                            <CodeSnippet language={'javascript'}>{lightNodeSnippet}</CodeSnippet>
+                            <CodeSnippet language={'javascript'}>
+                                {lightNodeSnippet}
+                            </CodeSnippet>
                         </StreamConnectLightNodeSnippetContainer>
                         <StreamConnectSnippetCopyContainer>
                             <Button
                                 kind="secondary"
-                                onClick={() => void copy(currentBrokerSnippet, {
-                                    toastMessage: 'Copied',
-                                })}
+                                onClick={() =>
+                                    void copy(currentBrokerSnippet, {
+                                        toastMessage: 'Copied',
+                                    })
+                                }
                             >
                                 Copy
                             </Button>
@@ -223,23 +246,34 @@ mqtt.subscribe('${streamId}', (msg) => {
 
                 {nodeType === 'brokerNode' && (
                     <BrokerNodeSnippetContainer>
-                        <Tabs selected={'websocket'} onSelect={(tab) => setCurrentProtocol(tab)}>
+                        <Tabs
+                            selected={'websocket'}
+                            onSelect={(tab) => setCurrentProtocol(tab)}
+                        >
                             <Tabs.Item label={'Websocket'} value={'websocket'} key={0}>
-                                <CodeSnippet language={'javascript'}>{websocketSnippet}</CodeSnippet>
+                                <CodeSnippet language={'javascript'}>
+                                    {websocketSnippet}
+                                </CodeSnippet>
                             </Tabs.Item>
                             <Tabs.Item label={'HTTP'} value={'http'} key={1}>
-                                <CodeSnippet language={'javascript'}>{httpSnippet}</CodeSnippet>
+                                <CodeSnippet language={'javascript'}>
+                                    {httpSnippet}
+                                </CodeSnippet>
                             </Tabs.Item>
                             <Tabs.Item label={'MQTT'} value={'mqtt'} key={2}>
-                                <CodeSnippet language={'javascript'}>{mqttSnippet}</CodeSnippet>
+                                <CodeSnippet language={'javascript'}>
+                                    {mqttSnippet}
+                                </CodeSnippet>
                             </Tabs.Item>
                         </Tabs>
                         <StreamConnectSnippetCopyContainer>
                             <Button
                                 kind="secondary"
-                                onClick={() => void copy(currentBrokerSnippet, {
-                                    toastMessage: 'Copied',
-                                })}
+                                onClick={() =>
+                                    void copy(currentBrokerSnippet, {
+                                        toastMessage: 'Copied',
+                                    })
+                                }
                             >
                                 Copy
                             </Button>
@@ -254,11 +288,19 @@ mqtt.subscribe('${streamId}', (msg) => {
                     <SvgIcon name={'linkOut'} />
                 </StreamConnectLink>
             */}
-                <StreamConnectLink href={'https://www.npmjs.com/package/@streamr/cli-tools'} target={'_blank'} rel={'noreferrer noopener'}>
+                <StreamConnectLink
+                    href={'https://www.npmjs.com/package/@streamr/cli-tools'}
+                    target={'_blank'}
+                    rel={'noreferrer noopener'}
+                >
                     <span>Streamr command line tool</span>
                     <SvgIcon name={'linkOut'} />
                 </StreamConnectLink>
-                <StreamConnectLink href={'https://www.npmjs.com/package/streamr-client'} target={'_blank'} rel={'noreferrer noopener'}>
+                <StreamConnectLink
+                    href={'https://www.npmjs.com/package/streamr-client'}
+                    target={'_blank'}
+                    rel={'noreferrer noopener'}
+                >
                     <span>Streamr JavaScript Client quickstart</span>
                     <SvgIcon name={'linkOut'} />
                 </StreamConnectLink>
@@ -268,84 +310,84 @@ mqtt.subscribe('${streamId}', (msg) => {
 }
 
 const RightColumn = styled.div`
-  padding-top: 88px;
+    padding-top: 88px;
 `
 
 const StreamConnectHeader = styled.p`
-  font-size: 24px;
-  color: ${COLORS.primary};
-  line-height: 64px;
-  margin-bottom: 24px;
+    font-size: 24px;
+    color: ${COLORS.primary};
+    line-height: 64px;
+    margin-bottom: 24px;
 `
 
 const StreamConnectText = styled.p`
-  font-size: 16px;
-  color: ${COLORS.primary};
+    font-size: 16px;
+    color: ${COLORS.primary};
 `
 
 const StreamConnectSubHeader = styled.p`
-  font-size: 20px;
-  color: ${COLORS.primary};
-  font-weight: ${MEDIUM};
-  margin-bottom: 24px;
+    font-size: 20px;
+    color: ${COLORS.primary};
+    font-weight: ${MEDIUM};
+    margin-bottom: 24px;
 `
 
 const StreamConnectLightNodeSnippetContainer = styled.div`
-  border: 1px solid ${COLORS.separator};
-  background-color: white;
-  margin-top: 30px;
+    border: 1px solid ${COLORS.separator};
+    background-color: white;
+    margin-top: 30px;
 `
 const StreamConnectSnippetCopyContainer = styled.div`
-  border: 1px solid ${COLORS.separator};
-  border-top: none;
-  background-color: white;
-  padding: 16px;
-  display: flex;
-  align-items: center;
-  justify-content: flex-end;
+    border: 1px solid ${COLORS.separator};
+    border-top: none;
+    background-color: white;
+    padding: 16px;
+    display: flex;
+    align-items: center;
+    justify-content: flex-end;
 `
 
 const StreamConnectList = styled.ul`
-  padding: 0;
-  list-style-position: inside;
-  margin-bottom: 36px;
-  li {
-    font-size: 16px;
-  }
+    padding: 0;
+    list-style-position: inside;
+    margin-bottom: 36px;
+    li {
+        font-size: 16px;
+    }
 `
 const StreamConnectLink = styled.a`
-  font-size: 16px;
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-  background-color: ${COLORS.secondaryLight};
-  padding: 10px 16px;
-  margin-bottom: 8px;
-  border-radius: 4px;
-  span {
-    color: ${COLORS.primary}
-  }
-  svg {
-    color: ${COLORS.primary};
-  }
+    font-size: 16px;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    background-color: ${COLORS.secondaryLight};
+    padding: 10px 16px;
+    margin-bottom: 8px;
+    border-radius: 4px;
+    span {
+        color: ${COLORS.primary};
+    }
+    svg {
+        color: ${COLORS.primary};
+    }
 `
 
 const SnippetSelectorContainer = styled.div`
-  display: flex;
-  flex-wrap: nowrap;
-  margin-top: 20px;
-  align-items: center;
-  
-  > * {
-    margin-right: 10px;
-    &:last-child {
-      margin-right: 0;
+    display: flex;
+    flex-wrap: nowrap;
+    margin-top: 20px;
+    align-items: center;
+
+    > * {
+        margin-right: 10px;
+        &:last-child {
+            margin-right: 0;
+        }
     }
-  }
 `
 
 const BrokerNodeSnippetContainer = styled.div`
-  margin-top: 30px;
+    margin-top: 30px;
 `
 
 const SelectContainer = styled.div`
