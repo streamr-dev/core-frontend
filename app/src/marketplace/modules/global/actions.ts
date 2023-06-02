bdc4a40cec1775632e8845515cda997bcd8b2402
Piotr Michałowski <pmichalowski1@gmail.com>
2023-06-02 13:26:43 +0200

    redux removal in progress part 3

--- app/src/marketplace/modules/global/actions.ts
+++
@@ -1,5 +0,0 @@
-import { createAction } from 'redux-actions'
-import { SET_ETHEREUM_NETWORK_ID } from './constants'
-export const setEthereumNetworkId = createAction(SET_ETHEREUM_NETWORK_ID, (networkId) => ({
-    networkId,
-}))
--- app/src/marketplace/modules/global/constants.ts
+++
@@ -1,1 +0,0 @@
-export const SET_ETHEREUM_NETWORK_ID = 'marketplace/global/SET_ETHEREUM_NETWORK_ID'
