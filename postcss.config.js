a5d7237241d164e8b04c080275f1df3d6a379c0b
Tuomas Koponen <432588+tumppi@users.noreply.github.com>
2023-06-07 11:52:16 +0300

    First try of removing now unsupported precss

--- postcss.config.js
+++ postcss.config.js
@@ -6,9 +6,11 @@ const postcssImport = require('postcss-import')({
     addModulesDirectories: [path.resolve(__dirname, 'app/src/shared/assets/stylesheets')],
 })
 const calc = require('postcss-calc')
-const precss = require('precss')
 const postcssColorFunction = require('postcss-color-function')
+const postCssJitProps = require('postcss-jit-props')
 const cssMqpacker = require('css-mqpacker')
+const postcssPresetEnv = require('postcss-preset-env')
+const postcssNested = require('postcss-nested')
 const ums = require('./scripts/postcss/ums')
 const { xs, sm, md, lg, xl } = require('./scripts/breakpoints')
 
@@ -16,33 +18,22 @@ module.exports = {
     plugins: [
         ums,
         postcssImport,
-        precss({
-            features: {
-                // causes some issue in chrome.
-                // modern browsers support this anyway
-                filter: false,
-            },
-            importFrom: [
-                path.resolve(
-                    __dirname,
-                    'app/src/shared/assets/stylesheets/variables.css',
-                ),
-                {
-                    customMedia: {
-                        '--xs': `(max-width: ${xs.max}px)`,
-                        '--sm-up': `(min-width: ${sm.min}px)`,
-                        '--sm-down': `(max-width: ${sm.max}px)`,
-                        '--md-up': `(min-width: ${md.min}px)`,
-                        '--md-down': `(max-width: ${md.max}px)`,
-                        '--lg-up': `(min-width: ${lg.min}px)`,
-                        '--lg-down': `(max-width: ${lg.max}px)`,
-                        '--xl': `(min-width: ${xl.min}px)`,
-                        '--retina':
-                            '(-webkit-min-device-pixel-ratio: 2), (min-resolution: 192dpi)',
-                    },
-                },
+        postCssJitProps({
+            files: [
+                path.resolve(__dirname, 'app/src/shared/assets/stylesheets/variables.css'),
             ],
+            '--xs': `@custom-media --xs (max-width: ${xs.max}px);`,
+            '--sm-up': `@custom-media --sm-up (max-width: ${sm.min}px);`,
+            '--sm-down': `@custom-media --sm-down (max-width: ${sm.max}px);`,
+            '--md-up': `@custom-media --md-up (max-width: ${md.min}px);`,
+            '--md-down': `@custom-media --md-down (max-width: ${md.max}px);`,
+            '--lg-up': `@custom-media --lg-up (max-width: ${lg.min}px);`,
+            '--lg-down': `@custom-media --lg-down (max-width: ${lg.max}px);`,
+            '--xl': `@custom-media --xl (max-width: ${xl.min}px);`,
+            '--retina': `@custom-media --retina (-webkit-min-device-pixel-ratio: 2), (min-resolution: 192dpi);`,
         }),
+        postcssPresetEnv({ stage: 0 }),
+        postcssNested,
         calc, // Has to go after precss.
         postcssColorFunction,
         cssMqpacker,
