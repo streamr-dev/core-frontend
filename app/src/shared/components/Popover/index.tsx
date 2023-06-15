f0400107b2e17499bab2e4b6862c97f8a7a6eb6f
Piotr Michałowski <pmichalowski1@gmail.com>
2023-06-15 11:56:05 +0200

    navigation update in progress

--- app/src/shared/components/Popover/index.tsx
+++ app/src/shared/components/Popover/index.tsx
@@ -104,6 +104,22 @@ export const ToggleLabel = styled.span`
 `
 export const StyledDropdownMenu = styled(DropdownMenu)`
     min-width: 8rem;
+    &.nav-dropdown {
+        box-shadow: 0px 6px 12px 0px #52525226;
+        border: none;
+        border-radius: 8px;
+        padding: 0;
+        margin: 0;
+        .dropdown-item {
+            padding: 16px 36px !important;
+            &:first-of-type {
+                padding-top: 26px !important;
+            }
+            &:last-of-type {
+                padding-bottom: 26px !important;
+            }
+        }
+    }
 `
 const TextCaret = styled.span`
     margin-left: 0.35em;
