b4a320ec38d1e760dd9294ec1ee98c9b62301afb
Mariusz Roliński <mariusz@mondoreale.pl>
2023-07-21 15:14:35 +0200

    Fetch ENS graph using the Apollo client, too
    Comes with types and what not.

--- codegen.ts
+++ codegen.ts
@@ -4,6 +4,7 @@ const {
     NETWORK_GRAPH_SCHEMA_PATH = 'http://localhost:8000/subgraphs/name/streamr-dev/network-subgraphs',
     HUB_GRAPH_SCHEMA_PATH = 'http://localhost:8000/subgraphs/name/streamr-dev/hub-subgraph',
     DU_GRAPH_SCHEMA_PATH = 'http://localhost:8000/subgraphs/name/streamr-dev/dataunion',
+    ENS_GRAPH_SCHEMA_PATH = 'https://api.thegraph.com/subgraphs/name/ensdomains/ens',
 } = process.env
 
 const config: CodegenConfig = {
@@ -22,6 +23,14 @@ const config: CodegenConfig = {
         'src/generated/gql/du.ts': {
             documents: ['src/queries/du.ts'],
             schema: DU_GRAPH_SCHEMA_PATH,
+            plugins: ['typescript', 'typescript-operations', 'typescript-react-apollo'],
+            config: {
+                withHooks: false,
+            },
+        },
+        'src/generated/gql/ens.ts': {
+            documents: ['src/queries/ens.ts'],
+            schema: ENS_GRAPH_SCHEMA_PATH,
             plugins: ['typescript', 'typescript-operations', 'typescript-react-apollo'],
             config: {
                 withHooks: false,
