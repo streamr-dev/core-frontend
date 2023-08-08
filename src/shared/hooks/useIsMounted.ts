55eda3ca579eb87829331b6d53f3d959c2b0cbc8
Mariusz Roliński <mariusz@mondoreale.pl>
2023-08-08 11:41:27 +0200

    Drop `useIsMountedRef`; refactor `useIsMounted`
    I also mart `useIsMounted` as deprecated. All in good faith!

--- src/shared/hooks/useIsMounted.ts
+++ src/shared/hooks/useIsMounted.ts
@@ -1,6 +1,21 @@
-import { useCallback } from 'react'
-import useIsMountedRef from '~/shared/hooks/useIsMountedRef'
-export default (): (() => boolean) => {
-    const isMountedRef = useIsMountedRef()
-    return useCallback(() => !!isMountedRef.current, [isMountedRef])
+import { useCallback, useEffect, useRef } from 'react'
+
+/**
+ * @deprecated `useIsMounted` will often lead to confusing
+ * behaviour. Alternatives:
+ * - in effects: use a local `mounted` flag and unset it on discard.
+ * - in callbacks: use `AbortController` and `AbortSignal`.
+ */
+export default function useIsMounted() {
+    const ref = useRef(false)
+
+    useEffect(() => {
+        ref.current = true
+
+        return () => {
+            ref.current = false
+        }
+    }, [])
+
+    return useCallback(() => ref.current, [])
 }
