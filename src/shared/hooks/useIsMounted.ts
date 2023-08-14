b46352ba1ee3088b3823f02a1cf9afe31e45c299
Mariusz Roliński <mariusz@mondoreale.pl>
2023-08-14 15:05:14 +0200

    Add examples

--- src/shared/hooks/useIsMounted.ts
+++ src/shared/hooks/useIsMounted.ts
@@ -3,8 +3,27 @@ import { useCallback, useEffect, useRef } from 'react'
 /**
  * @deprecated `useIsMounted` will often lead to confusing
  * behaviour. Alternatives:
- * - in effects: use a local `mounted` flag and unset it on discard.
+ * - in effects: use a local `mounted` flag and unset it on discard (see example),
  * - in callbacks: use `AbortController` and `AbortSignal`.
+ * @example
+    // `mounted` flag example
+    useEffect(() => {
+        let mounted = true
+
+        async function fn() {
+            await new Promise((resolve) => void setTimeout(resolve))
+
+            if (mounted) {
+                // Do stuff!
+            }
+        }
+
+        fn()
+
+        return () => {
+            mounted = false
+        }
+    }, [])
  */
 export default function useIsMounted() {
     const ref = useRef(false)
