b310801a1c97d1fb79aa51dc2aa504547a14b247
Tuomas Koponen <432588+tumppi@users.noreply.github.com>
2023-09-12 10:26:42 +0300

    Add config for Mumbai testnet

--- src/getters/getChainConfig.ts
+++ src/getters/getChainConfig.ts
@@ -1,6 +1,4 @@
 import { config } from '@streamr/config'
 import { Chain } from '~/shared/types/web3-types'
-export const defaultChainConfig: Chain =
-    (process.env.HUB_CONFIG_ENV || process.env.NODE_ENV) !== 'production'
-        ? config.dev2
-        : config.polygon
+import getCoreConfig from './getCoreConfig'
+export const defaultChainConfig: Chain = config[getCoreConfig().defaultChain || 'polygon']
