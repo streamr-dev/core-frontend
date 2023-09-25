cec5b87a5c318f4370a61ac9378b5e80da214ac4
Mariusz Roliński <mariusz@mondoreale.pl>
2023-09-25 16:41:36 +0200

    Redo delegation stats for the overview page
    This is better and the stats function don't use infinite query unlike getting all delegations for lists.

--- src/hooks/delegations.ts
+++ src/hooks/delegations.ts
@@ -1,4 +1,6 @@
 import { UseInfiniteQueryResult, useInfiniteQuery } from '@tanstack/react-query'
+import { useEffect, useState } from 'react'
+import { Operator } from '~/generated/gql/network'
 import { getOperatorsByDelegation } from '~/getters'
 import { OperatorParser, ParsedOperator } from '~/parsers/OperatorParser'
 import { getSpotApy } from '~/utils/apy'
@@ -11,39 +13,132 @@ interface EnhancedOperator extends ParsedOperator {
     myShare: BN
 }
 
-export function useDelegationStats(address = '') {
-    const { data } = useDelegationsForWalletQuery(address, { pageSize: 1000 })
-
-    if (!data) {
-        return
+async function getEnhancedOperatorsForWallet(
+    address = '',
+    {
+        batchSize,
+        skip,
+        onParseError,
+        onBeforeComplete,
+    }: {
+        batchSize?: number
+        skip?: number
+        onParseError?: (operator: Operator, error: unknown) => void
+        onBeforeComplete?: (total: number, parsed: number) => void
+    },
+): Promise<EnhancedOperator[]> {
+    if (!address) {
+        return []
     }
 
-    const operators = data?.pages.flatMap((page) => page.delegations) || []
+    const operators: EnhancedOperator[] = []
 
-    let minApy = 1
-
-    let maxApy = 0
-
-    operators.forEach(({ apy }) => {
-        minApy = Math.min(minApy, apy)
-
-        maxApy = Math.max(maxApy, apy)
+    const rawOperators = await getOperatorsByDelegation({
+        first: batchSize,
+        skip,
+        address,
     })
 
-    if (minApy > maxApy) {
-        minApy = 0
+    const preparsedCount = rawOperators.length
 
-        maxApy = 0
+    for (let i = 0; i < preparsedCount; i++) {
+        const rawOperator = rawOperators[i]
+
+        try {
+            const operator = OperatorParser.parse(rawOperator)
+
+            operators.push({
+                ...operator,
+                apy: getSpotApy(operator.poolValue, operator.stakes),
+                myShare: getDelegationAmountForAddress2(address, operator.delegators),
+            })
+        } catch (e) {
+            onParseError
+                ? onParseError(rawOperator as Operator, e)
+                : console.warn('Failed to parse an operator', e)
+        }
     }
 
-    const value = operators.reduce((sum, { myShare }) => sum.plus(myShare), toBN(0))
+    onBeforeComplete?.(preparsedCount, operators.length)
 
-    return {
-        value,
-        minApy,
-        maxApy,
-        numOfOperators: operators.length,
-    }
+    return operators
+}
+
+interface DelegationsStats {
+    value: BN
+    minApy: number
+    maxApy: number
+    numOfOperators: number
+}
+
+export function useDelegationStats(address = '') {
+    const [stats, setStats] = useState<DelegationsStats | undefined | null>()
+
+    const addr = address.toLowerCase()
+
+    useEffect(() => {
+        let mounted = true
+
+        if (!addr) {
+            setStats(null)
+
+            return () => {}
+        }
+
+        setStats(undefined)
+
+        setTimeout(async () => {
+            const operators = await getEnhancedOperatorsForWallet(addr, {
+                batchSize: 1000,
+                onBeforeComplete(total, parsed) {
+                    if (total !== parsed) {
+                        errorToast({
+                            title: 'Warning',
+                            desc: `Delegation stats are calculated using ${parsed} out of ${total} available operators due to parsing issues.`,
+                        })
+                    }
+                },
+            })
+
+            if (!mounted) {
+                return
+            }
+
+            let minApy = 1
+
+            let maxApy = 0
+
+            operators.forEach(({ apy }) => {
+                minApy = Math.min(minApy, apy)
+
+                maxApy = Math.max(maxApy, apy)
+            })
+
+            if (minApy > maxApy) {
+                minApy = 0
+
+                maxApy = 0
+            }
+
+            const value = operators.reduce(
+                (sum, { myShare }) => sum.plus(myShare),
+                toBN(0),
+            )
+
+            setStats({
+                value,
+                minApy,
+                maxApy,
+                numOfOperators: operators.length,
+            })
+        })
+
+        return () => {
+            mounted = false
+        }
+    }, [addr])
+
+    return stats
 }
 
 export function useDelegationsForWalletQuery(
@@ -53,55 +148,27 @@ export function useDelegationsForWalletQuery(
     return useInfiniteQuery({
         queryKey: ['useDelegationsForWalletQuery', address.toLowerCase(), pageSize],
         async queryFn({ pageParam: skip = 0 }) {
-            if (!address) {
-                return {
-                    skip,
-                    delegations: [],
-                }
-            }
-
-            const delegations: EnhancedOperator[] = []
-
-            let preparsedCount = 0
+            let delegations: EnhancedOperator[] = []
 
             try {
-                const operators = await getOperatorsByDelegation({
-                    first: pageSize,
+                delegations = await getEnhancedOperatorsForWallet(address, {
+                    batchSize: pageSize,
                     skip,
-                    address,
+                    onBeforeComplete(total, parsed) {
+                        if (total !== parsed) {
+                            errorToast({
+                                title: 'Failed to parse',
+                                desc: `${
+                                    total - parsed
+                                } out of ${total} operators could not be parsed.`,
+                            })
+                        }
+                    },
                 })
-
-                preparsedCount = operators.length
-
-                for (let i = 0; i < operators.length; i++) {
-                    try {
-                        const operator = OperatorParser.parse(operators[i])
-
-                        delegations.push({
-                            ...operator,
-                            apy: getSpotApy(operator.poolValue, operator.stakes),
-                            myShare: getDelegationAmountForAddress2(
-                                address,
-                                operator.delegators,
-                            ),
-                        })
-                    } catch (e) {
-                        console.warn('Failed to parse an operator', e)
-                    }
-                }
             } catch (e) {
                 console.warn('Could not fetch operators', e)
 
                 errorToast({ title: 'Could not fetch operators' })
-            }
-
-            if (preparsedCount !== delegations.length) {
-                errorToast({
-                    title: 'Failed to parse',
-                    desc: `${
-                        preparsedCount - delegations.length
-                    } out of ${preparsedCount} operators could not be parsed.`,
-                })
             }
 
             return {
