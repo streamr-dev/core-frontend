c6c9b93f9fdcd6b3cba7f7c6b51bbee8c999abe9
Mariusz Roliński <mariusz@mondoreale.pl>
2023-09-30 01:11:06 +0200

    Use zod; patch up weak points

--- src/hooks/useOperatorLiveNodes.ts
+++ src/hooks/useOperatorLiveNodes.ts
@@ -1,31 +1,53 @@
-import { useEffect, useMemo, useState } from 'react'
+import { useEffect, useState } from 'react'
 import { useSubscribe } from 'streamr-client-react'
+import { z } from 'zod'
+
+const HeartbeatMessage = z.object({
+    parsedContent: z.object({
+        msgType: z.literal('heartbeat'),
+        peerDescriptor: z.object({
+            id: z.string(),
+        }),
+    }),
+})
+
+type HeartbeatMessage = z.infer<typeof HeartbeatMessage>
+
+function isHeartbeatMessage(arg: unknown): arg is HeartbeatMessage {
+    return HeartbeatMessage.safeParse(arg).success
+}
 
 export default function useOperatorLiveNodes(operatorId: string) {
     const streamId = `${operatorId}/operator/coordination`
-    const [liveNodes, setLiveNodes] = useState<Record<string, boolean>>({})
-    const [isLoading, setIsLoading] = useState(true)
+
+    const [liveNodes, setLiveNodes] = useState<Record<string, true>>({})
+
+    const [isLoading, setIsLoading] = useState(false)
 
     useSubscribe(
         { id: streamId },
         {
             onError: (e) => {
-                console.warn(e)
+                console.warn('Failed to count live nodes', e)
             },
             onMessage(msg) {
-                const data = msg.parsedContent as any
-                if (data?.msgType === 'heartbeat' && data?.peerDescriptor?.id != null) {
-                    const address = (data.peerDescriptor.id as string).toLowerCase()
-                    if (!liveNodes[address]) {
-                        setLiveNodes((prev) => ({...prev, [address]: true}))
-                    }
+                if (!isHeartbeatMessage(msg)) {
+                    return
                 }
+
+                const address = msg.parsedContent.peerDescriptor.id.toLowerCase()
+
+                setLiveNodes((prev) =>
+                    prev[address] ? prev : { ...prev, [address]: true },
+                )
             },
         },
     )
 
     useEffect(() => {
         let mounted = true
+
+        setIsLoading(true)
 
         setTimeout(() => {
             if (mounted) {
