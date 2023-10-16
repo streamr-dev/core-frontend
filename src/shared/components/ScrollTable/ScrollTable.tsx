23dc563de5b2f514ece77c6c1ae29c187e642e54
Piotr Michałowski <pmichalowski1@gmail.com>
2023-10-16 15:26:43 +0200

    removed unused import

--- src/shared/components/ScrollTable/ScrollTable.tsx
+++ src/shared/components/ScrollTable/ScrollTable.tsx
@@ -1,6 +1,5 @@
 import React, { FunctionComponent, ReactNode, useState } from 'react'
 import { Link } from 'react-router-dom'
-import cx from 'classnames'
 import {
     FloatingLoadingIndicator,
     NoDataWrap,
