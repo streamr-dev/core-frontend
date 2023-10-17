b9cf201dc0fa973caae28879a3cac593fc147742
Mariusz Roliński <mariusz@mondoreale.pl>
2023-10-17 13:59:40 +0200

    Make `getTokenLogUrl` support mumbai

--- src/shared/utils/tokenAssets.ts
+++ src/shared/utils/tokenAssets.ts
@@ -1,21 +1,27 @@
 import { Address } from '~/shared/types/web3-types'
+import { getConfigForChain } from '~/shared/web3/config'
+
 const BASE_URL = 'https://streamr-public.s3.amazonaws.com/truswallet-assets/blockchains'
-// Got from: https://api.coingecko.com/api/v3/asset_platforms
-const chainIdToNetworkMap = new Map<number, string>([
-    [1, 'ethereum'],
-    [100, 'xdai'],
-    [137, 'polygon'],
-    [8995, 'ethereum'],
-    [8997, 'ethereum'],
-])
+
 export const getTokenLogoUrl = (
     tokenContractAddress: Address,
     chainId: number,
 ): string => {
-    const network = chainIdToNetworkMap.get(chainId)
+    const network = (() => {
+        switch (chainId) {
+            case 100:
+                return 'xdai'
+            case 8995:
+            case 8996:
+                return 'ethereum'
+            default:
+                return getConfigForChain(chainId).name
+        }
+    })()
 
-    if (network == null) {
-        throw new Error(`Could not map chainId to network: ${chainId}`)
-    }
+    /**
+     * For more details see:
+     * https://api.coingecko.com/api/v3/asset_platforms
+     */
     return `${BASE_URL}/${network}/assets/${tokenContractAddress}/logo.png`
 }
