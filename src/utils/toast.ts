8c834eb8034a7817b691ccd6f9510d0143bf418e
Mariusz Roliński <mariusz@mondoreale.pl>
2023-10-19 13:46:23 +0200

    Single toast instance for the "Copied!" toasts

--- src/utils/toast.ts
+++ src/utils/toast.ts
@@ -1,28 +1,43 @@
 import { ComponentProps } from 'react'
-import { toaster } from 'toasterhea'
+import { Toaster, toaster } from 'toasterhea'
 import Toast, { ToastType } from '~/shared/toasts/Toast'
 import { Layer } from './Layer'
 
-export default function toast(props: ComponentProps<typeof Toast>) {
+export default function toast(
+    props: ComponentProps<typeof Toast>,
+    t?: Toaster<typeof Toast>,
+) {
     setTimeout(async () => {
         try {
-            await toaster(Toast, Layer.Toast).pop(props)
+            await (t || toaster(Toast, Layer.Toast)).pop(props)
         } catch (_) {
             // Do nothing
         }
     })
 }
 
-export function successToast(props: Omit<ComponentProps<typeof Toast>, 'type'>) {
-    toast({
-        type: ToastType.Success,
-        ...props,
-    })
+export function successToast(
+    props: Omit<ComponentProps<typeof Toast>, 'type'>,
+    t?: Toaster<typeof Toast>,
+) {
+    toast(
+        {
+            type: ToastType.Success,
+            ...props,
+        },
+        t,
+    )
 }
 
-export function errorToast(props: Omit<ComponentProps<typeof Toast>, 'type'>) {
-    toast({
-        type: ToastType.Warning,
-        ...props,
-    })
+export function errorToast(
+    props: Omit<ComponentProps<typeof Toast>, 'type'>,
+    t?: Toaster<typeof Toast>,
+) {
+    toast(
+        {
+            type: ToastType.Warning,
+            ...props,
+        },
+        t,
+    )
 }
