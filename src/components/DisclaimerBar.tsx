80ad6d6e8abbfc59971fb5c60ae01fee379668b0
Tuomas Koponen <432588+tumppi@users.noreply.github.com>
2023-11-16 09:12:02 +0200

    Show DisclaimerBar only on /network/ pages. Also update copy.

--- src/components/DisclaimerBar.tsx
+++ src/components/DisclaimerBar.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { useLocation } from 'react-router-dom'
 import styled from 'styled-components'
 import { toaster } from 'toasterhea'
 import LightModal, { ModalProps } from '~/modals/LightModal'
@@ -8,38 +9,55 @@ import { Layer } from '~/utils/Layer'
 
 const DisclaimerModal = (props: ModalProps) => <LightModal {...props} />
 
-export const DisclaimerBar = () => (
-    <Root>
-        <SvgIcon name="infoBadge" />
-        <div>
-            Streamr risk disclaimer: Unproven technology, participate at your own risk.
-            Click to{' '}
-            <a
-                onClick={async () => {
-                    try {
-                        await toaster(DisclaimerModal, Layer.Modal).pop({
-                            title: 'Disclaimer',
-                            children: (
-                                <p>
-                                    Streamr 1.0 testnets involve new, unproven technology
-                                    including smart contracts and new protocols, which
-                                    contain inherent risks. Users should conduct thorough
-                                    research before engaging, and participate at their own
-                                    risk. Participation can result in irreversible loss of
-                                    funds. Exercise caution. Never share your private key
-                                    with anyone.
-                                </p>
-                            ),
-                        })
-                    } catch (e) {}
-                }}
-            >
-                learn more
-            </a>
-            .
-        </div>
-    </Root>
-)
+export const DisclaimerBar = () => {
+    const { pathname } = useLocation()
+
+    // Show disclaimer only for Overview, Sponsorships, Sponsorship, Operators, Operator pages
+    if (!pathname.includes('/hub/network/')) {
+        return null
+    }
+
+    return (
+        <Root>
+            <SvgIcon name="infoBadge" />
+            <div>
+                Streamr risk disclaimer: The sponsorship, staking & delegation smart
+                contracts are unproven technology, participate at your own risk.{' '}
+                <a
+                    onClick={async () => {
+                        try {
+                            await toaster(DisclaimerModal, Layer.Modal).pop({
+                                title: 'Disclaimer',
+                                children: (
+                                    <p>
+                                        The Streamr 1.0 testnets involve new, unproven
+                                        technology including smart contracts and new
+                                        protocols, which contain inherent risks. Users
+                                        should conduct thorough research before engaging,
+                                        and participate at their own risk. Participation
+                                        can result in irreversible loss of funds. Exercise
+                                        caution. Never share your private key with anyone.{' '}
+                                        <a
+                                            href="https://docs.streamr.network/streamr-testnets/testnets"
+                                            target="_blank"
+                                            rel="noopener noreferrer"
+                                        >
+                                            Learn more
+                                        </a>
+                                        .
+                                    </p>
+                                ),
+                            })
+                        } catch (e) {}
+                    }}
+                >
+                    Learn more
+                </a>
+                .
+            </div>
+        </Root>
+    )
+}
 
 const Root = styled.div`
     display: grid;
