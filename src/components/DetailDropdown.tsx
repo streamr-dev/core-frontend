92af34cb3029a6ffbef446dfd0d40f4ea5372491
Mariusz Roliński <mariusz@mondoreale.pl>
2023-12-01 09:59:05 +0100

    Revert "Fix overflowing project url"
    This reverts commit 6a52fa9139215c7991155f7f8f7dc8f0e25c34b8.

--- src/components/DetailDropdown.tsx
+++ src/components/DetailDropdown.tsx
@@ -185,7 +185,6 @@ const Popover = styled.div`
     width: ${PopoverWidth}px;
     position: absolute;
     top: 100%;
-    z-index: 10;
 `
 
 const IconWrap = styled.div`
@@ -216,9 +215,12 @@ const Value = styled.div<{ $unset?: boolean }>`
 `
 
 export const List = styled.ul`
-    display: flex;
-    flex-wrap: wrap;
+    display: grid;
     gap: 8px;
+    grid-template-columns: repeat(
+        ${({ children }) => React.Children.count(children)},
+        auto
+    );
     justify-content: start;
     list-style: none;
     margin: 0;
