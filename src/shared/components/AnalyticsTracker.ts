65d90c87a9fd1708a83d44fcf29ae124406e6a77
Mariusz Roliński <mariusz@mondoreale.pl>
2023-12-14 14:52:10 +0100

    GA → GTM #wip

--- src/shared/components/AnalyticsTracker.ts
+++ src/shared/components/AnalyticsTracker.ts
@@ -1,24 +1,27 @@
 import { useEffect, useRef } from 'react'
 import { useLocation } from 'react-router-dom'
-import ReactGA from 'react-ga'
-const GA_ID = process.env.GOOGLE_ANALYTICS_ID
+
 export default function AnalyticsTracker() {
     const { pathname } = useLocation()
+
     const createdRef = useRef(false)
+
     useEffect(() => {
-        if (!GA_ID || !pathname) {
-            return
-        }
+        // @ts-expect-error lol
+        console.log(window.gtag)
+        // if (!GA_ID || !pathname) {
+        //     return
+        // }
 
-        if (!createdRef.current) {
-            // GA script is already in the DOM. Calling `ReactGA.initialize` would
-            // add another copy. We don't want that. `window.ga('create', …)` is
-            // all we need.
-            window.ga('create', GA_ID)
-            createdRef.current = true
-        }
+        // if (!createdRef.current) {
+        //     // GA script is already in the DOM. Calling `ReactGA.initialize` would
+        //     // add another copy. We don't want that. `window.ga('create', …)` is
+        //     // all we need.
+        //     window.ga('create', GA_ID)
+        //     createdRef.current = true
+        // }
 
-        ReactGA.pageview(pathname)
+        // ReactGA.pageview(pathname)
     }, [pathname])
     return null
 }
