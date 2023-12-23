16f496e5b5e0180626ee92f0c1b92ee142d5e110
Henri Pihkala <henri@streamr.network>
2023-12-23 12:46:49 +0200

    set sponsorships default orderBy to staked

--- src/pages/SponsorshipsPage.tsx
+++ src/pages/SponsorshipsPage.tsx
@@ -41,7 +41,7 @@ export const SponsorshipsPage = () => {
     const wallet = useWalletAccount()
 
     const { orderBy, orderDirection, handleOrderChange } = useTableOrder({
-        orderBy: 'payoutPerDay',
+        orderBy: 'staked',
         orderDirection: ScrollTableOrderDirection.Desc,
     })
 
