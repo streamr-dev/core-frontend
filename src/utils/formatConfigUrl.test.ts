61e93d9a79704935476a05b65c262b6958ab3cc2
Mariusz Roliński <mariusz@mondoreale.pl>
2024-02-19 14:24:05 +0100

    Adjust tests for `formatConfigUrl`

--- src/utils/formatConfigUrl.test.ts
+++ src/utils/formatConfigUrl.test.ts
@@ -1,47 +1,43 @@
-import getConfig from '~/getters/getConfig'
 import setTempEnv from '../../test/test-utils/setTempEnv'
-import f from './formatConfigUrl'
-jest.mock('~//getters/getConfig', () => ({
-    __esModule: true,
-    default: jest.fn(),
-}))
-const WS = {
-    protocol: 'ws',
-}
+import formatConfigUrl from './formatConfigUrl'
 
 function testDockerHost({ envHost, expectedHost }) {
+    const dockerHost = 'testhost'
+
     return () => {
-        beforeEach(() => {
-            ;(getConfig as any).mockImplementation(() => ({
-                docker: {
-                    host: 'testhost',
-                },
-            }))
-        })
         setTempEnv({
             STREAMR_DOCKER_DEV_HOST: envHost,
         })
+
         it('applies proper hostname to strings starting with "/"', () => {
-            expect(f('/path')).toEqual(`http://${expectedHost}/path`)
-            expect(f('/path', WS)).toEqual(`ws://${expectedHost}/path`)
+            expect(formatConfigUrl('/path', { dockerHost })).toEqual(
+                `http://${expectedHost}/path`,
+            )
+            expect(formatConfigUrl('/path', { protocol: 'ws', dockerHost })).toEqual(
+                `ws://${expectedHost}/path`,
+            )
         })
+
         it('applies proper hostname to strings starting with ":"', () => {
-            expect(f(':1234/path')).toEqual(`http://${expectedHost}:1234/path`)
-            expect(f(':1234/path', WS)).toEqual(`ws://${expectedHost}:1234/path`)
+            expect(formatConfigUrl(':1234/path', { dockerHost })).toEqual(
+                `http://${expectedHost}:1234/path`,
+            )
+            expect(formatConfigUrl(':1234/path', { protocol: 'ws', dockerHost })).toEqual(
+                `ws://${expectedHost}:1234/path`,
+            )
         })
+
         it('applies proper hostname to empty strings', () => {
-            expect(f('')).toEqual(`http://${expectedHost}`)
-            expect(f('', WS)).toEqual(`ws://${expectedHost}`)
+            expect(formatConfigUrl('', { dockerHost })).toEqual(`http://${expectedHost}`)
+            expect(formatConfigUrl('', { protocol: 'ws', dockerHost })).toEqual(
+                `ws://${expectedHost}`,
+            )
         })
-        it('passes non-strings along, unchanged', () => {
-            expect(f(undefined)).toBe(undefined)
-            expect(f(null)).toBe(null)
-            expect(f('false')).toBe('false') // and so on.
-        })
+
         it('passes non-empty strings that start with charaters other than ":" and "/" along', () => {
-            expect(f('http://url')).toEqual('http://url')
-            expect(f('anything')).toEqual('anything')
-            expect(f(' ')).toEqual(' ')
+            expect(formatConfigUrl('http://url', { dockerHost })).toEqual('http://url')
+            expect(formatConfigUrl('anything', { dockerHost })).toEqual('anything')
+            expect(formatConfigUrl(' ', { dockerHost })).toEqual(' ')
         })
     }
 }
@@ -53,6 +49,7 @@ describe(
         expectedHost: 'testhost',
     }),
 )
+
 describe(
     'formatConfigUrl with STREAMR_DOCKER_DEV_HOST',
     testDockerHost({
