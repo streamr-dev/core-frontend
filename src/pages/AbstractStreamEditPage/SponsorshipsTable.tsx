8a4db534f28a1f2e0cdc19d9f7c5999c0ea11b09
Mariusz Roliński <mariusz@mondoreale.pl>
2024-02-20 14:39:55 +0100

    Simplify the structure of Stream overview page

--- src/pages/AbstractStreamEditPage/SponsorshipsTable.tsx
+++ src/pages/AbstractStreamEditPage/SponsorshipsTable.tsx
@@ -37,76 +37,70 @@ export default function SponsorshipsTable({ streamId }: Props) {
     const sponsorships = query.data?.pages.map((page) => page.sponsorships).flat() || []
 
     return (
-        <Root>
-            <NetworkPageSegment
-                title={
-                    <TitleBar
-                        label={sponsorships.length || undefined}
-                        aux={
-                            sponsorships.length > 0 && (
-                                <CreateButton
-                                    type="button"
-                                    onClick={() =>
-                                        createSponsorship(chainId, wallet, { streamId })
-                                    }
-                                    disabled={streamId == null || wallet == null}
+        <NetworkPageSegment
+            title={
+                <TitleBar
+                    label={sponsorships.length || undefined}
+                    aux={
+                        sponsorships.length > 0 && (
+                            <CreateButton
+                                type="button"
+                                onClick={() =>
+                                    createSponsorship(chainId, wallet, { streamId })
+                                }
+                                disabled={streamId == null || wallet == null}
+                            >
+                                Create
+                            </CreateButton>
+                        )
+                    }
+                >
+                    Sponsorships
+                </TitleBar>
+            }
+        >
+            {sponsorships.length > 0 ? (
+                <QueriedSponsorshipsTable
+                    query={query}
+                    orderBy={orderBy}
+                    orderDirection={orderDirection}
+                    onOrderChange={setOrder}
+                    hideStreamId
+                />
+            ) : (
+                <NoDataContainer>
+                    <NoData
+                        firstLine="This stream does not have any Sponsorships yet."
+                        secondLine={
+                            <span>
+                                <a
+                                    href="https://docs.streamr.network/streamr-network/incentives/stream-sponsorships"
+                                    rel="noopener noreferrer"
+                                    target="_blank"
                                 >
-                                    Create
-                                </CreateButton>
-                            )
+                                    Stream Sponsorships
+                                </a>{' '}
+                                incentivize node operators to relay the
+                                <br /> stream&apos;s data, which enhances its
+                                connectability and robustness.
+                            </span>
                         }
-                    >
-                        Sponsorships
-                    </TitleBar>
-                }
-            >
-                {sponsorships.length > 0 ? (
-                    <QueriedSponsorshipsTable
-                        query={query}
-                        orderBy={orderBy}
-                        orderDirection={orderDirection}
-                        onOrderChange={setOrder}
-                        hideStreamId
+                        compact
                     />
-                ) : (
-                    <NoDataContainer>
-                        <NoData
-                            firstLine="This stream does not have any Sponsorships yet."
-                            secondLine={
-                                <span>
-                                    <a
-                                        href="https://docs.streamr.network/streamr-network/incentives/stream-sponsorships"
-                                        rel="noopener noreferrer"
-                                        target="_blank"
-                                    >
-                                        Stream Sponsorships
-                                    </a>{' '}
-                                    incentivize node operators to relay the
-                                    <br /> stream&apos;s data, which enhances its
-                                    connectability and robustness.
-                                </span>
-                            }
-                            compact
-                        />
-                        <CreateButton
-                            type="button"
-                            onClick={() => {
-                                createSponsorship(chainId, wallet, { streamId })
-                            }}
-                            disabled={streamId == null || wallet == null}
-                        >
-                            Create Sponsorship
-                        </CreateButton>
-                    </NoDataContainer>
-                )}
-            </NetworkPageSegment>
-        </Root>
+                    <CreateButton
+                        type="button"
+                        onClick={() => {
+                            createSponsorship(chainId, wallet, { streamId })
+                        }}
+                        disabled={streamId == null || wallet == null}
+                    >
+                        Create Sponsorship
+                    </CreateButton>
+                </NoDataContainer>
+            )}
+        </NetworkPageSegment>
     )
 }
-
-const Root = styled.div`
-    margin-top: 24px;
-`
 
 const NoDataContainer = styled.div`
     display: grid;
