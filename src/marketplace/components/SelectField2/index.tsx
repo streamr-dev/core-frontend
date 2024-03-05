9f76c847bc2e20909ad44d542b121647dd14f916
Mariusz Roliński <mariusz@mondoreale.pl>
2024-03-05 13:09:46 +0100

    Eslint

--- src/marketplace/components/SelectField2/index.tsx
+++ src/marketplace/components/SelectField2/index.tsx
@@ -1,4 +1,4 @@
-import React, { FunctionComponent, useEffect, useMemo, useState } from 'react'
+import React, { useEffect, useMemo, useState } from 'react'
 import ReactSelect, {
     ClearIndicatorProps,
     DropdownIndicatorProps,
