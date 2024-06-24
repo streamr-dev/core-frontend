40bd3a605f0cb37756098efa65f405e9d7b35e6a
Mariusz Roliński <mariusz@mondoreale.pl>
2024-06-24 18:40:49 +0200

    Refactor token info getting to use tanstack queries

--- src/components/FormattedPaymentRate.tsx
+++ src/components/FormattedPaymentRate.tsx
@@ -3,7 +3,7 @@ import { contractCurrencies as currencies } from '~/shared/utils/constants'
 import { formatPrice } from '~/marketplace/utils/price'
 import { TimeUnit } from '~/shared/utils/timeUnit'
 import { BNish, toBN } from '~/utils/bn'
-import useTokenInfo from '~/hooks/useTokenInfo'
+import { useTokenInfo } from '~/utils/tokens'
 
 interface Options {
     amount?: BNish
