dbc52da045de8fbba68d23f4f7533265dbbd6350
Mariusz Roliński <mariusz@mondoreale.pl>
2024-06-24 15:48:31 +0200

    Naming

--- src/getters/getConfigValueFromChain.ts
+++ src/getters/getConfigValueFromChain.ts
@@ -1,18 +1,18 @@
 import { Contract } from 'ethers'
 import { StreamrConfig, streamrConfigABI } from 'network-contracts-ethers6'
 import { getPublicWeb3Provider } from '~/shared/stores/wallet'
-import { ConfigKey } from '~/types'
+import { ChainConfigKey } from '~/types'
 import { getChainConfig } from '~/utils/chains'
 
 const cache: Record<
     number,
-    Partial<Record<ConfigKey, { updatedAt: number; value: unknown }>> | undefined
+    Partial<Record<ChainConfigKey, { updatedAt: number; value: unknown }>> | undefined
 > = {}
 
 const TTL = 60 * 60 * 1000 // 1h
 
 export async function getConfigValueFromChain<
-    T extends ConfigKey,
+    T extends ChainConfigKey,
     U extends Awaited<ReturnType<StreamrConfig[T]>>,
 >(chainId: number, key: T): Promise<U> {
     const { updatedAt = 0, value } = cache[chainId]?.[key] || {}
