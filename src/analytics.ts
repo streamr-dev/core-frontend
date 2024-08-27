e405439ed62d467ab13c60ad3132d6b57359bd8e
Mariusz Roliński <mariusz@mondoreale.pl>
2024-08-27 12:37:09 +0200

    Incl. chain id, sdk and config versions in Sentry payloads

--- src/analytics.ts
+++ src/analytics.ts
@@ -1,4 +1,4 @@
-import { init } from '@sentry/react'
+import { getGlobalScope, init } from '@sentry/react'
 
 if (process.env.SENTRY_DSN) {
     init({
@@ -6,5 +6,10 @@ if (process.env.SENTRY_DSN) {
         release: process.env.VERSION,
         environment: process.env.SENTRY_ENVIRONMENT,
         debug: true,
+    })
+
+    getGlobalScope().setExtras({
+        '@streamr/sdk': process.env.STREAMR_SDK_VERSION,
+        '@streamr/config': process.env.STREAMR_CONFIG_VERSION,
     })
 }
--- src/utils/sentry.ts
+++ src/utils/sentry.ts
@@ -1,16 +1,24 @@
-import { withScope } from '@sentry/react'
+import { getGlobalScope } from '@sentry/react'
 import { useEffect } from 'react'
 import { useWalletAccount } from '~/shared/stores/wallet'
+import { useCurrentChainId } from '~/utils/chains'
 
 export function useSetSentryScopeEffect() {
     const account = useWalletAccount()
 
+    const chainId = useCurrentChainId()
+
     useEffect(
         function setSentryScopeUser() {
-            withScope((scope) => {
-                scope.setUser({ id: account })
-            })
+            getGlobalScope().setUser({ id: account })
         },
         [account],
+    )
+
+    useEffect(
+        function setSentryScopeChainId() {
+            getGlobalScope().setExtra('chainId', chainId)
+        },
+        [chainId],
     )
 }
