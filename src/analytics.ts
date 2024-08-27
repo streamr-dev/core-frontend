eacd374c9797773facd6e81b3d2abbb2f956b7c1
Mariusz Roliński <mariusz@mondoreale.pl>
2024-08-27 12:22:38 +0200

    Simplify
    Let's not filter out urls for now. Doesn't seem to work the way it used to.

--- src/analytics.ts
+++ src/analytics.ts
@@ -1,19 +1,10 @@
 import { init } from '@sentry/react'
-import { getEnvironmentConfig } from './getters/getEnvironmentConfig'
-
-const { streamrUrl, platformOriginUrl } = getEnvironmentConfig()
 
 if (process.env.SENTRY_DSN) {
     init({
         dsn: process.env.SENTRY_DSN,
         release: process.env.VERSION,
         environment: process.env.SENTRY_ENVIRONMENT,
-        allowUrls: [
-            window.location.origin,
-            process.env.PLATFORM_PUBLIC_PATH || '',
-            platformOriginUrl,
-            streamrUrl,
-        ].filter(Boolean),
         debug: true,
     })
 }
